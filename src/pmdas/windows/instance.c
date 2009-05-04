3a20a83633c303225f237a1068753be8e2d3aa1c
Nathan Scott <nathans@debian.org>
2009-05-04 16:02:47 +1000

    Fix up mismerge of last checkin, missing code snippet in Windows PMDA

--- src/pmdas/windows/instance.c
+++ src/pmdas/windows/instance.c
@@ -315,7 +315,7 @@ windows_check_instance(char *path, pdh_metric_t *mp)
 	    p = strchr(path, '(');	// skip hostname and Process/Thread
 	    if (p != NULL) {
 		p++;
-		if ((strncmp(p, "_Total)", 7) == 0 ||
+		if ((strncmp(p, "_Total)", 7) == 0) ||
 		    (strncmp(p, "_Total/", 7) == 0)) {
 		    /*
 		     * The totals are done as independent metrics,
