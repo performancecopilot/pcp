210be7f679b10d71dda985fdebc7dd9aec5d606a
Nathan Scott <nathans@debian.org>
2009-08-28 08:44:50 +1000

    Correct some regular expressions in Samba PMDA.
    Of particular note, some of the heading lines in the
    smbstatus --profile output have trailing whitespace.
    Otherwise, this PMDA is now fully functional; and as
    it used the stock Samba utilities, is not dependent
    on the many flavours of Samba shmem format anymore.
    At the cost of a slight overhead increase, of course,
    but the original was unmaintainable.
    
    Thanks to James Peach for advise on redesigning this.

--- src/pmdas/samba/pmdasamba.pl
+++ src/pmdas/samba/pmdasamba.pl
@@ -31,13 +31,13 @@ sub samba_fetch
     my $cluster = 0;
     my $prefix = '';
 
-    open(STATS, $smbstats) ||
-	$pmda->err("pmdasamba failed to open $smbstats: $!");
+    open(STATS, "$smbstats |") ||
+	$pmda->err("pmdasamba failed to open $smbstats pipe: $!");
 
     while (<STATS>) {
-	if (m/^\*\*\*\* (\W+)/) {
+	if (m/^\*\*\*\*\s+(\w+.*)$/) {
 	    my $heading = $1;
-
+	    $heading =~ s/ +$//g;
 	    $item = 0;
 	    if ($heading eq 'System Calls') {
 		$cluster = 1; $prefix = 'syscalls';
@@ -58,12 +58,12 @@ sub samba_fetch
 	    } elsif ($heading eq 'NMBD Calls') {
 		$cluster = 9; $prefix = 'nmb';
 	    } else {
-		$pmda->warn("pmdasamba failed to parse $smbstats heading: $1");
+		$pmda->log("pmdasamba failed to parse $smbstats heading: $1");
 		$cluster = 0; $prefix = '';
 	    }
 	}
 	# we've found a real name/value pair, work out PMID and hash it
-	elsif (m/^(\W+):\w+(\d+)/) {
+	elsif (m/^(\w+):\s+(\d+)$/) {
 	    my @metric = ( $1, $2 );
 	    my $pmid;
 
@@ -103,8 +103,8 @@ samba_fetch();	# extract names and values into %metrics, keyed on PMIDs
 # hash iterate, keys are PMIDs, names and values are in @metrics{$pmid}.
 foreach my $pmid (sort(keys %metrics)) {
     my $name = $metrics{$pmid}[0];
-    if ($name == 'samba.writecache.num_write_caches' ||
-	$name == 'samba.writecache.allocated_caches') {
+    if ($name eq 'samba.writecache.num_write_caches' ||
+	$name eq 'samba.writecache.allocated_caches') {
 	$pmda->add_metric($pmid, PM_TYPE_U32, PM_INDOM_NULL, PM_SEM_INSTANT,
 			pmda_units(0,0,1,0,0,PM_COUNT_ONE), $name, '', '');
     } elsif ($name =~ /_time$/) {
@@ -133,10 +133,10 @@ pmdasamba - Linux virtualisation performance metrics domain agent (PMDA)
 B<pmdasamba> is a Performance Metrics Domain Agent (PMDA) which exports
 metric values from Samba, a Windows SMB/CIFS server for UNIX.
 
-In order for values to be made available by this PMDA, Samba must
-be built with profiling support (WITH_PROFILE in "smbd -b" output).
-Unlike many PMDAs it dynamically enumerates much of its metric hierarchy,
-based on the contents of "smbstatus --profile".
+In order for values to be made available by this PMDA, Samba must have
+been built with profiling support (WITH_PROFILE in "smbd -b" output).
+This PMDA dynamically enumerates much of its metric hierarchy, based on
+the contents of "smbstatus --profile".
 
 When the agent is installed (see below), the Install script will attempt
 to enable Samba statistics gathering, using "smbcontrol --profile".
