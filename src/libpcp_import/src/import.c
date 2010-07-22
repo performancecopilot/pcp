b541e61950d74c4ce4c8f30caa7b8897019c73bd
Ken McDonell <kenj@internode.on.net>
2010-07-22 17:01:11 +1000

    libpcp_import - minor fixes

--- src/libpcp_import/src/import.c
+++ src/libpcp_import/src/import.c
@@ -584,7 +584,7 @@ pmiWrite(int sec, int usec)
 	return current->last_sts = PMI_ERR_NODATA;
 
     if (sec < 0) {
-	gettimeofday(&current->result->timestamp, NULL);
+	__pmtimevalNow(&current->result->timestamp);
     }
     else {
 	current->result->timestamp.tv_sec = sec;
