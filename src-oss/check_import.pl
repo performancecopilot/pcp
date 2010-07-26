e214ade16ab3117c8d7670318d67be4a37eb64e7
Ken McDonell <kenj@internode.on.net>
2010-07-26 18:19:19 +1000

    369, 370, 371, 372 - pmimport stuff
    Assorted QA for libpcp_import, PCP::LogImport, sar2pcp and sheet2pcp.

--- src-oss/check_import.pl
+++ src-oss/check_import.pl
@@ -50,9 +50,9 @@ check($_, "pmiUseContext");
 $_ = pmiUseContext($ctx1);
 check($_, "pmiUseContext");
 
-$_ = pmiAddMetric("my.metric.foo", pmid_build(245,0,1), PM_TYPE_U32, PM_INDOM_NULL, PM_SEM_INSTANT, pmiUnits(1,-1,0,PM_SPACE_MBYTE,PM_TIME_SEC,0));
+$_ = pmiAddMetric("my.metric.foo", pmid_build(PMI_DOMAIN,0,1), PM_TYPE_U32, PM_INDOM_NULL, PM_SEM_INSTANT, pmiUnits(1,-1,0,PM_SPACE_MBYTE,PM_TIME_SEC,0));
 check($_, "pmiAddMetric");
-$_ = pmiAddMetric("my.metric.bar", PM_ID_NULL, PM_TYPE_U64, pmInDom_build(245,1), PM_SEM_INSTANT, pmiUnits(1,-1,0,PM_SPACE_MBYTE,PM_TIME_SEC,0));
+$_ = pmiAddMetric("my.metric.bar", PM_ID_NULL, PM_TYPE_U64, pmInDom_build(PMI_DOMAIN,1), PM_SEM_INSTANT, pmiUnits(1,-1,0,PM_SPACE_MBYTE,PM_TIME_SEC,0));
 check($_, "pmiAddMetric");
 $_ = pmiAddMetric("my.metric.foo", 1, 2, 3, 4, pmiUnits(0,0,0,0,0,0));
 check($_, "pmiAddMetric");
@@ -62,18 +62,18 @@ $_ = pmiAddMetric("my.metric.double", PM_ID_NULL, PM_TYPE_DOUBLE, PM_INDOM_NULL,
 check($_, "pmiAddMetric");
 $_ = pmiAddMetric("my.metric.string", PM_ID_NULL, PM_TYPE_STRING, PM_INDOM_NULL, PM_SEM_INSTANT, pmiUnits(0,0,0,0,0,0));
 check($_, "pmiAddMetric");
-$_ = pmiAddMetric("my.dup.pmid", pmid_build(245,0,3), PM_TYPE_32, PM_INDOM_NULL, PM_SEM_INSTANT, pmiUnits(0,0,0,0,0,0));
+$_ = pmiAddMetric("my.dup.pmid", pmid_build(PMI_DOMAIN,0,3), PM_TYPE_32, PM_INDOM_NULL, PM_SEM_INSTANT, pmiUnits(0,0,0,0,0,0));
 check($_, "pmiAddMetric");
 $_ = pmiAddMetric("my.metric.float", PM_ID_NULL, PM_TYPE_FLOAT, PM_INDOM_NULL, PM_SEM_DISCRETE, pmiUnits(0,0,0,0,0,0));
 check($_, "pmiAddMetric");
 
-$_ = pmiAddInstance(pmInDom_build(245,1), "eek really", 1);
+$_ = pmiAddInstance(pmInDom_build(PMI_DOMAIN,1), "eek really", 1);
 check($_, "pmiAddInstance");
-$_ = pmiAddInstance(pmInDom_build(245,1), "eek", 2);
+$_ = pmiAddInstance(pmInDom_build(PMI_DOMAIN,1), "eek", 2);
 check($_, "pmiAddInstance");
-$_ = pmiAddInstance(pmInDom_build(245,1), "blah", 3);
+$_ = pmiAddInstance(pmInDom_build(PMI_DOMAIN,1), "blah", 3);
 check($_, "pmiAddInstance");
-$_ = pmiAddInstance(pmInDom_build(245,1), "not-blah-again", 3);
+$_ = pmiAddInstance(pmInDom_build(PMI_DOMAIN,1), "not-blah-again", 3);
 check($_, "pmiAddInstance");
 
 $_ = pmiPutValue("my.metric.foo", "", 123);
@@ -128,7 +128,7 @@ check($_, "pmiEnd");
 
 $ctx2 = pmiStart("myotherarchive", 1);
 check($ctx2, "pmiStart");
-$_ = pmiAddInstance(pmInDom_build(245,1), "other", 2);
+$_ = pmiAddInstance(pmInDom_build(PMI_DOMAIN,1), "other", 2);
 check($_, "pmiAddInstance");
 $hdl2 = pmiGetHandle("my.metric.bar", "eek");
 check($hdl2, "pmiGetHandle");
