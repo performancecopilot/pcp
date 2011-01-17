f5bbb48c0d57d936fd4a508a7b022e501cc5b83d
Ken McDonell <kenj@internode.on.net>
2011-01-18 06:36:35 +1100

    026, chkhelp.c - improve test coverage
    Test more error cases and add iteration with a context created
    using pmDupContext.

--- src-oss/chkhelp.c
+++ src-oss/chkhelp.c
@@ -14,6 +14,7 @@ int
 main()
 {
     int		i;
+    int		j;
     char	*namelist[2];
     pmID	pmidlist[2];
     int		n;
@@ -34,9 +35,10 @@ main()
 
     i = 0;
     namelist[i++] = "sample.colour";
+    namelist[i++] = "sample.byte_ctr";
     numpmid = i;
     n = pmLookupName(numpmid, namelist, pmidlist);
-    if (n < 0) {
+    if (n != numpmid) {
 	fprintf(stderr, "pmLookupName: %s\n", pmErrStr(n));
 	for (i = 0; i < numpmid; i++) {
 	    if (pmidlist[i] == PM_ID_NULL)
@@ -44,48 +46,60 @@ main()
 	}
 	exit(1);
     }
-    if ((n = pmLookupDesc(pmidlist[0], &desc)) < 0) {
-	fprintf(stderr, "initial pmLookupDesc failed: %s\n", pmErrStr(n));
-	exit(1);
-    }
 
-    for (i = 0; i < 2; i++) {
-	if (i == 0)
-	    fprintf(stderr, "\nDefault Context\n");
-	else {
-	    fprintf(stderr, "\nNew Context\n");
-	    if ((n = pmNewContext(PM_CONTEXT_HOST, "localhost")) < 0) {
-		fprintf(stderr, "pmNewContext: %s\n", pmErrStr(n));
+    for (j = 0; j < 3; j++) {
+	/* default context (j==0) then NewContext(j==1) then DupContext(j==2) */
+	for (i = 0; i < numpmid; i++) {
+	    if (j == 0)
+		fprintf(stderr, "\nDefault Context\n");
+	    else if (j == 1) {
+		fprintf(stderr, "\nNew Context\n");
+		if ((n = pmNewContext(PM_CONTEXT_HOST, "localhost")) < 0) {
+		    fprintf(stderr, "pmNewContext: %s\n", pmErrStr(n));
+		    exit(1);
+		}
+	    }
+	    else {
+		fprintf(stderr, "\nDup Context\n");
+		if ((n = pmDupContext()) < 0) {
+		    fprintf(stderr, "pmDupContext: %s\n", pmErrStr(n));
+		    exit(1);
+		}
+	    }
+
+	    fprintf(stderr, "metric: %s\n", namelist[i]);
+	    if ((n = pmLookupDesc(pmidlist[i], &desc)) < 0) {
+		fprintf(stderr, "pmLookupDesc failed: %s\n", pmErrStr(n));
 		exit(1);
 	    }
-	}
 
-	if ((n = pmLookupText(pmidlist[0], PM_TEXT_ONELINE, &buf)) < 0)
-	    fprintf(stderr, "pmLookupText: %s\n", pmErrStr(n));
-	else {
-	    fprintf(stderr, "\nOneline Text: %s\n", buf);
-	    free(buf);
-	}
+	    if ((n = pmLookupText(pmidlist[i], PM_TEXT_ONELINE, &buf)) < 0)
+		fprintf(stderr, "pmLookupText: %s\n", pmErrStr(n));
+	    else {
+		fprintf(stderr, "\nOneline Text: %s\n", buf);
+		free(buf);
+	    }
 
-	if ((n = pmLookupInDomText(desc.indom, PM_TEXT_ONELINE, &buf)) < 0)
-	    fprintf(stderr, "pmLookupInDomText: %s\n", pmErrStr(n));
-	else {
-	    fprintf(stderr, "\nOneline InDomText: %s\n", buf);
-	    free(buf);
-	}
+	    if ((n = pmLookupInDomText(desc.indom, PM_TEXT_ONELINE, &buf)) < 0)
+		fprintf(stderr, "pmLookupInDomText: %s\n", pmErrStr(n));
+	    else {
+		fprintf(stderr, "\nOneline InDomText: %s\n", buf);
+		free(buf);
+	    }
 
-	if ((n = pmLookupText(pmidlist[0], PM_TEXT_HELP, &buf)) < 0)
-	    fprintf(stderr, "pmLookupText: %s\n", pmErrStr(n));
-	else {
-	    fprintf(stderr, "\nHelp Text: %s\n", buf);
-	    free(buf);
-	}
+	    if ((n = pmLookupText(pmidlist[i], PM_TEXT_HELP, &buf)) < 0)
+		fprintf(stderr, "pmLookupText: %s\n", pmErrStr(n));
+	    else {
+		fprintf(stderr, "\nHelp Text: %s\n", buf);
+		free(buf);
+	    }
 
-	if ((n = pmLookupInDomText(desc.indom, PM_TEXT_HELP, &buf)) < 0)
-	    fprintf(stderr, "pmLookupInDomText: %s\n", pmErrStr(n));
-	else {
-	    fprintf(stderr, "\nHelp InDomText: %s\n", buf);
-	    free(buf);
+	    if ((n = pmLookupInDomText(desc.indom, PM_TEXT_HELP, &buf)) < 0)
+		fprintf(stderr, "pmLookupInDomText: %s\n", pmErrStr(n));
+	    else {
+		fprintf(stderr, "\nHelp InDomText: %s\n", buf);
+		free(buf);
+	    }
 	}
     }
 
