576ab3f486424f5fd399fab7572b13310b07a1b4
Ken McDonell <kenj@internode.on.net>
2011-01-18 07:05:36 +1100

    423, atomstr.c - improved test coverage
    Longer aggregate to check ellipsis formating after start of value.
    More PM_TYPE_STRING test cases (different length strings).
    More PM_TYPE_EVENT test cases.
    More PM_TYPE_AGGREGATE test cases.

--- src-oss/atomstr.c
+++ src-oss/atomstr.c
@@ -18,8 +18,10 @@ main(int argc, char **argv)
     int		errflag = 0;
     static char	*usage = "[-D N]";
     pmAtomValue	atom;
-    char	aggr[] = { '\01', '\02', '\03', '\04', '\05', '\06', '\07' };
-    int		*ip;
+    char	aggr[] = {
+	    '\00', '\01', '\02', '\03', '\04', '\05', '\06', '\07',
+	    '\10', '\11', '\12', '\13', '\14', '\15', '\16', '\17'
+    			 };
     int		hdl;
     struct timeval	stamp = { 123, 456 };
 
@@ -73,26 +75,41 @@ main(int argc, char **argv)
 
     atom.cp = "mary had a little lamb";
     printf("%s-> %s\n", atom.cp, pmAtomStr(&atom, PM_TYPE_STRING));
+    atom.cp = NULL;
+    printf("%s-> %s\n", atom.cp, pmAtomStr(&atom, PM_TYPE_STRING));
+    /* length = 37 */
+    atom.cp = "abcdefghijklmnopqrstuvwxyz0123456789X";
+    printf("%s -> %s\n", atom.cp, pmAtomStr(&atom, PM_TYPE_STRING));
+    /* length = 39 */
+    atom.cp = "abcdefghijklmnopqrstuvwxyz0123456789XYZ";
+    printf("%s -> %s\n", atom.cp, pmAtomStr(&atom, PM_TYPE_STRING));
 
     hdl = pmdaEventNewArray();
+    atom.vbp = (pmValueBlock *)pmdaEventGetAddr(hdl);
+    printf("??? -> %s\n", pmAtomStr(&atom, PM_TYPE_EVENT));
     pmdaEventAddRecord(hdl, &stamp, 0);
     atom.l = -42;
     pmdaEventAddParam(hdl, PM_ID_NULL, PM_TYPE_32, &atom);
     atom.vbp = (pmValueBlock *)pmdaEventGetAddr(hdl);
     printf("??? -> %s\n", pmAtomStr(&atom, PM_TYPE_EVENT));
+    pmdaEventAddRecord(hdl, &stamp, 0);
+    atom.cp = "hullo world";
+    pmdaEventAddParam(hdl, PM_ID_NULL, PM_TYPE_STRING, &atom);
+    atom.vbp = (pmValueBlock *)pmdaEventGetAddr(hdl);
+    printf("??? -> %s\n", pmAtomStr(&atom, PM_TYPE_EVENT));
 
     atom.vbp = (pmValueBlock *)malloc(PM_VAL_HDR_SIZE + sizeof(aggr));
     atom.vbp->vlen = PM_VAL_HDR_SIZE + sizeof(aggr);
-    atom.vbp->vtype = PM_TYPE_AGGREGATE;
-    memcpy(atom.vbp->vbuf, (void *)aggr, sizeof(aggr));
-    ip = (int *)atom.vbp->vbuf;
-    *ip = htonl(*ip);
-    ip++;
-    *ip = htonl(*ip);
-    printf("??? -> %s\n", pmAtomStr(&atom, PM_TYPE_AGGREGATE));
     atom.vbp->vtype = PM_TYPE_AGGREGATE_STATIC;
+    memcpy(atom.vbp->vbuf, (void *)aggr, sizeof(aggr));
     printf("??? -> %s\n", pmAtomStr(&atom, PM_TYPE_AGGREGATE_STATIC));
+    atom.vbp->vtype = PM_TYPE_AGGREGATE;
+    for (atom.vbp->vlen = PM_VAL_HDR_SIZE; atom.vbp->vlen <= PM_VAL_HDR_SIZE + sizeof(aggr); atom.vbp->vlen += 2) {
+	printf("??? [len=%d] -> %s\n", atom.vbp->vlen - PM_VAL_HDR_SIZE, pmAtomStr(&atom, PM_TYPE_AGGREGATE));
+    }
     free(atom.vbp);
+    atom.vbp = NULL;
+    printf("NULL -> %s\n", pmAtomStr(&atom, PM_TYPE_AGGREGATE));
 
     printf("\nAnd now some error cases ...\n");
     printf("bad type -> %s\n", pmAtomStr(&atom, 123));
