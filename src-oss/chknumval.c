891328cd9de5ea867cbd098eaaa7b94ef41ba186
Ken McDonell <kenj@internode.on.net>
2011-01-18 06:43:34 +1100

    043, chknumval.c - improved test coverage
    Add -L and -K options to allow PM_CONTEXT_LOCAL testing.
    Allow non-default metrics to be specified from the command line.
    Do a baseline pmStore test (numval > 0, no errors expected).
    
    Beyond 043, the new test 273 uses the additional chknumval
    features to drive additional libpcp test coverage.

--- src-oss/chknumval.c
+++ src-oss/chknumval.c
@@ -4,6 +4,8 @@
 
 /*
  * chknumval - check out new encoding of errors within numval of a pmResult
+ *
+ * also drives pmStore, __pmConnectLogger and __pmControlLog testing
  */
 
 #include <ctype.h>
@@ -16,10 +18,10 @@ main(int argc, char **argv)
     int		c;
     int		sts;
     int		errflag = 0;
+    char	*errmsg;
+    int		type = 0;
     char	*host = "localhost";
     char	*namespace = PM_NS_DEFAULT;
-    static char	*debug = "[-D N]";
-    static char	*usage = "[-D N] [-h hostname] [-n namespace] [-v]";
     int		i;
     int		ctlport;
     int		pid = PM_LOG_PRIMARY_PID;
@@ -28,14 +30,15 @@ main(int argc, char **argv)
     pmID	pmidlist[20];
     int		*instlist;
     char	**inamelist;
-    int		numpmid;
+    int		numpmid = 0;
+    int		default_metrics = 0;
     pmResult	*req;
     pmResult	*status;
     pmDesc	desc;
 
     __pmSetProgname(argv[0]);
 
-    while ((c = getopt(argc, argv, "D:h:n:")) != EOF) {
+    while ((c = getopt(argc, argv, "D:h:K:Ln:")) != EOF) {
 	switch (c) {
 
 	case 'D':	/* debug flag */
@@ -50,7 +53,27 @@ main(int argc, char **argv)
 	    break;
 
 	case 'h':	/* hostname for PMCD to contact */
+	    if (type != 0) {
+		fprintf(stderr, "%s: at most one of --h and -L allowed\n", pmProgname);
+		errflag++;
+	    }
 	    host = optarg;
+	    type = PM_CONTEXT_HOST;
+	    break;
+
+	case 'K':	/* update local PMDA table */
+	    if ((errmsg = __pmSpecLocalPMDA(optarg)) != NULL) {
+		fprintf(stderr, "%s: __pmSpecLocalPMDA failed: %s\n", pmProgname, errmsg);
+		errflag++;
+	    }
+	    break;
+
+	case 'L':	/* local PMDA connection, no PMCD */
+	    if (type != 0) {
+		fprintf(stderr, "%s: at most one of -h and -L allowed\n", pmProgname);
+		errflag++;
+	    }
+	    type = PM_CONTEXT_LOCAL;
 	    break;
 
 	case 'n':	/* alternative name space file */
@@ -65,17 +88,38 @@ main(int argc, char **argv)
     }
 
     if (errflag) {
-	printf("Usage: %s %s%s\n", pmProgname, debug, usage);
+	fprintf(stderr,
+"Usage: %s [options] [metricname ...]\n\
+\n\
+Options\n\
+  -D debug	standard PCP debug flag\n\
+  -h host	metrics source is PMCD on host (default is localhost)\n\
+  -L            metrics source is local connection to PMDA, no PMCD\n\
+  -K spec       optional additional PMDA spec for local connection\n\
+                spec is of the form op,domain,dso-path,init-routine\n\
+  -n namespace  use an alternative PMNS\n\
+  -v            be verbose\n",
+		pmProgname);
+	exit(1);
+    }
+
+    while (optind < argc) {
+	namelist[numpmid++] = argv[optind];
+	optind++;
+    }
+    if (numpmid > 0 && numpmid < 3) {
+	printf("Quitting need at least 3 metrics (not %d) for numval tests\n", numpmid);
 	exit(1);
     }
 
     if ((sts = pmLoadNameSpace(namespace)) < 0) {
-	printf("Usage: %s %s%s\n", pmProgname, debug, usage);
+	printf("%s: pmLoadNameSpace: %s\n", pmProgname, pmErrStr(sts));
 	exit(1);
     }
 
-    if ((sts = pmNewContext(PM_CONTEXT_HOST, host)) < 0) {
-	printf("%s: Cannot connect to PMCD on host \"%s\": %s\n", pmProgname, host, pmErrStr(sts));
+    if (type == 0) type = PM_CONTEXT_HOST;
+    if ((sts = pmNewContext(type, host)) < 0) {
+	printf("%s: pmNewContext(%d, %s): %s\n", pmProgname, type, host, pmErrStr(sts));
 	exit(1);
     }
 
@@ -85,15 +129,19 @@ main(int argc, char **argv)
     }
     ctlport = sts;
 
-    i = 0;
-    namelist[i++] = "pmcd.control.debug";
-    namelist[i++] = "sampledso.long.write_me";
-    namelist[i++] = "sample.colour";
-    numpmid = i;
+    if (numpmid == 0) {
+	/* default metrics */
+	default_metrics = 1;
+	i = 0;
+	namelist[i++] = "pmcd.control.debug";
+	namelist[i++] = "sampledso.long.write_me";
+	namelist[i++] = "sample.colour";
+	numpmid = i;
+    }
 
     sts = pmLookupName(numpmid, namelist, pmidlist);
-    if (sts < 0) {
-	printf("pmLookupName: %s\n", pmErrStr(sts));
+    if (sts != numpmid) {
+	printf("pmLookupName: failed: %s\n", sts < 0 ? pmErrStr(sts) : "");
 	for (i = 0; i < numpmid; i++) {
 	    if (pmidlist[i] == PM_ID_NULL)
 		printf("	%s - not known\n", namelist[i]);
@@ -113,6 +161,12 @@ main(int argc, char **argv)
 
     __pmDumpResult(stdout, req);
 
+    printf("\nbase store test (failures not unexpected) ...\n");
+    if ((sts = pmStore(req)) < 0)
+	printf("pmStore: %s\n", pmErrStr(sts));
+    else
+	printf("pmStore: OK\n");
+
     printf("\nnumpmid == 0 tests (failures expected) ...\n");
     req->numpmid = 0;
     __pmDumpResult(stdout, req);
@@ -124,7 +178,8 @@ main(int argc, char **argv)
     if ((sts = __pmControlLog(ctlport, req, PM_LOG_ENQUIRE, 0, 0, &status)) < 0)
 	printf("__pmControlLog: %s\n", pmErrStr(sts));
     else {
-	printf("__pmControlLog: botch, PM_ERR_VALUE expected\n");
+	printf("__pmControlLog: OK\n");
+	__pmDumpResult(stdout, status);
 	pmFreeResult(status);
     }
 
@@ -140,7 +195,8 @@ main(int argc, char **argv)
     if ((sts = __pmControlLog(ctlport, req, PM_LOG_ENQUIRE, 0, 0, &status)) < 0)
 	printf("__pmControlLog: %s\n", pmErrStr(sts));
     else {
-	printf("__pmControlLog: botch, PM_ERR_VALUE expected\n");
+	printf("__pmControlLog: OK\n");
+	__pmDumpResult(stdout, status);
 	pmFreeResult(status);
     }
 
@@ -156,23 +212,35 @@ main(int argc, char **argv)
     if ((sts = __pmControlLog(ctlport, req, PM_LOG_ENQUIRE, 0, 0, &status)) < 0)
 	printf("__pmControlLog: %s\n", pmErrStr(sts));
     else {
-	printf("__pmControlLog: botch, PM_ERR_VALUE expected\n");
+	printf("__pmControlLog: OK\n");
+	__pmDumpResult(stdout, status);
 	pmFreeResult(status);
     }
 
     /* exercise *.needprofile */
     pmFreeResult(req);
 
-    i = 0;
-    namelist[i++] = "sampledso.long.hundred";
-    namelist[i++] = "sampledso.needprofile";
-    namelist[i++] = "sample.long.hundred";
-    namelist[i++] = "sample.needprofile";
-    numpmid = i;
+    if (default_metrics) {
+	i = 0;
+	namelist[i++] = "sampledso.long.hundred";
+	namelist[i++] = "sampledso.needprofile";
+	namelist[i++] = "sample.long.hundred";
+	namelist[i++] = "sample.needprofile";
+	numpmid = i;
+    }
+    else {
+	if (numpmid < 4) {
+	    printf("Quitting need at least 4 metrics for profile tests\n");
+	    exit(1);
+	}
+	printf("Warning: running profile tests with non-default metrics ...\n");
+	printf("... metric %s needs an indom with at least %d instances\n", namelist[1], 5);
+	printf("... metric %s needs an indom with at least %d instances\n", namelist[3], 4);
+    }
 
     sts = pmLookupName(numpmid, namelist, pmidlist);
-    if (sts < 0) {
-	printf("pmLookupName: botch, %s\n", pmErrStr(sts));
+    if (sts != numpmid) {
+	printf("pmLookupName: failed: %s\n", sts < 0 ? pmErrStr(sts) : "");
 	for (i = 0; i < numpmid; i++) {
 	    if (pmidlist[i] == PM_ID_NULL)
 		printf("	%s - not known\n", namelist[i]);
