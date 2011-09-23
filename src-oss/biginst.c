37b474991acd8ef0151c98dc608cf6cab5728211
Ken McDonell <kenj@internode.on.net>
2011-09-24 07:05:52 +1000

    489 - additional test coverage for pmdaCacheStoreInst()

--- src-oss/biginst.c
+++ src-oss/biginst.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2011 Ken McDonell.  All Rights Reserved.
+ *
+ * Exercise pmdaCacheStoreInst() in libpcp_pmda
+ */
+
 #include <pcp/pmapi.h>
 #include <pcp/impl.h>
 #include <pcp/pmda.h>
@@ -5,7 +11,7 @@
 static int histo[128];
 
 int
-main()
+main(int argc, char **argv)
 {
     int		key[3];
     int		inst;
@@ -17,8 +23,52 @@ main()
     int		c;
     int		n = 0;
     char	name[40];
+    int		sts;
+    int		errflag = 0;
+    int		kflag = 0;
+    char	*usage = "[-D debug] [-k]";
+
+    __pmSetProgname(argv[0]);
+
+    while ((c = getopt(argc, argv, "D:k")) != EOF) {
+	switch (c) {
+
+	case 'D':	/* debug flag */
+	    sts = __pmParseDebug(optarg);
+	    if (sts < 0) {
+		fprintf(stderr, "%s: unrecognized debug flag specification (%s)\n",
+		    pmProgname, optarg);
+		errflag++;
+	    }
+	    else
+		pmDebug |= sts;
+	    break;
+
+	case 'k':	/* use key[], default is to use name[] */
+	    kflag = 1;
+	    break;
+
+	case '?':
+	default:
+	    errflag++;
+	    break;
+	}
+    }
+
+    if (errflag || optind != argc) {
+	fprintf(stderr, "Usage: %s %s\n", pmProgname, usage);
+	exit(1);
+    }
 
     indom = pmInDom_build(42, 42);
+
+    /*
+     * pattern of keys here is
+     * - 32, 64 or 96 bits long
+     * - features grouping and runs of values, as might be found in
+     *   multi-dimensional indexes that need to be mapped to a  value
+     *   no larger than 2^31 - 1 for an internal instance identifier
+     */
 
     for (i = 0; i < 255; i++) {
 	for (j = 1; j < 255; ) {
@@ -50,12 +100,22 @@ main()
 			break;
 		}
 
-		inst = pmdaCacheStoreInst(indom, PMDA_CACHE_ADD, name, k+1, key, NULL);
+		if (kflag)
+		    inst = pmdaCacheStoreInst(indom, PMDA_CACHE_ADD, name, k+1, key, NULL);
+		else
+		    inst = pmdaCacheStoreInst(indom, PMDA_CACHE_ADD, name, 0, NULL, NULL);
 		if (inst < 0) {
 		    printf("pmdaCacheStoreInst failed: %s\n", pmErrStr(inst));
 		    exit(1);
 		}
-		printf("%d <- %s\n", inst, name);
+		if (kflag) {
+		    printf("%d <-", inst);
+		    for (c = 0; c <= k; c++)
+			printf(" %d", key[k]);
+		    putchar('\n');
+		}
+		else
+		    printf("%d <- %s\n", inst, name);
 		histo[(int)(inst/(0x7fffffff/128))]++;
 		n++;
 	    }
@@ -65,8 +125,6 @@ main()
 		j = 2*j + 1;
 	}
     }
-
-    pmdaCacheStoreInst(PM_INDOM_NULL, 0, NULL, 0, NULL, NULL);
 
     for (s = 1; s <= 64; s *= 2) {
 	printf("\nInstances distribution across %d bins\n", 128/s);
