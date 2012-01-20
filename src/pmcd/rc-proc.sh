5785e65169e617647fff735122d5e1e62a1a4e77
Ken McDonell <kenj@internode.on.net>
2012-01-20 21:34:38 +1100

    FreeBSD PMDA - newie
    A basic kernel PMDA for FreeBSD.  There are enough metrics here to allow
    pmstat to run, the pmchart Overview chart works, and most of the "sanity"
    group of QA tests pass.
    
    Other metrics could easily be added using the sysctl() or /dev/mem
    reader methods used so far.
    
    All the code here has been developed from scratch, although the /dev/kmem
    reader parts rely on recalling fragments from user space code I wrote
    for Edition 7 Unix some 34 years ago! ... 8^)>

--- src/pmcd/rc-proc.sh
+++ src/pmcd/rc-proc.sh
@@ -124,7 +124,7 @@ is_chkconfig_on()
     _cmds_exist $_flag
     $_have_runlevel && _rl=`runlevel | $PCP_AWK_PROG '{print $2}'`
 
-    if [ "$PCP_PLATFORM" = mingw ]
+    if [ "$PCP_PLATFORM" = mingw -o "$PCP_PLATFORM" = "freebsd" ]
     then
 	# no chkconfig, just do it
 	#
@@ -211,7 +211,7 @@ chkconfig_on()
     _cmds_exist $_flag
     $_have_flag || return 1 # fail
 
-    if [ "$PCP_PLATFORM" = mingw ]
+    if [ "$PCP_PLATFORM" = mingw -o "$PCP_PLATFORM" = "freebsd" ]
     then
 	# no chkconfig, just pretend
 	#
@@ -274,7 +274,7 @@ chkconfig_off()
     _cmds_exist $_flag
     $_have_flag || return 1 # fail
 
-    if [ "$PCP_PLATFORM" = mingw ]
+    if [ "$PCP_PLATFORM" = mingw -o "$PCP_PLATFORM" = "freebsd" ]
     then
 	# no chkconfig, just pretend
 	#
@@ -315,7 +315,7 @@ chkconfig_on_msg()
     _cmds_exist $_flag
     $_have_flag || return 1 # fail
 
-    if [ "$PCP_PLATFORM" = mingw ]
+    if [ "$PCP_PLATFORM" = mingw -o "$PCP_PLATFORM" = "freebsd" ]
     then
 	# no chkconfig, just pretend
 	#
