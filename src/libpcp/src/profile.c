470e92bbd7069b615ee057358bb8885acc1f0374
Ken McDonell <kenj@internode.on.net>
2012-01-21 21:05:29 +1100

    libpcp/profile.c - deadcode removal Original Coverity case #27. http://people.redhat.com/mgoodwin/pcp-cov/1/27profile.c.html#error

--- src/libpcp/src/profile.c
+++ src/libpcp/src/profile.c
@@ -206,7 +206,7 @@ pmAddProfile(pmInDom indom, int instlist_len, int instlist[])
 	return sts;
 
     ctxp = __pmHandleToPtr(sts);
-    if (indom == PM_INDOM_NULL && (instlist == NULL || instlist_len == 0)) {
+    if (indom == PM_INDOM_NULL && instlist_len == 0) {
 	_setGlobalState(ctxp, PM_PROFILE_INCLUDE);
 	goto SUCCESS;
     }
