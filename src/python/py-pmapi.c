77b5aef0eb6c347b185b62ea0b18d2c4809435f9
Stan Cox <scox@redhat.com>
2012-04-09 17:35:16 -0400

    Add more pm api functionality.
    * py-pmapi.c (pypmIdStr): New.
    (pypmFreeResult): New.
    (pypmErrStr): New.
    (pypmAddProfile): New.
    (pypmDelProfile): New.
    (pypmLookupInDom): New.

--- src/python/py-pmapi.c
+++ src/python/py-pmapi.c
@@ -20,6 +20,11 @@ pypmNewContext(PyObject* self)
   local[MAXHOSTNAMELEN-1] = '\0';
   host = local;
   sts = pmNewContext(type, host);
+  if (sts < 0)
+    {
+      PyErr_SetString(PyExc_RuntimeError, "PCP is not running");
+      return 0;
+    }
   return Py_BuildValue("i", sts);
 }
 
@@ -78,6 +83,38 @@ static PyObject* pypmLookupName(PyObject* self, PyObject* args)
 	
 	return pypmidlist;
       }
+  }
+}
+
+// Python Usage: pcp.pmIdStr (ID) OR ((ID1, ID2..))
+// Description: Equivalent to pmIdStr.  Arg can be an id or tuple of ids 
+// as returned by pmLookupName
+// Returns: tuple of name string descriptions
+
+static PyObject* pypmIdStr(PyObject* self, PyObject* args)
+{
+  PyObject *pmids = PyTuple_GetItem(args, 0);
+  int pm_size;
+  if (python_type (pmids) == LONG)
+    pm_size = 1;
+  else if (python_type (pmids) == TUPLE)
+    pm_size = PyTuple_Size (pmids);
+
+  {
+    long pmid[pm_size];
+    int i;
+
+    if (python_type (pmids) == LONG)
+      pmid[0] = PyLong_AsLong (pmids);
+    else if (python_type (pmids) == TUPLE)
+      for (i = 0; i < pm_size; i++)
+	pmid[i] = PyLong_AsLong (PyTuple_GetItem(pmids, i));
+    
+    PyObject *pypmidstringlist = PyTuple_New (pm_size);
+    
+    for (i = 0; i < pm_size; i++)
+      PyTuple_SetItem (pypmidstringlist, i, PyString_FromString (pmIDStr (pmid[i])));
+    return pypmidstringlist;
   }
 }
 
@@ -212,6 +249,157 @@ pypmExtractValue(PyObject* self, PyObject* args)
   return NULL;
 }
 
+// Python Usage: pcp.pmFreeResult (R) OR ((R1, R2..))
+// Description: Equivalent to pmFreeResult.
+
+static PyObject* pypmFreeResult(PyObject* self, PyObject* args)
+{
+  PyObject *pmresults = PyTuple_GetItem(args, 0);
+  int pm_size;
+  if (python_type (pmresults) == LONG)
+    pm_size = 1;
+  else if (python_type (pmresults) == TUPLE)
+    pm_size = PyTuple_Size (pmresults);
+
+  {
+    int i;
+
+    if (python_type (pmresults) == LONG)
+      pmFreeResult ((pmResult*)PyLong_AsLong (pmresults));
+    else if (python_type (pmresults) == TUPLE)
+      for (i = 0; i < pm_size; i++)
+	pmFreeResult ((pmResult*)PyLong_AsLong (PyTuple_GetItem(pmresults, i)));
+
+    return NULL;
+  }
+}
+
+
+// Python Usage: pcp.pmIdStr (ID)
+// Description: Equivalent to pmErrStr.  Arg is an error code
+
+static PyObject* 
+pypmErrStr(PyObject* self, PyObject* args)
+{
+  return PyString_FromString (pmErrStr(PyLong_AsLong(PyTuple_GetItem(args, 0))));
+}
+
+// Python Usage: pcp.pmAddProfile (D, I) OR ((D1, D2..), I)
+// Description: Equivalent to pmAddProfile.  Arg1 can be a descriptor or tuple
+// of descriptors as returned by pcp.pmLookupDesc.  Arg2 is an instance as
+// returned by pcp.pmLookupInDom
+
+static PyObject*
+pypmAddProfile(PyObject* self, PyObject* args)
+{
+  pmDesc *desc = (pmDesc*)PyTuple_GetItem(args, 0);
+  PyObject *inst = (PyObject*)PyTuple_GetItem(args, 1);
+  int i;
+  int pm_size;
+
+  if (python_type (inst) == TUPLE)
+    {
+      pm_size = PyTuple_Size (inst);
+
+      for (i = 0; i < pm_size; i++)
+	pmAddProfile (desc->indom, pm_size, (int*)PyLong_AsLong (PyTuple_GetItem(inst, i)));
+    }
+  
+  return NULL;
+}
+
+// Python Usage: pcp.pmDelProfile (D, I) OR ((D1, D2..), I)
+// Description: Equivalent to pmDelProfile.  Arg1 can be a descriptor or tuple
+// of descriptors as returned by pcp.pmLookupDesc.  Arg2 is an instance as
+// returned by pcp.pmLookupInDom
+
+static PyObject*
+pypmDelProfile(PyObject* self, PyObject* args)
+{
+  pmDesc *desc = (pmDesc*)PyTuple_GetItem(args, 0);
+  PyObject *inst = (PyObject*)PyTuple_GetItem(args, 1);
+  int i;
+  int pm_size;
+
+  if (python_type (inst) == TUPLE)
+    {
+      pm_size = PyTuple_Size (inst);
+
+      for (i = 0; i < pm_size; i++)
+	pmDelProfile (desc->indom, pm_size, (int*)PyLong_AsLong (PyTuple_GetItem(inst, i)));
+    }
+  
+  return NULL;
+}
+
+// Python Usage: pcp.pmLookupInDom (D, I) OR ((D1, D2..), I)
+// Description: Equivalent to pmAddProfile.  Arg1 is a descriptor.
+// Arg2 is a string instance description
+// Returned is the instance id
+
+static PyObject*
+pypmLookupInDom(PyObject* self, PyObject* args)
+{
+  pmDesc *desc = (pmDesc*)PyTuple_GetItem(args, 0);
+  PyObject *inst = (PyObject*)PyTuple_GetItem(args, 1);
+
+  if (python_type (inst) == TUPLE)
+    {
+      int this_inst = pmLookupInDom (desc->indom, PyString_AsString(inst));
+      return Py_BuildValue("i", this_inst);
+    }
+  
+  return NULL;
+}
+
+static PyObject*
+pypmParseInterval(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
+static PyObject*
+pypmConvScale(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
+static PyObject*
+pypmLoadNameSpace(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
+static PyObject*
+pypmGetArchiveLabel(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
+static PyObject*
+pypmNewContextZone(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
+static PyObject*
+pypmNewZone(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
+static PyObject*
+pypmSetMode(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
+static PyObject*
+pypmCtime(PyObject* self, PyObject* args)
+{
+  return NULL;
+}
+
 static char pcp_docs[] =
     "pcp( ): PCP ABI extension module\n";
 
@@ -221,6 +409,21 @@ static PyMethodDef pcp_funcs[] = {
     {"pmLookupDesc", (PyCFunction)pypmLookupDesc, METH_VARARGS, pcp_docs},
     {"pmFetch", (PyCFunction)pypmFetch, METH_VARARGS, pcp_docs},
     {"pmExtractValue", (PyCFunction)pypmExtractValue, METH_VARARGS, pcp_docs},
+    {"pmErrStr", (PyCFunction)pypmErrStr, METH_VARARGS, pcp_docs},
+    {"pmIdStr", (PyCFunction)pypmIdStr, METH_VARARGS, pcp_docs},
+    {"pmFreeResult", (PyCFunction)pypmFreeResult, METH_VARARGS, pcp_docs},
+    {"pmDelProfile", (PyCFunction)pypmDelProfile, METH_VARARGS, pcp_docs},
+    {"pmAddProfile", (PyCFunction)pypmAddProfile, METH_VARARGS, pcp_docs},
+    {"pmLookupInDom", (PyCFunction)pypmLookupInDom, METH_VARARGS, pcp_docs},
+    {"pmConvScale", (PyCFunction)pypmConvScale, METH_VARARGS, pcp_docs},
+    {"pmParseInterval", (PyCFunction)pypmParseInterval, METH_VARARGS, pcp_docs},
+    {"pmLoadNameSpace", (PyCFunction)pypmLoadNameSpace, METH_VARARGS, pcp_docs},
+    {"pmGetArchiveLabel", (PyCFunction)pypmGetArchiveLabel, METH_VARARGS, pcp_docs},
+    {"pmNewContextZone", (PyCFunction)pypmNewContextZone, METH_VARARGS, pcp_docs},
+    {"pmNewZone", (PyCFunction)pypmNewZone, METH_VARARGS, pcp_docs},
+    {"pmSetMode", (PyCFunction)pypmSetMode, METH_VARARGS, pcp_docs},
+    {"pmCtime", (PyCFunction)pypmCtime, METH_VARARGS, pcp_docs},
+
     {NULL}
 };
 
