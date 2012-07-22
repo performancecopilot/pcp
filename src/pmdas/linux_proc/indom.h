b28bb3bd72ef9d6bd538d991efec34f65374b7df
Ken McDonell <kenj@internode.on.net>
2012-07-22 20:03:14 +1000

    proc PMDA - missed file from last commit

---
+++ src/pmdas/linux_proc/indom.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2005,2007-2008 Silicon Graphics, Inc.  All Rights Reserved.
+ * Copyright (c) 2010 Aconex.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef _INDOM_H
+#define _INDOM_H
+
+/*
+ * indom cluster numbers ... to manage the indom migration after the
+ * linux -> linux + proc PMDAs split, these need to match the enum
+ * assigned values for *_INDOM from the linux PMDA.
+ */
+
+#define CPU_INDOM		 0 /* 0 - percpu */
+#define PROC_INDOM		 9 /* - processes */
+#define CGROUP_SUBSYS_INDOM	20 /* - control group subsystems */
+#define CGROUP_MOUNTS_INDOM	21 /* - control group mounts */
+
+#define MIN_INDOM  0		/* first cluster number we use here */
+#define NUM_INDOMS 22		/* one more than highest cluster number we use here */
+
+#if 0
+enum {
+	DISK_INDOM,		/* 1 - disks */
+	LOADAVG_INDOM,		/* 2 - 1, 5, 15 minute load averages */
+	NET_DEV_INDOM,		/* 3 - network interfaces */
+	PROC_INTERRUPTS_INDOM,	/* 4 - interrupt lines */
+	FILESYS_INDOM,		/* 5 - mounted bdev filesystems */
+	SWAPDEV_INDOM,		/* 6 - swap devices */
+	NFS_INDOM,		/* 7 - nfs operations */
+	NFS3_INDOM,		/* 8 - nfs v3 operations */
+	PARTITIONS_INDOM, 	/* 10 - disk partitions */
+	SCSI_INDOM,		/* 11 - scsi devices */
+	SLAB_INDOM,		/* 12 - kernel slabs */
+	IB_INDOM,		/* 13 - deprecated: do not re-use */
+	NFS4_CLI_INDOM,		/* 14 - nfs v4 client operations */
+	NFS4_SVR_INDOM,		/* 15 - nfs n4 server operations */
+	QUOTA_PRJ_INDOM,	/* 16 - project quota */
+	NET_INET_INDOM,		/* 17 - inet addresses */
+	TMPFS_INDOM,		/* 18 - tmpfs mounts */
+	NODE_INDOM,		/* 19 - NUMA nodes */
+};
+#endif
+
+#define INDOM(x) (indomtab[x].it_indom)
+extern pmdaIndom indomtab[];
+
+#endif /* _INDOM_H */
