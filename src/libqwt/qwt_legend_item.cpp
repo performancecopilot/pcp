20399ce63c5255043c676de4d5f0ec9dd6cc5e86
Nathan Scott <nathans@redhat.com>
2012-09-06 17:26:28 +1000

    Invert the selection sense for legend items as we did with Qwt5

--- src/libqwt/qwt_legend_item.cpp
+++ src/libqwt/qwt_legend_item.cpp
@@ -275,7 +275,7 @@ void QwtLegendItem::paintEvent( QPaintEvent *e )
     QPainter painter( this );
     painter.setClipRegion( e->region() );
 
-    if ( d_data->isDown )
+    if ( d_data->isDown == false )
     {
         qDrawWinButton( &painter, 0, 0, width(), height(),
             palette(), true );
