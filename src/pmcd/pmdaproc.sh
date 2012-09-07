performancecopilot/pcp

commit 2cc267d99144cc47c41231685fc6cca361cae969

pmcd.conf - pathname silliness
As a result of the long and convoluted set of changes from early Irix
versions to the present day, we were not being consistent about how
pathnames for executables and DSOs are specified in pmcd.conf.

It all broke when $PCP_DIR was set in the aftermath of trying to run
a non-default installation after a configure --prefix=... build.

Now we're using absolute pathnames in the initial pmcd.conf with the
default PMDAs, in pmcd's parsing of pmcd.conf and in all the PMDA
Install procedures.

src/pmcd/pmdaproc.sh
@@ -809,25 +809,15 @@ END		{ if (warn) printf "%d warnings, ",warn
 		}'
 }
 
-__strip_pcp_dir()
-{
-    if [ "X$PCP_DIR" = X ]
-    then
-	echo "$1"
-    else
-	echo "$1" | sed -e "s,^$PCP_DIR,,"
-    fi
-}
-
 _setup()
 {
     # some more configuration controls
     pmns_name=${pmns_name-$iam}
     pmda_name=pmda$iam
     dso_name="${PCP_PMDAS_DIR}/${iam}/pmda_${iam}.${dso_suffix}"
-    dso_name=`__strip_pcp_dir "$dso_name"`
+    dso_name="$dso_name"
     dso_entry=${iam}_init
-    pmda_dir=`__strip_pcp_dir "${PCP_PMDAS_DIR}/${iam}"`
+    pmda_dir="${PCP_PMDAS_DIR}/${iam}"
 
     _check_userroot
     _check_directory
@@ -837,7 +827,7 @@ _setup()
     if $perl_opt
     then
 	perl_name="${PCP_PMDAS_DIR}/${iam}/pmda${iam}.pl"
-	perl_name=`__strip_pcp_dir "$perl_name"`
+	perl_name="$perl_name"
 	perl_pmns="${PCP_PMDAS_DIR}/${iam}/pmns.perl"
 	perl_dom="${PCP_PMDAS_DIR}/${iam}/domain.h.perl"
 	perl -e 'use PCP::PMDA' 2>/dev/null
@@ -1297,8 +1287,13 @@ _check_userroot()
 {
     if [ "$uid" -ne 0 ]
     then
-	echo "Error: You must be root (uid 0) to update the PCP collector configuration."
-	exit 1
+	if [ -n "$PCP_DIR" ]
+	then
+	    : running in a non-default installation, do not need to be root
+	else
+	    echo "Error: You must be root (uid 0) to update the PCP collector configuration."
+	    exit 1
+	fi
     fi
 }
 

src/pmcd/src/config.c
@@ -109,22 +109,6 @@ CopyToken(void)
     return copy;
 }
 
-/* Return a strdup-ed copy of the current token, plus an optional prefix. */
-static char*
-CopyPathToken(const char *prefix)
-{
-    int		len = (int)(tokenend - token);
-    int		plen = prefix ? strlen(prefix) : 0;
-    char	*copy = (char *)malloc(plen + len + 1);
-    if (copy != NULL) {
-	if (plen > 0)
-	    strncpy(copy, prefix, plen);
-	strncpy(copy + plen, token, len);
-	copy[plen + len] = '\0';
-    }
-    return copy;
-}
-
 /* Get the next line from the input stream into linebuf. */
 
 static void
@@ -525,7 +509,7 @@ BuildArgv(void)
 	if (result != NULL) {
 	    if (*token != '/')
 		result[nArgs] = CopyToken();
-	    else if ((result[nArgs] = CopyPathToken(getenv("PCP_DIR"))))
+	    else if ((result[nArgs] = CopyToken()) != NULL)
 		__pmNativePath(result[nArgs]);
 	}
 	if (result == NULL || result[nArgs] == NULL) {
@@ -655,7 +639,7 @@ ParseDso(char *pmDomainLabel, int pmDomainId)
 	return -1;
     }
 
-    if ((pathName = CopyPathToken(getenv("PCP_DIR"))) == NULL) {
+    if ((pathName = CopyToken()) == NULL) {
 	fprintf(stderr, "pmcd config[line %d]: Error: couldn't copy DSO pathname\n",
 			nLines);
 	__pmNoMem("pmcd config", tokenend - token + 1, PM_FATAL_ERR);

commit 24155dc5ec7307cec4d0f2db452c6937a4c600fe

Allow pmda Install scripts to specify own dso name
Fix an inadvertent regression from recent a commit
(2cc267d99144cc47c41231685fc6cca361cae969) which
prevents the pmdabigun Install script from working
anymore, since its using a not-quite-default name
for the dso (and could no longer override).

src/pmcd/pmdaproc.sh
@@ -820,8 +820,8 @@ _setup()
     # some more configuration controls
     pmns_name=${pmns_name-$iam}
     pmda_name=pmda$iam
-    dso_name="${PCP_PMDAS_DIR}/${iam}/pmda_${iam}.${dso_suffix}"
-    dso_name="$dso_name"
+    pmda_dso_name="${PCP_PMDAS_DIR}/${iam}/pmda_${iam}.${dso_suffix}"
+    dso_name="${dso_name-$pmda_dso_name}"
     dso_entry=${iam}_init
     pmda_dir="${PCP_PMDAS_DIR}/${iam}"
 