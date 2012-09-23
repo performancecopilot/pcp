274af1071f92343c67ab8edae2e9a9f260b4cf8e
Nathan Scott <nathans@redhat.com>
2012-09-23 15:25:23 +1000

    Make use of additions to import library in perl wrapper
    Since we have just recently added error codes into the perl
    PCP::LogImport module, now is a good time to add in these
    other two (which were mainly added for Python, but we may as
    well be consistent).
    
    Perl interfaces for pmiID() and pmiInDom() are now available.

--- src/perl/LogImport/LogImport.pm
+++ src/perl/LogImport/LogImport.pm
@@ -13,7 +13,7 @@ our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
     pmiStart pmiUseContext pmiEnd pmiSetHostname pmiSetTimezone
     pmiAddMetric pmiAddInstance pmiPutValue pmiGetHandle pmiPutValueHandle
     pmiWrite
-    pmiDump pmiErrStr pmiUnits
+    pmiDump pmiErrStr pmiUnits pmiID pmiInDom
     pmid_build pmInDom_build
     PM_ID_NULL PM_INDOM_NULL PM_IN_NULL
     PM_SPACE_BYTE PM_SPACE_KBYTE PM_SPACE_MBYTE PM_SPACE_GBYTE PM_SPACE_TBYTE
