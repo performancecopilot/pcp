76becf8f65e0e02ab22de571c1b4126c754102d0
Nathan Scott <nathans@redhat.com>
2013-03-06 19:29:57 +1100

    Ensure sys/stat.h is presented early enough in pmdalogger builds
    On Mac OS X a header file warning was observed:
     In file included from event.c:18:
    event.h:29: error: field 'pathstat' has incomplete type
    
    because the sys/stat.h #include comes after the event.h #include.
    Resolveed by moving the stat.h include before event.h in event.c.

--- src/pmdas/logger/event.c
+++ src/pmdas/logger/event.c
@@ -18,9 +18,6 @@
 #include "event.h"
 #include "pmda.h"
 #include "util.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <ctype.h>
 #ifdef HAVE_REGEX_H
 #include <regex.h>
