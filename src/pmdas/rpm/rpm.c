662ee2e1dbff6146b9fcf4fa16a710fd7306ae10
Stan Cox <scox@redhat.com>
2013-12-04 17:46:21 -0500

    For rpm pmda: simplify caching, use mutexes, improve metric setup.
    Remove the separate hash table in favor of pmdaCacheLookupName.  Switched to
    using mutexes since there is only one reader.  Improved the initialization of
    metrics.

--- src/pmdas/rpm/rpm.c
+++ src/pmdas/rpm/rpm.c
@@ -15,292 +15,101 @@
  */
 
 #include <sys/stat.h>
-#include <assert.h>
-#include <stdio.h>
 #include <pthread.h>
 #include <search.h>
-
 #include <sys/inotify.h>
-
-
 #include <rpm/rpmlib.h>
 #include <rpm/header.h>
 #include <rpm/rpmts.h>
 #include <rpm/rpmdb.h>
-
 #include <pcp/pmapi.h>
 #include <pcp/impl.h>
 #include <pcp/pmda.h>
-
 #include "domain.h"
+#include "timer.h"
 
 // List of metrics corresponding to rpm --querytags
 
 // corresponds to metrictab to give the tag rpm uses to fetch the given
 // value
 
+#define REFRESH_COUNT_IDX 0
+#define REFRESH_TIME_USER_IDX 1
+#define REFRESH_TIME_KERNEL_IDX 2
+#define REFRESH_TIME_ELAPSED_IDX 3
+#define RPMTAG_ARCH_IDX 0
+#define RPMTAG_BUILDHOST_IDX 1
+#define RPMTAG_BUILDTIME_IDX 2
+#define RPMTAG_DESCRIPTION_IDX 3
+#define RPMTAG_EPOCH_IDX 4
+#define RPMTAG_GROUP_IDX 5
+#define RPMTAG_INSTALLTIME_IDX 6
+#define RPMTAG_LICENSE_IDX 7
+#define RPMTAG_NAME_IDX 8
+#define RPMTAG_PACKAGER_IDX 9
+#define RPMTAG_RELEASE_IDX 10
+#define RPMTAG_SIZE_IDX 11
+#define RPMTAG_SOURCERPM_IDX 12
+#define RPMTAG_SUMMARY_IDX 13
+#define RPMTAG_URL_IDX 14
+#define RPMTAG_VENDOR_IDX 15
+#define RPMTAG_VERSION_IDX 16
 static struct metrics {
     int tag;
     int type;
 } metrics[] = {
-    /*
-     * arch 
-     */  {
-    RPMTAG_ARCH, 0},
-	/*
-	 * basenames 
-	 */  {
-    RPMTAG_BASENAMES, 0},
-	/*
-	 * buildtime 
-	 */  {
-    RPMTAG_BUILDTIME, 0},
-	/*
-	 * description 
-	 */  {
-    RPMTAG_DESCRIPTION, 0},
-	/*
-	 * dirnames 
-	 */  {
-    RPMTAG_DIRNAMES, 0},
-	/*
-	 * distribution 
-	 */  {
-    RPMTAG_DISTRIBUTION, 0},
-	/*
-	 * evr 
-	 */  {
-    RPMTAG_EVR, 0},
-	/*
-	 * file.class 
-	 */  {
-    RPMTAG_FILECLASS, 0},
-	/*
-	 * file.linktos 
-	 */  {
-    RPMTAG_FILELINKTOS, 0},
-	/*
-	 * file.md5s 
-	 */  {
-    RPMTAG_FILEMD5S, 0},
-	/*
-	 * file.modes 
-	 */  {
-    RPMTAG_FILEMODES, 0},
-	/*
-	 * file.names 
-	 */  {
-    RPMTAG_FILENAMES, 0},
-	/*
-	 * file.require 
-	 */  {
-    RPMTAG_FILEREQUIRE, 0},
-	/*
-	 * file.sizes 
-	 */  {
-    RPMTAG_FILESIZES, 0},
-	/*
-	 * group 
-	 */  {
-    RPMTAG_GROUP, 0},
-	/*
-	 * license 
-	 */  {
-    RPMTAG_LICENSE, 0},
-	/*
-	 * name 
-	 */  {
-    RPMTAG_NAME, 0},
-	/*
-	 * obsoletes 
-	 */  {
-    RPMTAG_OBSOLETES, 0},
-	/*
-	 * packageid 
-	 */  {
-    RPMTAG_PKGID, 0},
-	/*
-	 * platform 
-	 */  {
-    RPMTAG_PLATFORM, 0},
-	/*
-	 * provideversion 
-	 */  {
-    RPMTAG_PROVIDEVERSION, 0},
-	/*
-	 * provides 
-	 */  {
-    RPMTAG_PROVIDES, 0},
-	/*
-	 * release 
-	 */  {
-    RPMTAG_RELEASE, 0},
-	/*
-	 * requires 
-	 */  {
-    RPMTAG_REQUIRES, 0},
-	/*
-	 * rpmversion 
-	 */  {
-    RPMTAG_RPMVERSION, 0},
-	/*
-	 * size 
-	 */  {
-    RPMTAG_SIZE, 0},
-	/*
-	 * sourcerpm 
-	 */  {
-    RPMTAG_SOURCERPM, 0},
-	/*
-	 * summary 
-	 */  {
-    RPMTAG_SUMMARY, 0},
-	/*
-	 * url 
-	 */  {
-    RPMTAG_URL, 0},
-	/*
-	 * version 
-	 */  {
-    RPMTAG_VERSION, 0}
+    [RPMTAG_ARCH_IDX] = {RPMTAG_ARCH, 0},
+    [RPMTAG_BUILDHOST_IDX] = {RPMTAG_BUILDHOST, 0},
+    [RPMTAG_BUILDTIME_IDX] = {RPMTAG_BUILDTIME, 0},
+    [RPMTAG_DESCRIPTION_IDX] = {RPMTAG_DESCRIPTION, 0},
+    [RPMTAG_EPOCH_IDX] = {RPMTAG_EPOCH, 0},
+    [RPMTAG_GROUP_IDX] = {RPMTAG_GROUP, 0},
+    [RPMTAG_INSTALLTIME_IDX] = {RPMTAG_INSTALLTIME, 0},
+    [RPMTAG_LICENSE_IDX] = {RPMTAG_LICENSE, 0},
+    [RPMTAG_NAME_IDX] = {RPMTAG_NAME, 0},
+    [RPMTAG_PACKAGER_IDX] = {RPMTAG_PACKAGER, 0},
+    [RPMTAG_RELEASE_IDX] = {RPMTAG_RELEASE, 0},
+    [RPMTAG_SIZE_IDX] = {RPMTAG_SIZE, 0},
+    [RPMTAG_SOURCERPM_IDX] = {RPMTAG_SOURCERPM, 0},
+    [RPMTAG_SUMMARY_IDX] = {RPMTAG_SUMMARY, 0},
+    [RPMTAG_URL_IDX] = {RPMTAG_URL, 0},
+    [RPMTAG_VENDOR_IDX] = {RPMTAG_VENDOR, 0},
+    [RPMTAG_VERSION_IDX] = {RPMTAG_VERSION, 0}
 };
 
 
 #define METRICTAB_ENTRY(M,N,T) (&metrics[N].tag), { PMDA_PMID (M, N), T, 0, PM_SEM_INSTANT, PMDA_PMUNITS(0,0,0,0,0,0) },
 
 static pmdaMetric metrictab[] = {
-    /*
-     * refresh.count 
-     */
     {NULL,
-     {PMDA_PMID(0, 0), PM_TYPE_U64, PM_INDOM_NULL, PM_SEM_INSTANT,
-      PMDA_PMUNITS(0, 0, 0, 0, 0, 0)},},
-    /*
-     * refresh.time 
-     */
+     {PMDA_PMID(0, REFRESH_COUNT_IDX), PM_TYPE_U64, PM_INDOM_NULL, PM_SEM_COUNTER,
+      PMDA_PMUNITS(0, 0, 1, 0, 0, PM_COUNT_ONE)},},
     {NULL,
-     {PMDA_PMID(0, 1), PM_TYPE_DOUBLE, PM_INDOM_NULL, PM_SEM_INSTANT,
-      PMDA_PMUNITS(0, 0, 0, 0, 0, 0)},},
-    /*
-     * arch 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * basenames 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * buildtime 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_U32)},
-    /*
-     * description 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * dirnames 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * distribution 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * evr 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * file.class 
-     */
-    {METRICTAB_ENTRY(2, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * file.linktos 
-     */
-    {METRICTAB_ENTRY(2, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * file.md5s 
-     */
-    {METRICTAB_ENTRY(2, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * file.modes 
-     */
-    {METRICTAB_ENTRY(2, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * file.names 
-     */
-    {METRICTAB_ENTRY(2, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * file.require 
-     */
-    {METRICTAB_ENTRY(2, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * file.sizes 
-     */
-    {METRICTAB_ENTRY(2, __COUNTER__, PM_TYPE_U32)},
-    /*
-     * group 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * license 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * name 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * obsoletes 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * packageid 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * platform 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * provideversion 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * provides 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * release 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * requires 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * rpmversion 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * size 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_U32)},
-    /*
-     * sourcerpm 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * summary 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * url 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)},
-    /*
-     * version 
-     */
-    {METRICTAB_ENTRY(1, __COUNTER__, PM_TYPE_STRING)}
+     {PMDA_PMID(0, REFRESH_TIME_USER_IDX), PM_TYPE_DOUBLE, PM_INDOM_NULL, PM_SEM_COUNTER,
+      PMDA_PMUNITS(0, 1, 0, 0, PM_TIME_SEC, 0)},},
+    {NULL,
+     {PMDA_PMID(0, REFRESH_TIME_KERNEL_IDX), PM_TYPE_DOUBLE, PM_INDOM_NULL, PM_SEM_COUNTER,
+      PMDA_PMUNITS(0, 1, 0, 0, PM_TIME_SEC, 0)},},
+    {NULL,
+     {PMDA_PMID(0, REFRESH_TIME_ELAPSED_IDX), PM_TYPE_DOUBLE, PM_INDOM_NULL, PM_SEM_COUNTER,
+      PMDA_PMUNITS(0, 1, 0, 0, PM_TIME_SEC, 0)},},
+    {METRICTAB_ENTRY (1, RPMTAG_ARCH_IDX, PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_BUILDHOST_IDX, PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_BUILDTIME_IDX, PM_TYPE_U32)},
+    {METRICTAB_ENTRY (1, RPMTAG_DESCRIPTION_IDX, PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_EPOCH_IDX, PM_TYPE_U32)},
+    {METRICTAB_ENTRY (1, RPMTAG_GROUP_IDX, PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_INSTALLTIME_IDX, PM_TYPE_U32)},
+    {METRICTAB_ENTRY (1, RPMTAG_LICENSE_IDX, PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_NAME_IDX, PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_PACKAGER_IDX, PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_RELEASE_IDX , PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_SIZE_IDX , PM_TYPE_U32)},
+    {METRICTAB_ENTRY (1, RPMTAG_SOURCERPM_IDX , PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_SUMMARY_IDX , PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_URL_IDX , PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_VENDOR_IDX , PM_TYPE_STRING)},
+    {METRICTAB_ENTRY (1, RPMTAG_VERSION_IDX , PM_TYPE_STRING)},
 };
 
 // RPM cache information.  A linked list of pcp pmAtonValue structures.
@@ -319,14 +128,9 @@ cache_entry *current_cache_entry;
 static pthread_t indom_thread;
 // To notice when the rpm database changes
 static pthread_t inotify_thread;
-static long numrefresh = 0;
+static unsigned long long numrefresh = 0;
 
-// hash table info used by hsearch_r
-
-#define HASH_HBOUND 5000
-struct hsearch_data htab;
-
-pthread_rwlock_t indom_lock;
+pthread_mutex_t indom_mutex;
 
 // Load the instances dynamically
 pmdaIndom indomtab[] = {
@@ -351,33 +155,50 @@ static int
 rpm_fetchCallBack(pmdaMetric * mdesc, unsigned int inst,
 		  pmAtomValue * atom)
 {
-    int ret_type;
-    ENTRY e,
-    *ep;
+    int sts = PMDA_FETCH_STATIC;
     __pmID_int *idp = (__pmID_int *) & (mdesc->m_desc.pmid);
     pmAtomValue this_atom;
     int type;
     int tag;
+
     // Get the instance name for this instance
     char *rpm_inst_name;
-    int cacheidx = pmdaCacheLookup(*rpm_indom, inst, &rpm_inst_name, NULL);
+    cache_entry *this_cache_entry;
+
+    if (idp->cluster == 0) {
+	sts = PMDA_FETCH_STATIC;
+	pthread_mutex_lock(&indom_mutex);
+	switch (idp->item) {
+	case REFRESH_COUNT_IDX:		/* rpm.refresh.count */
+	    atom->ull = numrefresh;
+	    break;
+	case REFRESH_TIME_USER_IDX:	/* rpm.refresh.time.user */
+	    atom->d = get_user_timer();
+	    break;
+	case REFRESH_TIME_KERNEL_IDX:	/* rpm.refresh.time.kernel */
+	    atom->d = get_kernel_timer();
+	    break;
+	case REFRESH_TIME_ELAPSED_IDX:	/* rpm.refresh.time.elapsed */
+	    atom->d = get_elapsed_timer();
+	    break;
+	default:
+	    sts = PM_ERR_PMID;
+	    break;
+ 	}
+	pthread_mutex_unlock(&indom_mutex);
+	return sts;
+    }
+
+    pthread_mutex_lock(&indom_mutex);
+
+    int cacheidx = pmdaCacheLookup(*rpm_indom, inst, &rpm_inst_name, (void*)&this_cache_entry);
+
+    pthread_mutex_unlock(&indom_mutex);
+
     if (cacheidx < 0) {
 	__pmNotifyErr(LOG_ERR, "pmdaCacheLookup failed: inst=%d: %s", inst,
 		      pmErrStr(cacheidx));
 	return PM_ERR_INST;
-    }
-
-    if (idp->cluster == 0) {
-	if (idp->item == 0) {	/* rpm.refresh.count */
-	    atom->ll = numrefresh;
-	    return PMDA_FETCH_STATIC;
-//	} else if (idp->item == 1) { /* rpm.refresh.time */
-	    double usr,
-	     sys;
-	    __pmProcessRunTimes(&usr, &sys);
-	    atom->d = usr + sys;
-	    return PMDA_FETCH_STATIC;
-	}
     }
 
     type = ((struct metrics *) mdesc->m_user)->type;
@@ -389,48 +210,66 @@ rpm_fetchCallBack(pmdaMetric * mdesc, unsigned int inst,
 	if (metrics[metric_idx].tag == tag)
 	    break;
 
-    // search for this rpm
-    pthread_rwlock_rdlock(&indom_lock);
-    e.key = rpm_inst_name;
-    e.data = NULL;
-    if (hsearch_r(e, FIND, &ep, &htab) == 0)
-	return PM_ERR_INST;
-
-    // the hash table points to the metrics for this rpm
-    this_atom = ((cache_entry *) (ep->data))->entry[metric_idx];
+    this_atom = this_cache_entry->entry[metric_idx];
 
     switch (type) {
     case PM_TYPE_STRING:
-	atom->cp = this_atom.cp;
-	ret_type = PMDA_FETCH_DYNAMIC;
+	if (this_atom.cp)
+	    atom->cp = strdup(this_atom.cp);
+	else
+	    atom->cp = strdup("");
+	sts = PMDA_FETCH_DYNAMIC;
 	break;
     case PM_TYPE_32:
 	atom->l = this_atom.l;
-	ret_type = PMDA_FETCH_STATIC;
+	sts = PMDA_FETCH_STATIC;
 	break;
     case PM_TYPE_64:
 	atom->ll = this_atom.ll;
-	ret_type = PMDA_FETCH_STATIC;
+	sts = PMDA_FETCH_STATIC;
 	break;
+    default:
+	return PM_ERR_TYPE;
     }
 
-    pthread_rwlock_unlock(&indom_lock);
-
-    return ret_type;
+    return sts;
 }
 
 /*
- * Called once for each pmFetch(3) operation.  Join the indom loading thread here.
+  Sync up with the (initial) indom loading thread
  */
 
 static int
-rpm_fetch(int numpmid, pmID pmidlist[], pmResult ** resp, pmdaExt * pmda)
+notready(pmdaExt *pmda)
+ {
+     __pmSendError(pmda->e_outfd, FROM_ANON, PM_ERR_PMDANOTREADY);
+     pthread_join(indom_thread, NULL);
+     return PM_ERR_PMDAREADY;
+}
+
+/*
+  Called once for each pmFetch(3) operation
+*/
+static int
+rpm_fetch(int numpmid, pmID pmidlist[], pmResult **resp, pmdaExt *pmda)
 {
-    pthread_join(indom_thread, NULL);
+    if (numrefresh == 0)
+	return notready(pmda);
     return pmdaFetch(numpmid, pmidlist, resp, pmda);
 }
 
 
+/*
+  Called once for each pmGetInDom(3) operation
+*/
+static int
+rpm_instance(pmInDom id, int i, char *name, __pmInResult **in, pmdaExt *pmda)
+{
+    if (numrefresh == 0)
+	return notready(pmda);
+    return pmdaInstance(id, i, name, in, pmda);
+}
+ 
 /*
  * Load the rpm module names into the instance table
  */
@@ -442,21 +281,16 @@ rpm_update_cache(void *ptr)
     Header h;
     rpmdbMatchIterator mi;
     rpmtd td;
-    double usr1,
-     usr2,
-     sys1,
-     sys2;
+    int sts = 0;
 
-    numrefresh++;
 
-    __pmProcessRunTimes(&usr1, &sys1);
-    pthread_rwlock_wrlock(&indom_lock);
-
+    start_timing();
     td = rpmtdNew();
     ts = rpmtsCreate();
 
     rpmReadConfigFiles(NULL, NULL);
 
+    pthread_mutex_lock(&indom_mutex);
     cache_entry *this_cache_entry;
     // Is this an inotify rpm reload?
     if (!cache) {
@@ -477,13 +311,10 @@ rpm_update_cache(void *ptr)
 	for (i = 0; i < (sizeof(metrics) / sizeof(struct metrics)); i++) {
 	    headerGet(h, RPMTAG_NAME, td, HEADERGET_EXT);
 	    const char *rpmname = rpmtdGetString(td);
-	    ENTRY e,
-	    *ep;
-	    e.key = rpmname;
-	    e.data = 0;
+
 	    // If we already have this rpm then grab the next one
 	    // ?? we need to check the rpm version and reload if it is a newer rpm version
-	    if (hsearch_r(e, FIND, &ep, &htab) != 0)
+	    if (pmdaCacheLookupName (*rpm_indom, rpmname, NULL, NULL) > 0)
 		continue;
 
 	    headerGet(h, metrics[i].tag, td, HEADERGET_EXT);
@@ -496,7 +327,8 @@ rpm_update_cache(void *ptr)
 		    if (current_cache_entry == NULL) {
 			__pmNotifyErr(LOG_INFO, "strdup(%d) for %d failed",
 				      (int) strlen(rpmval), i);
-			return -ENOMEM;
+			sts = -ENOMEM;
+			break;
 		    }
 		    metrics[i].type = PM_TYPE_STRING;
 		    break;
@@ -507,7 +339,7 @@ rpm_update_cache(void *ptr)
 		    // concatenate the string parts
 		    char **strings;
 		    strings = td->data;
-		    char *ccecp;
+		    char *ccecp = 0;
 		    int idx;
 		    int string_length = 0;
 
@@ -526,7 +358,8 @@ rpm_update_cache(void *ptr)
 					      (int) strlen(strings[idx]),
 					      i);
 			    strcpy(ccecp, strings[idx]);
-			} else {
+			}
+			else {
 			    string_length = strlen(ccecp) +
 				strlen(strings[idx]) + 2;
 			    ccecp = realloc(ccecp, string_length);
@@ -541,18 +374,6 @@ rpm_update_cache(void *ptr)
 			}
 		    }
 		    current_cache_entry->entry[i].cp = ccecp;
-		    /*
-		     * printf 
-		     */
-		    /*
-		     * ("STRING_ARRAY count=%d[%#lx][%d]=%s/%s\n", 
-		     */
-		    /*
-		     * td->count, current_cache_entry, i, 
-		     */
-		    /*
-		     * current_cache_entry->entry[i].cp, ccecp); 
-		     */
 		    free(td->data);
 		    metrics[i].type = PM_TYPE_STRING;
 		    break;
@@ -577,6 +398,8 @@ rpm_update_cache(void *ptr)
 		    ((long long *) (td->data))[0];
 		metrics[i].type = PM_TYPE_64;
 		break;
+	    default:
+		break;
 	    }
 	    rpmtdReset(td);
 	}
@@ -585,25 +408,9 @@ rpm_update_cache(void *ptr)
     rpmdbFreeIterator(mi);
     rpmtsFree(ts);
 
-    ENTRY e, *ep;
-    int code;
-    // run through the cache and load the hash table
-    for (this_cache_entry = cache; this_cache_entry != NULL;
-	 this_cache_entry = this_cache_entry->next) {
-	e.key = this_cache_entry->entry[16].cp;
-	if (!e.key)
-	    continue;
-	e.data = this_cache_entry;
-	if (hsearch_r(e, ENTER, &ep, &htab) == 0)
-	    return PM_ERR_INST;
-    }
-
-    __pmProcessRunTimes(&usr2, &sys2);
-    __pmNotifyErr(LOG_NOTICE, "user/%g sys/%g", (usr2 - usr1) / 100,
-		  (sys2 - sys1) / 100);
-
-    pthread_rwlock_unlock(&indom_lock);
-    return 0;
+    pthread_mutex_unlock(&indom_mutex);
+    stop_timing();
+    return sts;
 }
 
 
@@ -616,22 +423,11 @@ rpm_update_indom(void *ptr)
 {
     int sts;
     cache_entry *this_cache_entry;
-    double usr1,
-     usr2,
-     sys1,
-     sys2;
-
-    numrefresh++;
-
-    // For rebuilding the hash table
-    hdestroy_r(&htab);
-    memset((void *) &htab, 0, sizeof(htab));
-    hcreate_r(HASH_HBOUND, &htab);
 
     rpm_update_cache(NULL);
 
-    __pmProcessRunTimes(&usr1, &sys1);
-    pthread_rwlock_wrlock(&indom_lock);
+    start_timing();
+    pthread_mutex_lock(&indom_mutex);
     sts = pmdaCacheOp(*rpm_indom, PMDA_CACHE_INACTIVE);
     if (sts < 0)
 	__pmNotifyErr(LOG_ERR,
@@ -643,21 +439,21 @@ rpm_update_indom(void *ptr)
     for (this_cache_entry = cache; this_cache_entry != NULL;
 	 this_cache_entry = this_cache_entry->next) {
 	ENTRY e;
-	e.key = this_cache_entry->entry[16].cp;
+	e.key = this_cache_entry->entry[RPMTAG_NAME_IDX].cp;
 	if (!e.key)
 	    continue;
 	sts =
 	    pmdaCacheStore(*rpm_indom, PMDA_CACHE_ADD, e.key,
-			   (void *) e.data);
+			   (void *) this_cache_entry);
+
 	if (sts < 0)
 	    __pmNotifyErr(LOG_ERR, "pmdaCacheStore failed: indom=%s: %s",
 			  pmInDomStr(*rpm_indom), pmErrStr(sts));
     }
 
-    __pmProcessRunTimes(&usr2, &sys2);
-    __pmNotifyErr(LOG_NOTICE, "user/%g sys/%g", (usr2 - usr1) / 100,
-		  (sys2 - sys1) / 100);
-    pthread_rwlock_unlock(&indom_lock);
+    stop_timing();
+    pthread_mutex_unlock(&indom_mutex);
+    numrefresh++;
 
     return NULL;
 }
@@ -711,26 +507,30 @@ void
 rpm_init(pmdaInterface * dp)
 {
     if (isDSO) {
-	pmdaDSO(dp, PMDA_INTERFACE_5, "rpm DSO", mypath);
-    } else {
+	char helppath[MAXPATHLEN];
+	pmdaDSO(dp, PMDA_INTERFACE_5, "rpm DSO", helppath);
+    }
+    else {
 	__pmSetProcessIdentity(username);
     }
 
     if (dp->status != 0)
 	return;
 
+    pthread_mutex_init(&indom_mutex, NULL);
     // Load rpms into instance table
     pthread_create(&indom_thread, NULL, rpm_update_indom, NULL);
     // Note changes to the rpm database
     pthread_create(&inotify_thread, NULL, rpm_inotify, NULL);
 
     dp->version.any.fetch = rpm_fetch;
+    dp->version.any.instance = rpm_instance;
 
     pmdaSetFetchCallBack(dp, rpm_fetchCallBack);
 
 
-    pmdaInit(dp, indomtab, 1, metrictab,
-	     sizeof(metrictab) / sizeof(metrictab[0]));
+    pmdaInit(dp, indomtab, sizeof(indomtab) / sizeof(indomtab[0]),
+		metrictab, sizeof(metrictab) / sizeof(metrictab[0]));
 
     if (dp->status != 0)
 	__pmNotifyErr(LOG_ERR, "pmdaInit failed %d", dp->status);
@@ -764,15 +564,18 @@ main(int argc, char **argv)
      err = 0;
     int sep = __pmPathSeparator();
     pmdaInterface dispatch;
+    char helppath[MAXPATHLEN];
 
     isDSO = 0;
     __pmSetProgname(argv[0]);
+    __pmProcessDataSize(NULL);
     __pmGetUsername(&username);
 
-    snprintf(mypath, sizeof(mypath), "%s%c" "rpm" "%c" "help",
+    __pmNotifyErr(LOG_INFO, "in rpm_instance");
+    snprintf(helppath, sizeof(helppath), "%s%c" "rpm" "%c" "help",
 	     pmGetConfig("PCP_PMDAS_DIR"), sep, sep);
     pmdaDaemon(&dispatch, PMDA_INTERFACE_5, pmProgname, RPM,
-	       "rpm.log", mypath);
+	       "rpm.log", helppath);
 
     while ((c =
 	    pmdaGetOpt(argc, argv, "D:d:i:l:pu:6:U:?", &dispatch,
