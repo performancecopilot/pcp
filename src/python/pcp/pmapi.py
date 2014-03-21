d24c67406c79f116c2701ee76d2c9ca25628362d
Nathan Scott <nathans@redhat.com>
2014-03-21 16:04:16 +1100

    Switch python pmGetContextHostname to thread-safe libpcp interface

--- src/python/pcp/pmapi.py
+++ src/python/pcp/pmapi.py
@@ -500,8 +500,8 @@ LIBPCP.pmSetMode.argtypes = [c_int, POINTER(timeval), c_int]
 LIBPCP.pmReconnectContext.restype = c_int
 LIBPCP.pmReconnectContext.argtypes = [c_int]
 
-LIBPCP.pmGetContextHostName.restype = c_char_p
-LIBPCP.pmGetContextHostName.argtypes = [c_int]
+LIBPCP.pmGetContextHostName_r.restype = c_char_p
+LIBPCP.pmGetContextHostName_r.argtypes = [c_int, c_char_p, c_int]
 
 
 ##
@@ -1102,13 +1102,14 @@ class pmContext(object):
     def pmGetContextHostName(self):
         """PMAPI - Lookup the hostname for the given context
 
-        Unlike the underlying PMAPI function, this method takes no parameter.
         This method simply returns the hostname for the context belonging to
         its pmContext instance object.
 
         "hostname" = pmGetContextHostName()
         """
-        return LIBPCP.pmGetContextHostName(self.ctx)
+        buflen = c_api.PM_LOG_MAXHOSTLEN
+        buffer = ctypes.create_string_buffer(buflen)
+        return str(LIBPCP.pmGetContextHostName_r(self.ctx, buffer, buflen))
 
     ##
     # PMAPI Timezone Services
