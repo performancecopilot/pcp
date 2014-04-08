c7b1bb5ec75f3f2974cd5f261e85b63a32c74ae8
Nathan Scott <nathans@redhat.com>
2014-04-08 12:01:55 +1000

    Use _get_port in qa/779, fix missed comments
    Make use of the _get_port function to find a suitable port,
    rather than hard-coding one with "#hack" comments :)
    
    Several spots where cut&paste of comments was NQR resolved.

--- qa/pmdas/slow/pmdaslow.pl
+++ qa/pmdas/slow/pmdaslow.pl
@@ -1,6 +1,5 @@
 #
-# Copyright (c) 2012 Red Hat.
-# Copyright (c) 2008 Aconex.  All Rights Reserved.
+# Copyright (c) 2014 Ken McDonell.  All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
