3505db7315c6653ee1f39bb790ec94445723c520
Frank Ch. Eigler <fche@redhat.com>
2014-05-12 17:58:11 -0400

    whitespace: gnu-indent pmwebd to keep it looking spic & span

--- src/pmwebapi/util.c
+++ src/pmwebapi/util.c
@@ -17,78 +17,73 @@
 
 #ifdef IS_MINGW
 
-void
-pmweb_start_daemon(int argc, char **argv)
+void pmweb_start_daemon (int argc, char **argv)
 {
     PROCESS_INFORMATION piProcInfo;
     STARTUPINFO siStartInfo;
     LPTSTR cmdline = NULL;
-    int i, sz = 3; /* -f\0 */
+    int i, sz = 3;		/* -f\0 */
 
     for (i = 0; i < argc; i++)
-	sz += strlen(argv[i]) + 1;
-    if ((cmdline = malloc(sz)) == NULL) {
-	__pmNotifyErr(LOG_ERR, "pmweb_start_daemon: no memory");
-	exit(1);
+	sz += strlen (argv[i]) + 1;
+    if ((cmdline = malloc (sz)) == NULL) {
+	__pmNotifyErr (LOG_ERR, "pmweb_start_daemon: no memory");
+	exit (1);
     }
     for (sz = i = 0; i < argc; i++)
-	sz += sprintf(cmdline, "%s ", argv[i]);
-    sprintf(cmdline + sz, "-f");
+	sz += sprintf (cmdline, "%s ", argv[i]);
+    sprintf (cmdline + sz, "-f");
 
-    ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
-    ZeroMemory(&siStartInfo, sizeof(STARTUPINFO));
-    siStartInfo.cb = sizeof(STARTUPINFO);
+    ZeroMemory (&piProcInfo, sizeof (PROCESS_INFORMATION));
+    ZeroMemory (&siStartInfo, sizeof (STARTUPINFO));
+    siStartInfo.cb = sizeof (STARTUPINFO);
 
-    if (0 == CreateProcess(
-		NULL, cmdline,
-		NULL, NULL,	/* process and thread attributes */
-		FALSE,		/* inherit handles */
-		CREATE_NEW_PROCESS_GROUP | CREATE_NO_WINDOW | DETACHED_PROCESS,
-		NULL,		/* environment (from parent) */
-		NULL,		/* current directory */
-		&siStartInfo,	/* STARTUPINFO pointer */
-		&piProcInfo)) {	/* receives PROCESS_INFORMATION */
-	__pmNotifyErr(LOG_ERR, "pmweb_start_daemon: CreateProcess");
+    if (0 == CreateProcess (NULL, cmdline, NULL, NULL,	/* process and thread attributes */
+			    FALSE,	/* inherit handles */
+			    CREATE_NEW_PROCESS_GROUP | CREATE_NO_WINDOW | DETACHED_PROCESS, NULL,	/* environment (from parent) */
+			    NULL,	/* current directory */
+			    &siStartInfo,	/* STARTUPINFO pointer */
+			    &piProcInfo)) {	/* receives PROCESS_INFORMATION */
+	__pmNotifyErr (LOG_ERR, "pmweb_start_daemon: CreateProcess");
 	/* but keep going */
-    }
-    else {
+    } else {
 	/* parent, let her exit, but avoid ugly "Log finished" messages */
-	fclose(stderr);
-	exit(0);
+	fclose (stderr);
+	exit (0);
     }
 }
 
 #else
 
 /* Based on Stevens (Unix Network Programming, p.83) */
-void
-pmweb_start_daemon(int argc, char **argv)
+void pmweb_start_daemon (int argc, char **argv)
 {
     int childpid;
 
-    (void)argc; (void)argv;
+    (void) argc;
+    (void) argv;
 
 #if defined(HAVE_TERMIO_SIGNALS)
-    signal(SIGTTOU, SIG_IGN);
-    signal(SIGTTIN, SIG_IGN);
-    signal(SIGTSTP, SIG_IGN);
+    signal (SIGTTOU, SIG_IGN);
+    signal (SIGTTIN, SIG_IGN);
+    signal (SIGTSTP, SIG_IGN);
 #endif
 
-    if ((childpid = fork()) < 0)
-	__pmNotifyErr(LOG_ERR, "pmweb_start_daemon: fork");
-	/* but keep going */
+    if ((childpid = fork ()) < 0)
+	__pmNotifyErr (LOG_ERR, "pmweb_start_daemon: fork");
+    /* but keep going */
     else if (childpid > 0) {
 	/* parent, let her exit, but avoid ugly "Log finished" messages */
-	fclose(stderr);
-	exit(0);
+	fclose (stderr);
+	exit (0);
     }
 
     /* not a process group leader, lose controlling tty */
-    if (setsid() == -1)
-	__pmNotifyErr(LOG_WARNING, "pmweb_start_daemon: setsid");
-	/* but keep going */
+    if (setsid () == -1)
+	__pmNotifyErr (LOG_WARNING, "pmweb_start_daemon: setsid");
+    /* but keep going */
 
-    close(0);
+    close (0);
     /* don't close other fd's -- we know that only good ones are open! */
 
     /* don't chdir("/") -- we still need to open pmcd.log */
