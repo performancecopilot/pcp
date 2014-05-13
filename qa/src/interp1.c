performancecopilot/pcp

commit 068ab84d26645a4895220edb70b8cdca0320b734

qa/787 (new) - exercise slow archive reading
Uses the reduced version of Frank's archive that used to show
really excessive log I/O for some metrics in the presence of
<mark> records.

qa/787
@@ -1,0 +1,82 @@
+#!/bin/sh
+# PCP QA Test No. 787
+# Frank's "slow" archive with <mark> records
+#
+# Copyright (c) 2014 Ken McDonell.  All Rights Reserved.
+#
+# TODO - read backwards?
+
+seq=`basename $0`
+echo "QA output created by $seq"
+
+# get standard environment, filters and checks
+. ./common.product
+. ./common.filter
+. ./common.check
+
+status=1	# failure is the default!
+$sudo rm -rf $tmp.* $seq.full
+trap "cd $here; rm -rf $tmp.*; exit \$status" 0 1 2 3 15
+
+# change to true to produce detailed values in $seq.full to
+# validate the interpolated values reported by pmval
+#
+full=false
+$full && rm -f $seq.full
+
+_summary()
+{
+    grep '^__pmLogRead:' | wc -l | sed -e 's/ //g'
+}
+
+_filter()
+{
+    sed -e "s@$tmp@TMP@g"
+}
+
+pmdumplog -xz src/slow >$tmp.tmp
+echo "log records: `egrep '^([A-Z][a-z][a-z] )|([0-2][0-9]:)' <$tmp.tmp | wc -l | sed -e 's/ //g'`"
+
+# for full output, lines look like
+# Fri Jan 10 17:58:09.600 2014  60.0.32 (hinv.ncpu): value 4
+#
+$full && grep '<mark>' <$tmp.tmp | LC_COLLATE=POSIX sort -k 3,3n -k 4,4 >$tmp.mark
+
+# real QA test starts here
+for metric in hinv.ncpu kernel.all.nprocs pmcd.numagents
+do
+    echo
+    echo "=== $metric ==="
+    $full && pmdumplog -xz src/slow $metric | egrep '^([A-Z][a-z][a-z] )|([0-2][0-9]:)' | LC_COLLATE=POSIX sort -k 3,3n -k 4,4 >$tmp.tmp
+    $full && LC_COLLATE=POSIX sort -k 3,3n -k 4,4 $tmp.tmp $tmp.mark >>$seq.full
+
+    echo
+    echo "+++ forwards +++"
+    pmval -z -Dlog -t 1hr -A 1hr -a src/slow $metric 2>$tmp.err
+    echo "log reads: `_summary <$tmp.err`"
+
+    echo
+    echo "+++ backwards +++"
+    src/interp1 -s 1000 -a src/slow -t 3600 $metric
+
+    echo "`echo $metric | sed -e 's/\./_/g'` = $metric;" >>$tmp.pmie
+done
+
+echo
+echo "=== pmcd.numagents converted to discrete semantics ==="
+echo "metric pmcd.numagents {  sem -> discrete }" >$tmp.config
+pmlogrewrite -c $tmp.config src/slow $tmp
+pmval -z -Dlog -t 1hr -A 1hr -a $tmp pmcd.numagents 2>$tmp.err \
+| _filter
+echo "log reads: `_summary <$tmp.err`"
+
+echo
+echo "=== all metrics at once ==="
+pmie -z -a src/slow -c $tmp.pmie -t 1hr -A 1hr -v -Dlog 2>$tmp.err
+echo "log reads: `_summary <$tmp.err`"
+
+
+# success, all done
+status=0
+
+exit

qa/src/interp1.c
@@ -32,8 +32,7 @@ main(int argc, char **argv)
     int		numpmid = 3;
     pmID	pmid[3];
     char	*name[] = { "sample.seconds", "sample.drift", "sample.milliseconds" };
-    pmDesc	desc;
-    int		type[3];
+    pmDesc	desc[3];
     struct timeval tend = {0x7fffffff, 0};
 
     __pmSetProgname(argv[0]);
@@ -161,13 +160,11 @@ Options\n\
     }
 
     for (i = 0; i < numpmid; i++) {
-	sts = pmLookupDesc(pmid[i], &desc);
+	sts = pmLookupDesc(pmid[i], &desc[i]);
 	if (sts < 0) {
 	    printf("Warning: pmLookupDesc(%s): %s\n", pmIDStr(pmid[i]), pmErrStr(sts));
-	    type[i] = -1;
+	    desc[i].type = -1;
 	}
-	else
-	    type[i] = desc.type;
     }
 
     for (i = 0; i < samples; i++) {
@@ -198,12 +195,16 @@ Options\n\
 				result->vset[j]->vlist[k].inst);
 			continue;
 		    }
-		    if (type[j] == PM_TYPE_32 || type[j] == PM_TYPE_U32) {
-			printf("delta[%d]: %d\n", k,
-			    result->vset[j]->vlist[k].value.lval -
-			    prev->vset[j]->vlist[k].value.lval);
+		    if (desc[j].type == PM_TYPE_32 || desc[j].type == PM_TYPE_U32) {
+			if (desc[j].sem == PM_SEM_COUNTER)
+			    printf("delta[%d]: %d\n", k,
+				result->vset[j]->vlist[k].value.lval -
+				prev->vset[j]->vlist[k].value.lval);
+			else
+			    printf("value[%d]: %d\n", k,
+				result->vset[j]->vlist[k].value.lval);
 		    }
-		    else if (type[j] == PM_TYPE_DOUBLE) {
+		    else if (desc[j].type == PM_TYPE_DOUBLE) {
 			void		*cp = (void *)result->vset[j]->vlist[k].value.pval->vbuf;
 			void		*pp = (void *)prev->vset[j]->vlist[k].value.pval->vbuf;
 			double		cv, pv;
@@ -211,17 +212,21 @@ Options\n\
 
 			memcpy((void *)&av, cp, sizeof(pmAtomValue));
 			cv = av.d;
-			memcpy((void *)&av, pp, sizeof(pmAtomValue));
-			pv = av.d;
-			printf("delta[%d]: %.0f\n", k, cv - pv);
+			if (desc[j].sem == PM_SEM_COUNTER) {
+			    memcpy((void *)&av, pp, sizeof(pmAtomValue));
+			    pv = av.d;
+			    printf("delta[%d]: %.0f\n", k, cv - pv);
+			}
+			else
+			    printf("value[%d]: %.0f\n", k, cv);
 		    }
-		    else if (type[j] == PM_TYPE_STRING) {
+		    else if (desc[j].type == PM_TYPE_STRING) {
 			printf("value[%d]: %s\n", k,
 			    result->vset[j]->vlist[k].value.pval->vbuf);
 		    }
 		    else {
 			printf("don't know how to display type %d for PMID %s\n",
-			    type[j], pmIDStr(pmid[j]));
+			    desc[j].type, pmIDStr(pmid[j]));
 			break;
 		    }
 		}

qa/group
@@ -913,6 +913,7 @@ avahi
 779 pmda local oss
 783 pmda.rpm local oss
 786 context context_local libpcp local oss
+787 archive local oss pmie pmlogrewrite pmdumplog pmval
 798 pmda.nfsclient local oss
 815 pmie local oss
 823 pmda.sample pmstore secure local oss

commit dff1686a9b5be5d3c6c88949b4340fe9ed548df6

Add -d option into qa/src/interp1.c for new-style output
Commit 068ab84d2 has accidentally regressed test qa/091 by
changing the output that interp1.c produces, which it was
relying being a certain way.

Resolve this by adding a new option to get the new output,
and keep on using the old output in existing tests.  Quite
probably test 091 could be reworked to use the new output,
but going for the simplest fix at this stage.

qa/787
@@ -57,7 +57,7 @@ do
 
     echo
     echo "+++ backwards +++"
-    src/interp1 -s 1000 -a src/slow -t 3600 $metric
+    src/interp1 -d -s 1000 -a src/slow -t 3600 $metric
 
     echo "`echo $metric | sed -e 's/\./_/g'` = $metric;" >>$tmp.pmie
 done

qa/src/interp1.c
@@ -17,6 +17,7 @@ main(int argc, char **argv)
     int		j;
     int		k;
     double	tdiff;
+    int		dflag = 0;
     int		errflag = 0;
     int		ahtype = 0;
     char	*host = NULL;			/* pander to gcc */
@@ -37,7 +38,7 @@ main(int argc, char **argv)
 
     __pmSetProgname(argv[0]);
 
-    while ((c = getopt(argc, argv, "a:D:n:s:t:?")) != EOF) {
+    while ((c = getopt(argc, argv, "a:D:dn:s:t:?")) != EOF) {
 	switch (c) {
 
 	case 'a':	/* archive name */
@@ -47,6 +48,10 @@ main(int argc, char **argv)
 	    }
 	    ahtype = PM_CONTEXT_ARCHIVE;
 	    host = optarg;
+	    break;
+
+	case 'd':	/* use metric descriptor to decide on rate conversion */
+	    dflag++;
 	    break;
 
 	case 'D':	/* debug flag */
@@ -93,6 +98,7 @@ main(int argc, char **argv)
 \n\
 Options\n\
   -a   archive	  metrics source is an archive log\n\
+  -d              use metric descriptors to decide on value or delta\n\
   -D   debug	  standard PCP debug flag\n\
   -n   namespace  use an alternative PMNS\n\
   -s   samples	  terminate after this many iterations\n\
@@ -196,7 +202,7 @@ Options\n\
 			continue;
 		    }
 		    if (desc[j].type == PM_TYPE_32 || desc[j].type == PM_TYPE_U32) {
-			if (desc[j].sem == PM_SEM_COUNTER)
+			if (!dflag || desc[j].sem == PM_SEM_COUNTER)
 			    printf("delta[%d]: %d\n", k,
 				result->vset[j]->vlist[k].value.lval -
 				prev->vset[j]->vlist[k].value.lval);
@@ -212,7 +218,7 @@ Options\n\
 
 			memcpy((void *)&av, cp, sizeof(pmAtomValue));
 			cv = av.d;
-			if (desc[j].sem == PM_SEM_COUNTER) {
+			if (!dflag || desc[j].sem == PM_SEM_COUNTER) {
 			    memcpy((void *)&av, pp, sizeof(pmAtomValue));
 			    pv = av.d;
 			    printf("delta[%d]: %.0f\n", k, cv - pv);