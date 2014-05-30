dccdb6233db68d7d9a7f8403db9bc157e2d0b242
Frank Ch. Eigler <fche@redhat.com>
2014-05-30 11:50:32 -0400

    pmwebapi: switched to astyle as indenter

--- src/pmwebapi/util.cxx
+++ src/pmwebapi/util.cxx
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2013-2014 Red Hat, Inc.  All Rights Reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
@@ -29,7 +29,8 @@ extern "C" {
 #include <stdlib.h>
 #include <netdb.h>
 #include <microhttpd.h>
-} using namespace std;
+}
+using namespace std;
 
 
 // Print a string to cout/cerr progress reports, similar to the
@@ -39,11 +40,12 @@ ostream & timestamp (ostream & o)
     time_t now;
     time (&now);
     char *now2 = ctime (&now);
-    if (now2)
-	now2[19] = '\0';	// overwrite \n
+    if (now2) {
+        now2[19] = '\0';    // overwrite \n
+    }
 
     return o << "[" << (now2 ? now2 : "") << "] " << pmProgname << "(" << getpid () <<
-	"): ";
+           "): ";
     // NB: we're single-threaded; no point printing out a thread-id too
 }
 
@@ -57,19 +59,20 @@ ostream & connstamp (ostream & o, struct MHD_Connection * conn)
 
     /* Look up client address data. */
     const union MHD_ConnectionInfo *u =
-	MHD_get_connection_info (conn, MHD_CONNECTION_INFO_CLIENT_ADDRESS);
+                MHD_get_connection_info (conn, MHD_CONNECTION_INFO_CLIENT_ADDRESS);
     struct sockaddr *so = u ? u->client_addr : 0;
 
     if (so && so->sa_family == AF_INET)
-	sts = getnameinfo (so, sizeof (struct sockaddr_in),
-			   hostname, sizeof (hostname),
-			   servname, sizeof (servname), NI_NUMERICHOST | NI_NUMERICSERV);
+        sts = getnameinfo (so, sizeof (struct sockaddr_in),
+                           hostname, sizeof (hostname),
+                           servname, sizeof (servname), NI_NUMERICHOST | NI_NUMERICSERV);
     else if (so && so->sa_family == AF_INET6)
-	sts = getnameinfo (so, sizeof (struct sockaddr_in6),
-			   hostname, sizeof (hostname),
-			   servname, sizeof (servname), NI_NUMERICHOST | NI_NUMERICSERV);
-    if (sts != 0)
-	hostname[0] = servname[0] = '\0';
+        sts = getnameinfo (so, sizeof (struct sockaddr_in6),
+                           hostname, sizeof (hostname),
+                           servname, sizeof (servname), NI_NUMERICHOST | NI_NUMERICSERV);
+    if (sts != 0) {
+        hostname[0] = servname[0] = '\0';
+    }
 
     timestamp (o) << "[" << hostname << ":" << servname << "] ";
 
@@ -84,7 +87,7 @@ vector < string > split (const std::string & s, char delim)
     stringstream ss (s);
     string item;
     while (getline (ss, item, delim)) {
-	elems.push_back (item);
+        elems.push_back (item);
     }
     return elems;
 }
@@ -97,26 +100,30 @@ vector < string > split (const std::string & s, char delim)
 bool cursed_path_p (const string & blessed, const string & questionable)
 {
     char *rp1c = realpath (blessed.c_str (), NULL);
-    if (rp1c == NULL)
-	return true;
+    if (rp1c == NULL) {
+        return true;
+    }
     string rp1 = string (rp1c);
     free (rp1c);
 
     char *rp2c = realpath (questionable.c_str (), NULL);
     if (rp2c == NULL) {
-	return true;
+        return true;
     }
     string rp2 = string (rp2c);
     free (rp2c);
 
-    if (rp1 == rp2)		// identical: OK
-	return false;
+    if (rp1 == rp2) {	// identical: OK
+        return false;
+    }
 
-    if (rp2.size () < rp1.size () + 1)	// rp2 cannot be nested within rp1
-	return true;
+    if (rp2.size () < rp1.size () + 1) {	// rp2 cannot be nested within rp1
+        return true;
+    }
 
-    if (rp2.substr (0, rp1.size () + 1) == (rp1 + '/'))	// rp2 nested beneath rp1/
-	return false;
+    if (rp2.substr (0, rp1.size () + 1) == (rp1 + '/')) {	// rp2 nested beneath rp1/
+        return false;
+    }
 
     return true;
 }
