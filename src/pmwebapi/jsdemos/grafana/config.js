e01e3a38a5b2d1e3eb3e7df40ac94506eed1007d
Frank Ch. Eigler <fche@redhat.com>
2014-06-03 18:34:08 -0400

    grafana defaults: show something interesting

--- src/pmwebapi/jsdemos/grafana/config.js
+++ src/pmwebapi/jsdemos/grafana/config.js
@@ -5,50 +5,10 @@
  */
 define(['settings'],
 function (Settings) {
-  
-
   return new Settings({
-
-    /**
-     * elasticsearch url:
-     * For Basic authentication use: http://username:password@domain.com:9200
-     */
-    elasticsearch: "http://"+window.location.hostname+":9200",
-
-    /**
-     * graphite-web url:
-     * For Basic authentication use: http://username:password@domain.com
-     * Basic authentication requires special HTTP headers to be configured
-     * in nginx or apache for cross origin domain sharing to work (CORS).
-     * Check install documentation on github
-     */
     graphiteUrl: "http://"+window.location.hostname+":"+window.location.port+"/graphite",
-    //  default_route: '/dashboard/file/default.json',
-
-    /**
-     * If you experiance problems with zoom, it is probably caused by timezone diff between
-     * your browser and the graphite-web application. timezoneOffset setting can be used to have Grafana
-     * translate absolute time ranges to the graphite-web timezone.
-     * Example:
-     *   If TIME_ZONE in graphite-web config file local_settings.py is set to America/New_York, then set
-     *   timezoneOffset to "-0500" (for UTC - 5 hours)
-     * Example:
-     *   If TIME_ZONE is set to UTC, set this to "0000"
-     */
-
-    timezoneOffset: null,
-
-    /**
-     * Elasticsearch index for storing dashboards
-     *
-     */
-    grafana_index: "grafana-dash",
-
-    /**
-     * set to false to disable unsaved changes warning
-     */
+    timezoneOffset: "0000",
     unsaved_changes_warning: true,
-
     panel_names: [
       'text',
       'graphite'
