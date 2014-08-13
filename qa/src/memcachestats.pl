bd0b264f3b3588430c58953e7cd5a9f41b56c131
Nathan Scott <nathans@redhat.com>
2014-08-13 12:00:36 +1000

    Merge qa/777 and qa/835 into one pmdamemcache test
    Removed some accidental doubling-up in pmdamemcache QA - go
    with the perl checker program instead of using netcat which
    turned out to have some platform-dependencies anyway.
    
    Extended the checker tool to also query the other stats and
    also extended the test to marry up many more of the metrics
    for extra verification goodness.

--- qa/src/memcachestats.pl
+++ qa/src/memcachestats.pl
@@ -12,26 +12,24 @@ use Getopt::Long;
 my (@default_instances, @instances, $remote, $default_port);
 
 # List of servers/ ports to query.
-@default_instances = (
-  '127.0.0.1:11211',
-);
+@default_instances = ( '127.0.0.1:11211' );
 
 # Default port to connect to, if not specified
 $default_port = "11211";
 
 if (@ARGV) {
     GetOptions (
-      'instances=s'	=> \@instances,
-      'port=i'		=> \$default_port,
+	'instances=s'	=> \@instances,
+	'port=i'	=> \$default_port,
     );
     if (@instances) {
-      @instances = split(/,/,join(',',@instances));
+	@instances = split(/,/,join(',',@instances));
     } else {
-      @instances = @default_instances;
+	@instances = @default_instances;
     }
 }
 else {
-  @instances = @default_instances;
+    @instances = @default_instances;
 }
 
 foreach my $instance (@instances) {
@@ -40,26 +38,29 @@ foreach my $instance (@instances) {
 
     my @split = split(/:/,$instance);
     unless ( $split[1] ) {
-      $instance = $instance . ":" . $default_port;
+	$instance = $instance . ":" . $default_port;
     }
 
     $remote = IO::Socket::INET->new($instance);
     unless ( defined($remote) ) { 
-      print "no cigar\n";
+	print "no cigar: $!\n";
+	exit(1);
     }
-
     $remote->autoflush(1);
-    print $remote "stats\n";
 
-    LINE: while ( defined ( my $line = <$remote> ) ) {
-      last LINE if ( $line =~ /END/ );
-      chomp $line;
-      $line =~ s/^STAT //;
-      $line =~ s///;
-      print $line . "\n";
-      next LINE;
+    my @commands = ("stats", "stats slabs", "stats items" );
+    foreach my $command (@commands) {
+	print $remote "$command\n";
+
+        LINE: while ( defined ( my $line = <$remote> ) ) {
+	    last LINE if ( $line =~ /^END/ );
+	    chomp $line;
+	    $line =~ s/^STAT //;
+	    $line =~ s///;
+	    print $line . "\n";
+	    next LINE;
+	}
     }
 
     close $remote;
 }
-
