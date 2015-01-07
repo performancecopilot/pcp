bf0f0c3e8550412c1952bc6a266561a6b8a93ca7
Martins Innus <minnus@buffalo.edu>
2015-01-07 16:17:33 -0500

    Lustre pmda work
    Still in progress
    Initialize metrics to 0 since they are not reported by lustre if they are 0

--- src/pmdas/lustre/pmdalustre.pl
+++ src/pmdas/lustre/pmdalustre.pl
@@ -14,12 +14,28 @@ for my $file (pmda_config('PCP_PMDAS_DIR') . '/lustre/lustre.conf', 'luster.conf
         eval `cat $file` unless ! -f $file;
 }
 
-# Check env variable for mountstats file to use
-if ( defined $ENV{"LUSTRE_LLITE_STATS_PATH"} ) {
-	$MOUNTSTATS_PATH = $ENV{"LUSTRE_LLITE_STATS_PATH"}
+# Check env variable for llite path to use.  Should be a dir with all the files
+if ( defined $ENV{"LUSTRE_LLITE_PATH"} ) {
+	$LLITE_PATH = $ENV{"LUSTRE_LLITES_PATH"}
 }
 
 our $noval = PM_ERR_APPVERSION;
+
+# List of metrics we care about
+#
+# Bytes have: min, max, count, total
+our @byte_stats = ('read_bytes', 'write_bytes', 'osc_read', 'osc_write');
+
+# Pages have: min, max, count, total
+our @page_stats = ('brw_read', 'brw_write');
+
+# Reg have: count
+our @reg_stats = ('dirty_pages_hits', 'dirty_pages_misses', 'ioctl', 'open', 'close',
+	   'mmap', 'seek', 'fsync', 'readdir', 'setattr',
+	   'truncate', 'flock', 'getattr', 'create', 'link', 'unlink',
+	   'symlink', 'mkdir', 'rmdir', 'mknod', 'rename',
+	   'statfs', 'alloc_inode', 'setxattr', 'getxattr', 'getxattr_hits', 'listxattr',
+	   'removexattr', 'inode_permission');
 
 # The stats hash is keyed on base name of the entires in /proc/fs/lustre/llite/ e.g.
 # 'lustre' from lustre-ffff880378305c00.  The value is a hash ref, and that hash contains all of
@@ -33,9 +49,11 @@ our %h = ();
 # And parse the stats file underneath
 
 sub lustre_get_fs_stats{
+	$pmda->err("Doing update");
 	opendir(LLITEDIR, $LLITE_PATH) || die "Can't open directory: $LLITE_PATH\n";
 
 	while(my $ldev = readdir(LLITEDIR) ){
+		$pmda->err("Checking dev $ldev");
 		if( $ldev =~ /^(\w+)-([a-f0-9]{16})$/ ){
 
 			# Get the device information
@@ -48,7 +66,23 @@ sub lustre_get_fs_stats{
 
 			# Parse the actual stats file
 			# From: lustre-2.5.3/lustre/llite/lproc_llite.c
-			# Stats do not exists unless the counter has been incremented, need to initialize to 0
+			# Stats do not exists unless the counter has been incremented. Need to initialize to 0
+			
+			for my $stat_name (@byte_stats) {
+				$h{$mtroot}->{"lustre.llite.$stat_name.count"} = 0;
+				$h{$mtroot}->{"lustre.llite.$stat_name.min"} = 0;
+				$h{$mtroot}->{"lustre.llite.$stat_name.max"} = 0;
+				$h{$mtroot}->{"lustre.llite.$stat_name.total"} = 0;
+			}
+			for my $stat_name (@page_stats) {
+				$h{$mtroot}->{"lustre.llite.$stat_name.count"} = 0;
+				$h{$mtroot}->{"lustre.llite.$stat_name.min"} = 0;
+				$h{$mtroot}->{"lustre.llite.$stat_name.max"} = 0;
+				$h{$mtroot}->{"lustre.llite.$stat_name.total"} = 0;
+			}
+			for my $stat_name (@reg_stats) {
+				$h{$mtroot}->{"lustre.llite.$stat_name.count"} = 0;
+			}
 			
 			my $statspath = $LLITE_PATH . $ldev . '/stats';
 
@@ -58,38 +92,41 @@ sub lustre_get_fs_stats{
 
 			while (<STATS>) {
                 		my $line = $_;
+				$pmda->err("Parsing: $line");
 				# Byte types first
-				if( $line =~ /^(read_bytes|write_bytes|osc_read|osc_write)\w+(\d+) samples \[bytes\] (\d+) (\d+) (\d+)$/){
+				if( $line =~ /^(read_bytes|write_bytes|osc_read|osc_write)\s+(\d+) samples \[bytes\] (\d+) (\d+) (\d+)$/){
 					my $name = $1;
 					my $count = $2;
 					my $min = $3;
 					my $max = $4;
 					my $total = $5;
-					$h{$mtroot}->{"lustre.llite.count.$name"} = $count;
-					$h{$mtroot}->{"lustre.llite.min.$name"} = $min;
-					$h{$mtroot}->{"lustre.llite.max.$name"} = $max;
-					$h{$mtroot}->{"lustre.llite.total.$name"} = $total;
+					$pmda->err("Got: $name $count $min $max $total");
+					$h{$mtroot}->{"lustre.llite.$name.count"} = $count;
+					$h{$mtroot}->{"lustre.llite.$name.min"} = $min;
+					$h{$mtroot}->{"lustre.llite.$name.max"} = $max;
+					$h{$mtroot}->{"lustre.llite.$name.total"} = $total;
 				# Pages types
 				# I have not seen these, so can't test
-				} elsif ( $line =~ /^(brw_read|brw_write)\w+(\d+) samples \[pages\] (\d+) (\d+) (\d+)$/){
+				} elsif ( $line =~ /^(brw_read|brw_write)\s+(\d+) samples \[pages\] (\d+) (\d+) (\d+)$/){
 					my $name = $1;
                                         my $count = $2;
                                         my $min = $3;
                                         my $max = $4;
                                         my $total = $5;
-                                        $h{$mtroot}->{"lustre.llite.count.$name"} = $count;
-                                        $h{$mtroot}->{"lustre.llite.min.$name"} = $min;
-                                        $h{$mtroot}->{"lustre.llite.max.$name"} = $max;
-                                        $h{$mtroot}->{"lustre.llite.total.$name"} = $total;
+                                        $h{$mtroot}->{"lustre.llite.$name.count"} = $count;
+                                        $h{$mtroot}->{"lustre.llite.$name.min"} = $min;
+                                        $h{$mtroot}->{"lustre.llite.$name.max"} = $max;
+                                        $h{$mtroot}->{"lustre.llite.$name.total"} = $total;
 				# Regs types
 				# List them all to ensure they match the metric defs below
 				# TODO: generate from array of ops that is also to be used in the metric defs
-				} elsif ( $line =~ /^(dirty_pages_hits|dirty_pages_misses|ioctl|open|close|mmap|seek|fsync|readdir|setattr|truncate|flock|getattr|create|link|unlink|symlink|mkdir|rmdir|mknod|rename|statfs|alloc_inode|setxattr|getxattr|getxattr_hits|listxattr|removexattr|inode_permission)\w+(\d+) samples [regs]$$/ ){
+				} elsif ( $line =~ /^(dirty_pages_hits|dirty_pages_misses|ioctl|open|close|mmap|seek|fsync|readdir|setattr|truncate|flock|getattr|create|link|unlink|symlink|mkdir|rmdir|mknod|rename|statfs|alloc_inode|setxattr|getxattr|getxattr_hits|listxattr|removexattr|inode_permission)\s+(\d+) samples [regs]$$/ ){
 					my $name = $1;
                                         my $count = $2;
-					$h{$mtroot}->{"lustre.llite.count.$name"} = $count;
+					$h{$mtroot}->{"lustre.llite.$name.count"} = $count;
 				}
 			}
+			close( STATS );
 		}
 	}
 
@@ -131,32 +168,17 @@ our $pmda = PCP::PMDA->new('lustre', 437);
 # llite stats - cluster 1
 # lnet stats - cluster 2
 
-$pmda->add_metric(pmda_pmid(0,1), PM_TYPE_STRING, $nfsclient_indom,
+$pmda->add_metric(pmda_pmid(0,1), PM_TYPE_STRING, $lustre_indom,
 		PM_SEM_INSTANT, pmda_units(0,0,0,0,0,0),
 		'lustre.llite.volume',
 		'Volume Name',
 		'');
-$pmda->add_metric(pmda_pmid(0,2), PM_TYPE_STRING, $nfsclient_indom,
+$pmda->add_metric(pmda_pmid(0,2), PM_TYPE_STRING, $lustre_indom,
 		PM_SEM_INSTANT, pmda_units(0,0,0,0,0,0),
 		'lustre.llite.superblock',
 		'Superblock Identifier',
 		'');
 
-#
-our @byte_stats = ('read_bytes', 'write_bytes', 'osc_read', 'osc_write');
-
-#
-our @page_stats = ('brw_read', 'brw_write');
-
-#
-our @reg_stats = ('dirty_pages_hits', 'dirty_pages_misses', 'ioctl', 'open', 'close',
-	   'mmap', 'seek', 'fsync', 'readdir', 'setattr',
-	   'truncate', 'flock', 'getattr', 'create', 'link', 'unlink',
-	   'symlink', 'mkdir', 'rmdir', 'mknod', 'rename',
-	   'statfs', 'alloc_inode', 'setxattr', 'getxattr', 'getxattr_hits', 'listxattr',
-	   'removexattr', 'inode_permission');
-
-our @all_ops = ( @common_ops, @v3only_ops, @v4_ops, @v41_ops );
 
 my $item = 1;
 for my $stat_name (@byte_stats) {
@@ -223,7 +245,7 @@ for my $stat_name (@reg_stats) {
 
 }
 
-&lustre_get_fs_stats
+&lustre_get_fs_stats;
 
 $lustre_indom = $pmda->add_indom($lustre_indom, {}, '', '');
 
