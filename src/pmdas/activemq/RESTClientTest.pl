f43ead19d877dc87b32f5d063ad87d798a99e98d
Ryan Doyle <rdoyle@aconex.com>
2015-01-22 15:53:00 +1100

    activemq PMDA: fix tests to reflect changes in the caching behaviour (caching JSON, not the request object)

--- src/pmdas/activemq/RESTClientTest.pl
+++ src/pmdas/activemq/RESTClientTest.pl
@@ -17,6 +17,7 @@ my $http_client = mock;
 my $cache = mock;
 my $successful_response = mock;
 my $failed_response = mock;
+my $decoded_json = { "status" => 200 };
 
 when($http_client)->get('http://localhost:1234/success')->then_return($successful_response);
 when($http_client)->get('http://localhost:1234/fail')->then_return($failed_response);
@@ -28,7 +29,7 @@ when($failed_response)->is_success->then_return(0);
 my $rest_client = RESTClient->new( $http_client, $cache, "localhost", "1234", "myuser", "mypass", "myrealm" );
 
 # successful requests return valid JSON
-is_deeply($rest_client->get("/success"), { "status" => 200 });
+is_deeply($rest_client->get("/success"), $decoded_json);
 
 # failed requests return undef
 is($rest_client->get("/fail"), undef);
@@ -36,7 +37,7 @@ is($rest_client->get("/fail"), undef);
 # data is stored in the cache when it does not exist in the cache
 when($cache)->get("/success")->then_return(undef);
 $rest_client->get("/success");
-verify($cache)->put('/success', $successful_response);
+verify($cache)->put('/success', $decoded_json);
 
 # data is fetched from the cache when is already exists in the cache
 when($cache)->get("/success_cached")->then_return($successful_response);
