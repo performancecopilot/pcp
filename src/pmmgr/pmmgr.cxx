performancecopilot/pcp

commit 1362154654f6a7f1fceeed5132b5607800d2c904

Add a pmcd.services metric reflecting PCP daemon usage
After finding a need to quickly know which PCP services
are active on a host, on a number of occassions now, I've
finally cracked up and added a pmcd.services metric that
reflect this state.  A line that looks alot like:
 services: pmcd pmmgr pmproxy pmwebd
is added to pcp(1) output as well, using this metric when
it is available (else, output is as it was before).

This is implemented in a portable way by ensuring daemons
all manage pidfiles the same way pmcd does ($PCP_RUN_DIR),
so a helper function is added to libpcp for each server to
make use of.  While there, pulled the open-coded stackdump
functions from pmcd and pmie into a shared spot also since
this is more generally useful (they were inconsistent too,
with the pmie variant being relatively incomplete).

A small improvement is added to the common pidfile code in
that an atexit handler is now registered to cleanup, which
pmcd was lacking (ala the log footer writer).  In addition
the pidfiles are made readonly after initial creation.

src/pmmgr/pmmgr.cxx
@@ -1218,6 +1218,10 @@ int main (int argc, char *argv[])
   if (js.size() == 0)
     js.push_back (new pmmgr_job_spec(default_config_dir));
 
+  // let pmdapmcd know pmmgr is currently running
+  if (__pmServerCreatePIDFile(pmProgname, PM_FATAL_ERR) < 0)
+    exit(1);
+
   // lose root privileges if we have them
   __pmSetProcessIdentity(username.c_str());
 

qa/366.out
@@ -410,6 +410,7 @@ QA output created by 366
 > 	pmda.uname
 > 	pmcd.numagents
 > 	pmcd.numclients
+> 	pmcd.services
 > 	pmcd.version
 > 	pmcd.build
 > 	pmcd.agent.status

qa/507
@@ -61,6 +61,7 @@ log mandatory on once {
     pmda.uname
     pmcd.numagents
     pmcd.numclients
+    pmcd.services
     pmcd.version
     pmcd.build
     pmcd.timezone
@@ -131,7 +132,7 @@ pminfo -fa src/pcpcmd pmcd.pmlogger
 echo
 echo 'pcp(1) output:'
 pcp -n src/root_irix -a src/pcpcmd | fgrep -v "$PCP_PMDAS_DIR/summary"
-    # dont use pmie output here cos archive doesn't have all metrics
+# dont use services/pmie output here cos archive doesn't have all metrics
 
 # success, all done
 status=0

qa/common.filter
@@ -642,6 +642,7 @@ _filter_torture_api()
 	-e '/kernel\.all\.cpu/s/valfmt: [01]/valfmt: 0-or-1/' \
 	-e '/^    hostname /d' \
 	-e '/^    license /d' \
+	-e '/^    services /d' \
 	-e '/^    aconex /d' \
 	-e '/^    apache /d' \
 	-e '/^    bonding /d' \

src/include/pcp/impl.h
@@ -300,6 +300,7 @@ extern void __pmFreeResultValues(pmResult *);
 extern char *__pmPDUTypeStr_r(int, char *, int);
 extern const char *__pmPDUTypeStr(int);			/* NOT thread-safe */
 extern void __pmDumpNameSpace(FILE *, int);
+extern void __pmDumpStack(FILE *);
 EXTERN int __pmLogReads;
 
 #ifdef PCP_DEBUG
@@ -659,6 +660,7 @@ typedef enum {
 extern int __pmServerHasFeature(__pmServerFeature);
 extern int __pmServerSetFeature(__pmServerFeature);
 extern int __pmServerClearFeature(__pmServerFeature);
+extern int __pmServerCreatePIDFile(const char *, int);
 extern int __pmServerAddPorts(const char *);
 extern int __pmServerAddInterface(const char *);
 extern void __pmServerSetLocalSocket(const char *);

src/libpcp/src/auxserver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Red Hat.
+ * Copyright (c) 2013-2014 Red Hat.
  * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -114,6 +114,42 @@ __pmServerSetServiceSpec(const char *spec)
 	serviceSpec = strdup(spec);
     else
 	serviceSpec = PM_SERVER_SERVICE_SPEC;
+}
+
+static void
+pidonexit(void)
+{
+    char        pidpath[MAXPATHLEN];
+
+    if (serviceSpec) {
+	snprintf(pidpath, sizeof(pidpath), "%s%c%s.pid",
+	    pmGetConfig("PCP_RUN_DIR"), __pmPathSeparator(), serviceSpec);
+	unlink(pidpath);
+    }
+}
+
+int
+__pmServerCreatePIDFile(const char *spec, int verbose)
+{
+    char        pidpath[MAXPATHLEN];
+    FILE        *pidfile;
+
+    if (!serviceSpec)
+	__pmServerSetServiceSpec(spec);
+
+    snprintf(pidpath, sizeof(pidpath), "%s%c%s.pid",
+	     pmGetConfig("PCP_RUN_DIR"), __pmPathSeparator(), spec);
+
+    if ((pidfile = fopen(pidpath, "w")) == NULL) {
+	if (verbose)
+	    fprintf(stderr, "Error: cannot open PID file %s\n", pidpath);
+	return -oserror();
+    }
+    atexit(pidonexit);
+    fprintf(pidfile, "%" FMT_PID, getpid());
+    fclose(pidfile);
+    chmod(pidpath, S_IRUSR | S_IRGRP | S_IROTH);
+    return 0;
 }
 
 void

src/libpcp/src/exports
@@ -442,3 +442,9 @@ PCP_3.4 {
     __pmSocketClosed;
     __pmLogPutResult2;
 } PCP_3.3;
+
+PCP_3.5 {
+  global:
+    __pmDumpStack;
+    __pmServerCreatePIDFile;
+} PCP_3.4;

src/libpcp/src/util.c
@@ -2118,4 +2118,58 @@ __pmMemoryUnmap(void *addr, size_t sz)
 {
     munmap(addr, sz);
 }
+
+#if HAVE_TRACE_BACK_STACK
+#include <libexc.h>
+#define MAX_DEPTH 30	/* max callback procedure depth */
+#define MAX_SIZE 48	/* max function name length */
+
+void
+__pmDumpStack(FILE *f)
+{
+    __uint64_t	call_addr[MAX_DEPTH];
+    char	*call_fn[MAX_DEPTH];
+    char	names[MAX_DEPTH][MAX_SIZE];
+    int		res;
+    int		i;
+
+    for (i = 0; i < MAX_DEPTH; i++)
+	call_fn[i] = names[i];
+    res = trace_back_stack(MAX_DEPTH, call_addr, call_fn, MAX_DEPTH, MAX_SIZE);
+    for (i = 1; i < res; i++) {
+#if defined(HAVE_64BIT_PTR)
+	fprintf(f, "  0x%016llx [%s]\n", call_addr[i], call_fn[i]);
+#else
+	fprintf(f, "  0x%08lx [%s]\n", (__uint32_t)call_addr[i], call_fn[i]);
 #endif
+    }
+}
+
+#elif HAVE_BACKTRACE
+#include <execinfo.h>
+#define MAX_DEPTH 30	/* max callback procedure depth */
+
+void
+__pmDumpStack(FILE *f)
+{
+    int		nframe;
+    void	*buf[MAX_DEPTH];
+    char	**symbols;
+    int		i;
+
+    nframe = backtrace(buf, MAX_DEPTH);
+    if (nframe < 1) {
+	fprintf(f, "backtrace -> %d frames?\n", nframe);
+	return;
+    }
+    symbols = backtrace_symbols(buf, nframe);
+    if (symbols == NULL) {
+	fprintf(f, "backtrace_symbols failed!\n");
+	return;
+    }
+    for (i = 1; i < nframe; i++)
+	fprintf(f, "  " PRINTF_P_PFX "%p [%s]\n", buf[i], symbols[i]);
+}
+#endif /* HAVE_BACKTRACE */
+
+#endif /* !IS_MINGW */

commit 03dbb1400afa9fdb53afae9555e684aaaa70b543

pmmgr: restore ability to invoke as non-pcp/non-root
commit 1362154654f broke pmmgr's ability to be run as any ordinary
user, by mandating that it be able to report its pid in $PCP_RUN_DIR.
This level of strictness is unprecedented and unnecessary, so we don't
do that any more.

src/pmmgr/pmmgr.cxx
@@ -1271,8 +1271,10 @@ int main (int argc, char *argv[])
     js.push_back (new pmmgr_job_spec(default_config_dir));
 
   // let pmdapmcd know pmmgr is currently running
-  if (__pmServerCreatePIDFile(pmProgname, PM_FATAL_ERR) < 0)
-    exit(1);
+  // NB: A failure from this call is of no significance: pmmgr is not
+  // required to be run as uid pcp or root, so must not fail for the
+  // mere inability to write into /var/run/pcp.
+  (void) __pmServerCreatePIDFile(pmProgname, PM_FATAL_ERR);
 
   // lose root privileges if we have them
   __pmSetProcessIdentity(username.c_str());