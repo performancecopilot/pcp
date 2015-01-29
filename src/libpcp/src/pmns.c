43485745d5ec901ee37671634da494d3f1f151a8
Ken McDonell <kenj@internode.on.net>
2015-01-29 11:36:39 +1100

    libpcp: change default pmLoadNameSpace behaviour
    Allow duplicates names to map to the same PMID by default.
    To take control over the handling of duplicates, use
    pmLoadASCIINameSpace().

--- src/libpcp/src/pmns.c
+++ src/libpcp/src/pmns.c
@@ -1339,12 +1339,13 @@ locate(const char *name, __pmnsNode *root)
  */
 
 /*
- * As of PCP 3.6, there is _only_ the ASCII version of the PMNS
+ * As of PCP 3.6, there is _only_ the ASCII version of the PMNS.
+ * As of PCP 3.10.3, the default is to allow duplicates in the PMNS.
  */
 int
 pmLoadNameSpace(const char *filename)
 {
-    return pmLoadASCIINameSpace(filename, 0);
+    return pmLoadASCIINameSpace(filename, 1);
 }
 
 int
