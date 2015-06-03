performancecopilot/pcp

commit d21bb6c017696233d736d0c518eeeb742eb4d36f

pmie: remove unnecessary 2 second delay at live startup
Noticed that pmie was taking a while to start up and dump out the
result from initial sample for qa312.  ltrace pointed toward some
1 second calls to nanosleep, and following that led to unexpected
time deltas for scheduled tasks vs the initial notion of "now".
Turned out those delays were due to some places where we calcualte
'sched_time = now + 1sec' (or similar) - seems to be unnecessary,
from QA testing so far.

src/pmie/src/pmie.c
@@ -707,7 +707,7 @@ getargs(int argc, char *argv[])
     }
 
     /* initialize time */
-    now = archives ? first : getReal() + 1.0;
+    now = archives ? first : getReal() + 0.000000001;
     zoneInit();
     reflectTime(dfltDelta);
 
@@ -775,7 +775,7 @@ getargs(int argc, char *argv[])
 
     /* really parse time window */
     if (!archives) {
-	now = getReal() + 1.0;
+	now = getReal() + 0.000000001;
 	reflectTime(dfltDelta);
     }
     __pmtimevalFromReal(now, &tv1);

qa/312
@@ -25,7 +25,7 @@ trap "rm -f $tmp.*; exit \$status" 0 1 2 3 15
 echo 'load = sample.load;' | pmie -v -t 1hour >$tmp.out 2>$tmp.err &
 pmie_pid=$!
 
-sleep 2
+pmsleep 0.5
 $signal -s TERM $pmie_pid
 wait
 

qa/518
@@ -39,7 +39,7 @@ sleep 2
 # 518.out expects exactly three evaluation rounds to be complete by the
 # time the following operation is invoked.  If sudo/pmie is too slow,
 # it'll lose the race (actions<5).  If this thread is too slow, pmie
-# will go ahead (actions>5).  So we tolerate values in the range 4,5,6
+# will go ahead (actions>5).  So we tolerate values in the range 4,5,6-9
 # for the true, false and action counts, but the counts for each must
 # be equal.
 
@@ -79,9 +79,9 @@ s/)$/ )/
 '$debug' == "true"		{ print "state=" state " want=" want ": " $0 }
 state==1 && /'`echo $tmp.pmie | sed -e s@/@\\\\\\\\/@g`' \(2 rule\)/	{ print; want = 1; next }
 state==1 && / \(actions=/	{ if ($3 == $5 && $5 == $9) {
-				      if ($3 >= 4 && $3 <= 6) $3 = "OK"
-				      if ($5 >= 4 && $5 <= 6) $5 = "OK"
-				      if ($9 >= 4 && $9 <= 6) $9 = "OK"
+				      if ($3 >= 4 && $3 <= 9) $3 = "OK"
+				      if ($5 >= 4 && $5 <= 9) $5 = "OK"
+				      if ($9 >= 4 && $9 <= 9) $9 = "OK"
 				  }
 				}
 state==1 && / rule\)/		{ want = 0 }

commit f0bf06c607217977ed06aaa0c79f261429b0de5c

pmie: prevent premature rule evaluation in pmie live mode
Resolve an issue introduced in commit d21bb6c017696 - some
code Ken and I thought was unnecessary turned out to be in
use after all.  The rest of that commit remains, it's just
the snippet of code that re-adjusts the Task scheduling vs
the current time that is affected by slow reading on stdin
(which can happen if rules are manually entered).

Added a new qa/862 test to exercise the situation and fix.

src/pmie/src/pmie.c
@@ -771,6 +771,12 @@ getargs(int argc, char *argv[])
 	dumpRules();
 #endif
 
+    /* really parse time window */
+    if (!archives) {
+	now = getReal();
+	reflectTime(dfltDelta);
+    }
+
     if (checkFlag)
 	exit(errs == 0 ? 0 : 1);	/* exit 1 for syntax errors ...
 					 * suggestion from 

qa/862
@@ -1,0 +1,41 @@
+#!/bin/sh
+# PCP QA Test No. 862
+# Exercise pmie handling of interactive (stdin) configs.
+#
+# Copyright (c) 2015 Red Hat.  All Rights Reserved.
+#
+
+seq=`basename $0`
+echo "QA output created by $seq"
+
+# get standard environment, filters and checks
+. ./common.product
+. ./common.filter
+. ./common.check
+
+status=1	# failure is the default!
+$sudo rm -rf $tmp.* $seq.full
+trap "cd $here; rm -rf $tmp.*; exit \$status" 0 1 2 3 15
+
+# real QA test starts here
+echo Silence is golden
+( sleep 2 && echo 'one = sample.long.one;' ) | pmie -q -T 1
+
+# The above should exit immediately with no output, and should
+# not produce a task scheduling dump state ala...
+# 
+# sleepTight: negative delay (-1.995965). sched=1433305770.991639, cur=1433305772.987604
+# This sleepTight() entry: Wed Jun  3 14:29:32 2015.987603
+# Harvest children done: Wed Jun  3 14:29:32 2015.987603
+# Want sleepTight (eval) until: Wed Jun  3 14:29:30 2015.991638
+# This Task dump @ 0x7f020f9acce0
+#   nth=0 delta=10.000 tick=0 next=(nil) prev=(nil)
+#   eval time: Wed Jun  3 14:29:30 2015.991638
+#   retry time: Thu Jan  1 10:00:00 1970.000000
+#   host=localhost.localdomain (up)
+#   rules:
+#     one
+
+# success, all done
+status=0
+exit

qa/862.out
@@ -1,0 +1,2 @@
+QA output created by 862
+Silence is golden

qa/group
@@ -1017,6 +1017,7 @@ cgroups
 845 pmda.mounts local
 860 pmda pmda.proc ostest local
 861 pmcd pmda.pmcd local
+862 pmie local
 873 pmda.linux local
 874 valgrind pmval libpcp local
 875 libpcp local valgrind