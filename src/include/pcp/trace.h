b6110c2e22b214850be652df601754dc61015d7a
Nathan Scott <nathans@redhat.com>
2015-08-27 09:34:11 +1000

    build: fix LGPL license annotations in headers+libraries
    Correct several cut+paste license errors, where GPL annotation
    had been inadvertently applied to a handful of sources in LGPL
    libraries.
    
    With the exception of getdate.y, this resolves the issues from
    https://github.com/performancecopilot/pcp/issues/44

--- src/include/pcp/trace.h
+++ src/include/pcp/trace.h
@@ -1,15 +1,15 @@
 /*
  * Copyright (c) 1997 Silicon Graphics, Inc.  All Rights Reserved.
- * 
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
  */
 #ifndef PCP_TRACE_H
 #define PCP_TRACE_H
