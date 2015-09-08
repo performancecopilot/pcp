b5616d08dfc20d4be8ae1eb427941eaa418cfc0c
Nathan Scott <nathans@redhat.com>
2015-09-08 10:22:56 +1000

    libpcp_qmc, libpcp_qed: update source license annotations
    These two libraries had missing or abbreviated LGPL notices,
    or inadvertent use of GPL instead of LGPL, for some sources.
    
    Resolves https://github.com/performancecopilot/pcp/issues/44

--- src/libpcp_qed/src/qed_colorpicker.h
+++ src/libpcp_qed/src/qed_colorpicker.h
@@ -1,24 +1,19 @@
 /*
-** Copyright (C) 2014 Red Hat.
-** Copyright (C) 1999-2007 Trolltech AS.  All rights reserved.
-**
-** This file is derived from part of the QtGui module of the Qt Toolkit.
-**
-** This file may be used under the terms of the GNU General Public
-** License version 2.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of
-** this file.  Please review the following information to ensure GNU
-** General Public Licensing requirements will be met:
-** http://www.trolltech.com/products/qt/opensource.html
-**
-** If you are unsure which license is appropriate for your use, please
-** review the following information:
-** http://www.trolltech.com/products/qt/licensing.html or contact the
-** sales department at sales@trolltech.com.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**/
+ * Copyright (C) 2014 Red Hat.
+ * Copyright (C) 1999-2007 Trolltech AS.  All rights reserved.
+ *
+ * This file is derived from part of the QtGui module of the Qt Toolkit.
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ */
 #ifndef QED_COLORPICKER_H
 #define QED_COLORPICKER_H
 
