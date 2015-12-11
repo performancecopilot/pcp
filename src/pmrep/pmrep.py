performancecopilot/pcp

commit 876645e03864175fded38771d8c8b38bc59d7011

pmrep: adjust examples for RHBZ#1289912
The easiest solution for pmrep command line handling needs a change how
the context is being setup. But looks like it also revealed a corner
case issue in Python PMAPI command like option handling. Adjust the
examples to make to err on the safe side for the time being regardless
of the actual root cause. Adjust the allcache derived examples while at
it.

src/pmrep/pmrep.py
@@ -1299,7 +1299,7 @@ class PMReporter(object):
                 val = str(list(values[i])[j][2])
                 self.zabbix_metrics.append(ZabbixMetric(self.zabbix_host, key, val, ts))
 
-        # Send when need
+        # Send when needed
         if self.context.type == PM_CONTEXT_ARCHIVE:
             if len(self.zabbix_metrics) >= self.zabbix_interval:
                 send_to_zabbix(self.zabbix_metrics, self.zabbix_server, self.zabbix_port)
@@ -1311,7 +1311,7 @@ class PMReporter(object):
 
     def connect(self):
         """ Establish a PMAPI context to archive, host or local, via args """
-        self.context = pmapi.pmContext.fromOptions(self.opts, sys.argv)
+        self.context = pmapi.pmContext.fromOptions(self.opts, sys.argv, self.opts.pmGetOptionContext())
 
 if __name__ == '__main__':
     try:

man/html/guide.html
@@ -217,7 +217,7 @@ Monitor live CPU load, memory usage, and disk write operations per partition wit
 <TABLE WIDTH="100%" BORDER=0 CELLPADDING=10 CELLSPACING=20>
         <TR><TD BGCOLOR="#e2e2e2" WIDTH="70%"><BR><IMG SRC="images/stepfwd_on.png" ALT="" WIDTH=16 HEIGHT=16 BORDER=0>&nbsp;&nbsp;&nbsp;
 Monitor live process creation rate and free/used memory with two second interval printing timestamps and using GBs for output values in CSV format:<BR><B>
-<BR> $ <a href="http://man7.org/linux/man-pages/man1/pmrep.1.html">pmrep</a> -p -b GB -t 2sec -o csv kernel.all.sysfork mem.util.free mem.util.used -h acme.com</B>
+<BR> $ <a href="http://man7.org/linux/man-pages/man1/pmrep.1.html">pmrep</a> -h acme.com -p -b GB -t 2sec -o csv kernel.all.sysfork mem.util.free mem.util.used</B>
 </TD></TR>
 </TABLE>
 
@@ -320,7 +320,7 @@ Dump past CPU load, memory usage, and disk write operations per partition in an 
 <TABLE WIDTH="100%" BORDER=0 CELLPADDING=10 CELLSPACING=20>
         <TR><TD BGCOLOR="#e2e2e2" WIDTH="70%"><BR><IMG SRC="images/stepfwd_on.png" ALT="" WIDTH=16 HEIGHT=16 BORDER=0>&nbsp;&nbsp;&nbsp;
 Dump past CPU load, memory usage, and disk write operations per partition in an archive with extended header using MBs but without interpolation between 9 AM and 10 AM:<BR><B>
-<BR> $ <a href="http://man7.org/linux/man-pages/man1/pmrep.1.html">pmrep</a> -p -u -b MB -x -S @09:00 -T @10:00 kernel.all.load mem.util.used disk.partitions.write -a acme.com/20140902</B>
+<BR> $ <a href="http://man7.org/linux/man-pages/man1/pmrep.1.html">pmrep</a> -a acme.com/20140902 -p -u -b MB -x -S @09:00 -T @10:00 kernel.all.load mem.util.used disk.partitions.write</B>
 </TD></TR>
 </TABLE>
 
@@ -490,7 +490,7 @@ Create a file containing definitions of derived metrics and point PCP_DERIVED_CO
 <TABLE WIDTH="100%" BORDER=0 CELLPADDING=10 CELLSPACING=20>
         <TR><TD BGCOLOR="#e2e2e2" WIDTH="70%"><BR><IMG SRC="images/stepfwd_on.png" ALT="" WIDTH=16 HEIGHT=16 BORDER=0>&nbsp;&nbsp;&nbsp;
 Define a derived metric on the command line and monitor it with regular metrics:</BR><B>
-<BR> $ <a href="http://man7.org/linux/man-pages/man1/pmrep.1.html">pmrep</a> -t 2sec -p -b MB -e "mem.util.allcache = mem.util.cached + mem.util.slab + mem.util.bufmem" mem.util.free mem.util.allcache mem.util.used
+<BR> $ <a href="http://man7.org/linux/man-pages/man1/pmrep.1.html">pmrep</a> -t 2sec -p -b MB -e "mem.util.allcache = mem.util.bufmem + mem.util.cached + mem.util.slab" mem.util.free mem.util.allcache mem.util.used
 </TD></TR>
 </TABLE>
 

src/pmrep/pmrep.conf
@@ -72,7 +72,7 @@ pswpout.width = 10
 
 # vmstat example
 [vmstat]
-derived=mem.util.allcache=mem.util.cached+mem.util.slab,kernel.all.cpu.alluser=kernel.all.cpu.user+kernel.all.cpu.nice
+derived=mem.util.allcache=mem.util.bufmem+mem.util.cached+mem.util.slab,kernel.all.cpu.alluser=kernel.all.cpu.user+kernel.all.cpu.nice
 header = yes
 unitinfo = no
 globals = no

src/pmrep/pmrep.conf.5
@@ -432,7 +432,7 @@ space_scale = MB
 [global]
 kernel.all.sysfork = fork/s,,,,8
 allcache = mem.util.allcache
-allcache.formula = mem.util.cached+mem.util.slab
+allcache.formula = mem.util.bufmem + mem.util.cached + mem.util.slab
 
 [db1]
 source = db-host1.example.com

commit 32086ef5377fab823652bd2a85d29e00e61103fc

pmrep: further command line options guards, qa fix
Noticed qa/1069 started failing with commit 876645e038 as it
allows a None value to be passed into the context builder.

@@ -1,9 +1,13 @@
 QA output created by 1069
 == basic live mode reporting
-          s.l.one
-
-HH:MM:SS      N/A
-HH:MM:SS        1
+Traceback (most recent call last):
+  File "/bin/pmrep", line 1320, in <module>
+    P.connect()
+  File "/bin/pmrep", line 1313, in connect
+    self.context = pmapi.pmContext.fromOptions(self.opts, sys.argv, self.opts.pmGetOptionContext())
+  File "/usr/lib64/python3.3/site-packages/pcp/pmapi.py", line 1086, in fromOptions
+    if (typed <= 0):
+TypeError: unorderable types: NoneType() <= int()
 == basic archive mode reporting
           s.seconds  s.milliseconds
                util            util

Guard against this by making the pmapi wrapper code allow None,
and also guard against the BZ 1289912 situation by enforcing the
use of POSIX style argument parsing (this will need a more complex
fix in the python wrapper - leave that for next time, at this late
stage of a release).

src/pmrep/pmrep.py
@@ -164,6 +164,7 @@ class PMReporter(object):
         self.check = 0
         self.format = None # output format
         self.opts = self.options()
+        pmapi.c_api.pmSetOptionFlags(pmapi.c_api.PM_OPTFLAG_POSIX) # RHBZ#1289912
 
         # Configuration directives
         self.keys = ('source', 'output', 'derived', 'header', 'unitinfo',

src/python/pcp/pmapi.py
@@ -1083,7 +1083,7 @@ class pmContext(object):
             the typed/index parameters can be used to setup several
             contexts based on the given command line parameters.
         """
-        if (typed <= 0):
+        if typed == None or typed <= 0:
             options.need_reset = True
             if c_api.pmGetOptionsFromList(argv):
                 raise pmUsageErr