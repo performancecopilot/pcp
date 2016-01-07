6c9a30c22258a37a6c99848c1f37042fbc6d63cb
Frank Ch. Eigler <fche@redhat.com>
2016-01-07 15:46:11 -0500

    grafana 1.9.1 - png refreshing
    A patch applied to grafana 1.9.1 (included as PATCH-1) permits
    graphite-png rendered img objects to be auto-refreshed just as
    the json/flot rendered ones.  Enabled 5m auto-refresh on the
    default-png.json dashboard correspondingly.
    
    Rebuilt the grafana 1.9.1 distribution.  Removed test/ subdirectory.

--- grafana/plugins/custom.panel.example/module.js
+++ grafana/plugins/custom.panel.example/module.js
@@ -1,4 +1,4 @@
-/*! grafana - v1.9.1 - 2014-12-29
- * Copyright (c) 2014 Torkel Ödegaard; Licensed Apache License */
+/*! grafana - v1.9.1 - 2016-01-07
+ * Copyright (c) 2016 Torkel Ödegaard; Licensed Apache License */
 
-define(["angular","app","lodash","require"],function(a,b,c){var d=a.module("grafana.panels.custom",[]);b.useModule(d),d.controller("CustomPanelCtrl",["$scope","panelSrv",function(a,b){a.panelMeta={description:"Example plugin panel"};var d={};c.defaults(a.panel,d),a.init=function(){b.init(a)},a.init()}])});
+define(["angular","app","lodash","require"],function(a,b,c){"use strict";var d=a.module("grafana.panels.custom",[]);b.useModule(d),d.controller("CustomPanelCtrl",["$scope","panelSrv",function(a,b){a.panelMeta={description:"Example plugin panel"};var d={};c.defaults(a.panel,d),a.init=function(){b.init(a)},a.init()}])});
