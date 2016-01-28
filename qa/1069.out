performancecopilot/pcp

commit 19237bb9dcf97102d382fd2075015a038eea79fa

pmrep: support -F for csv/stdout
Allow optionally using -F with csv/stdout (so it'll be same as the shell
redirect this far), wrap printing to possibly allow future verbose/quiet
option enhancements.

src/pmrep/pmrep.py
@@ -188,6 +188,7 @@ class PMReporter(object):
         self.source = "local:"
         self.output = OUTPUT_STDOUT
         self.outfile = None
+        self.writer = None
         self.pmi = None
         self.derived = None
         self.header = 1
@@ -319,7 +320,7 @@ class PMReporter(object):
         opts.pmSetLongOption("config", 1, "c", "FILE", "config file path")
         opts.pmSetLongOption("check", 0, "C", "", "check config and metrics and exit")
         opts.pmSetLongOption("output", 1, "o", "OUTPUT", "output target: archive, csv, stdout (default), or zabbix")
-        opts.pmSetLongOption("output-file", 1, "F", "OUTFILE", "output file (with -o archive)")
+        opts.pmSetLongOption("output-file", 1, "F", "OUTFILE", "output file")
         opts.pmSetLongOption("derived", 1, "e", "FILE|DFNT", "derived metrics definitions")
         #opts.pmSetLongOptionGuiMode()     # -g/--guimode # RHBZ#1289910
         opts.pmSetLongOptionDebug()        # -D/--debug
@@ -381,6 +382,9 @@ class PMReporter(object):
         elif opt == 'F':
             if os.path.exists(optarg + ".index"):
                 sys.stderr.write("Archive %s already exists.\n" % optarg)
+                sys.exit(1)
+            if os.path.exists(optarg):
+                sys.stderr.write("File %s already exists.\n" % optarg)
                 sys.exit(1)
             self.outfile = optarg
         elif opt == 'e':
@@ -801,15 +805,17 @@ class PMReporter(object):
             self.delay = 1
             self.interpol = 1
 
-        # DBG_TRACE_APPL1 == 4096
-        if "pmDebug" in dir(self.context) and self.context.pmDebug(4096):
-            print("Known config file keywords: " + str(self.keys))
-            print("Known metric spec keywords: " + str(self.metricspec))
-
-        # Printing format and headers
-        if self.format == None:
+        # Print preparation
+        self.prepare_writer()
+        if self.output == OUTPUT_STDOUT:
             self.prepare_stdout()
 
+        # DBG_TRACE_APPL1 == 4096
+        if "pmDebug" in dir(self.context) and self.context.pmDebug(4096):
+            self.writer.write("Known config file keywords: " + str(self.keys) + "\n")
+            self.writer.write("Known metric spec keywords: " + str(self.metricspec) + "\n")
+
+        # Headers
         if self.extheader == 1:
             self.extheader = 0
             self.write_ext_header()
@@ -972,8 +978,19 @@ class PMReporter(object):
         if self.output == OUTPUT_ZABBIX:
             self.write_zabbix(tstamp, values)
 
+    def prepare_writer(self):
+        """ Prepare generic stdout writer """
+        if not self.writer:
+            if self.output == OUTPUT_ARCHIVE or \
+               self.output == OUTPUT_ZABBIX or \
+               self.outfile == None:
+                self.writer = sys.stdout
+            else:
+                self.writer = open(self.outfile, 'wb')
+        return self.writer
+
     def prepare_stdout(self):
-        """ Prepare stdout format string """
+        """ Prepare stdout output """
         index = 0
         if self.timestamp == 0:
             #self.format = "{:}{}"
@@ -1047,43 +1064,43 @@ class PMReporter(object):
                 timezone = self.context.pmGetArchiveLabel().tz
                 timezone += " (creation, current is " + currtz + ")"
 
-        print(comm)
+        self.writer.write(comm + "\n")
         if self.context.type == PM_CONTEXT_ARCHIVE:
-            print(comm + "  archive: " + self.source)
-        print(comm + "     host: " + host)
-        print(comm + " timezone: " + timezone)
-        print(comm + "    start: " + time.asctime(time.localtime(self.opts.pmGetOptionStart())))
-        print(comm + "      end: " + time.asctime(time.localtime(endtime)))
-        print(comm + "  samples: " + str(samples))
+            self.writer.write(comm + "  archive: " + self.source + "\n")
+        self.writer.write(comm + "     host: " + host + "\n")
+        self.writer.write(comm + " timezone: " + timezone + "\n")
+        self.writer.write(comm + "    start: " + time.asctime(time.localtime(self.opts.pmGetOptionStart())) + "\n")
+        self.writer.write(comm + "      end: " + time.asctime(time.localtime(endtime)) + "\n")
+        self.writer.write(comm + "  samples: " + str(samples) + "\n")
         if not (self.context.type == PM_CONTEXT_ARCHIVE and not self.interpol):
-            print(comm + " interval: " + str(float(self.interval)) + " sec")
-            print(comm + " duration: " + str(duration) + " sec")
+            self.writer.write(comm + " interval: " + str(float(self.interval)) + " sec\n")
+            self.writer.write(comm + " duration: " + str(duration) + " sec\n")
         else:
-            print(comm + " interval: N/A")
-            print(comm + " duration: N/A")
-        print(comm)
+            self.writer.write(comm + " interval: N/A\n")
+            self.writer.write(comm + " duration: N/A\n")
+        self.writer.write(comm + "\n")
 
     def write_header(self):
         """ Write metrics header """
         if self.output == OUTPUT_ARCHIVE:
-            sys.stdout.write("Recording %s (%d metrics)" % (self.outfile, len(self.metrics)))
+            self.writer.write("Recording %s (%d metrics)" % (self.outfile, len(self.metrics)))
             if self.runtime != -1:
-                sys.stdout.write(":\n%s samples(s) with %.1f sec interval ~ %d sec duration.\n" % (self.samples, float(self.interval), self.runtime))
+                self.writer.write(":\n%s samples(s) with %.1f sec interval ~ %d sec duration.\n" % (self.samples, float(self.interval), self.runtime))
             elif self.samples:
                 duration = (self.samples - 1) * int(self.interval)
-                sys.stdout.write(":\n%s samples(s) with %.1f sec interval ~ %d sec duration.\n" % (self.samples, float(self.interval), duration))
+                self.writer.write(":\n%s samples(s) with %.1f sec interval ~ %d sec duration.\n" % (self.samples, float(self.interval), duration))
             else:
-                sys.stdout.write("...")
+                self.writer.write("...")
                 if self.context.type != PM_CONTEXT_ARCHIVE:
-                    sys.stdout.write(" (Ctrl-C to stop)")
-                sys.stdout.write("\n")
+                    self.writer.write(" (Ctrl-C to stop)")
+                self.writer.write("\n")
             return
 
         if self.output == OUTPUT_CSV:
-            sys.stdout.write("Time")
+            self.writer.write("Time")
             for metric in self.metrics:
-                sys.stdout.write(self.delimiter + metric)
-            sys.stdout.write("\n")
+                self.writer.write(self.delimiter + metric)
+            self.writer.write("\n")
 
         if self.output == OUTPUT_STDOUT:
             names = ["", self.delimiter] # no timestamp on header line
@@ -1106,28 +1123,28 @@ class PMReporter(object):
             del names[-1]
             del units[-1]
             del insts[-1]
-            print(self.format.format(*tuple(names)))
+            self.writer.write(self.format.format(*tuple(names)) + "\n")
             if prnti == 1:
-                print(self.format.format(*tuple(insts)))
+                self.writer.write(self.format.format(*tuple(insts)) + "\n")
             if self.unitinfo:
-                print(self.format.format(*tuple(units)))
+                self.writer.write(self.format.format(*tuple(units)) + "\n")
 
         if self.output == OUTPUT_ZABBIX:
             if self.context.type == PM_CONTEXT_ARCHIVE:
                 self.delay = 0
                 self.interpol = 0
                 self.zabbix_interval = 250 # See zabbix_sender(8)
-                sys.stdout.write("Sending archived metrics to Zabbix server %s...\n(Ctrl-C to stop)\n" % self.zabbix_server)
+                self.writer.write("Sending archived metrics to Zabbix server %s...\n(Ctrl-C to stop)\n" % self.zabbix_server)
                 return
 
-            sys.stdout.write("Sending metrics to Zabbix server %s every %d sec" % (self.zabbix_server, self.zabbix_interval))
+            self.writer.write("Sending metrics to Zabbix server %s every %d sec" % (self.zabbix_server, self.zabbix_interval))
             if self.runtime != -1:
-                sys.stdout.write(":\n%s samples(s) with %.1f sec interval ~ %d sec runtime.\n" % (self.samples, float(self.interval), self.runtime))
+                self.writer.write(":\n%s samples(s) with %.1f sec interval ~ %d sec runtime.\n" % (self.samples, float(self.interval), self.runtime))
             elif self.samples:
                 duration = (self.samples - 1) * int(self.interval)
-                sys.stdout.write(":\n%s samples(s) with %.1f sec interval ~ %d sec runtime.\n" % (self.samples, float(self.interval), duration))
+                self.writer.write(":\n%s samples(s) with %.1f sec interval ~ %d sec runtime.\n" % (self.samples, float(self.interval), duration))
             else:
-                sys.stdout.write("...\n(Ctrl-C to stop)\n")
+                self.writer.write("...\n(Ctrl-C to stop)\n")
 
     def write_archive(self, timestamp, values):
         """ Write an archive record """
@@ -1198,8 +1215,7 @@ class PMReporter(object):
                     line += format(list(values[i])[j][2], fmt)
                 else:
                     line += str(list(values[i])[j][2])
-        sys.stdout.write(line)
-        sys.stdout.write("\n")
+        self.writer.write(line + "\n")
 
     def write_stdout(self, timestamp, values):
         """ Write a line to stdout """
@@ -1275,7 +1291,7 @@ class PMReporter(object):
                 line.append(self.delimiter)
 
         del line[-1]
-        #print('{}'.join(fmt).format(*tuple(line)))
+        #self.writer.write('{}'.join(fmt).format(*tuple(line)) + "\n")
         index = 0
         nfmt = ""
         for f in fmt:
@@ -1285,7 +1301,7 @@ class PMReporter(object):
             index += 1
         l = len(str(index-1)) + 2
         nfmt = nfmt[:-l]
-        print(nfmt.format(*tuple(line)))
+        self.writer.write(nfmt.format(*tuple(line)) + "\n")
 
     def write_zabbix(self, timestamp, values):
         """ Write (send) metrics to a Zabbix server """

commit b823e76fad77baeb8e14edc3f54436fedd7f0f0b

pmrep: extend QA coverage
- adjust the test config file to be as per man page
  (output unchanged as cmd line opts override these)
- some additional output testing, revealed a P3 bug
  introduced in 19237bb, fix sent off-list already
- archives/sample-secs has timezone :Australia/Melbourne,
  so exercise that unusual string
- some additional unit conversion tests
- un/interpolated archive tests

We should now have very good pmrep coverage in QA.

qa/1069
@@ -29,6 +29,7 @@ _live_filter()
 }
 
 log="--archive $here/archives/sample-secs -z"
+log2="--archive $here/archives/20130706 -z -O30m"
 
 # real QA test starts here
 echo "== basic live mode reporting"
@@ -42,13 +43,26 @@ pmrep -z -p -s 5 -t 2 -P 2 --archive $here/archives/sample-secs \
 
 echo "== basic configuration file handling"
 echo > $tmp.config <<EOF
-secs = sample.seconds
-msecs = sample.milliseconds
+[options]
+version = 1
+source = www.example.com
+samples = 1
+interval = 1
 EOF
 pmrep -s 5 -t 2 $log -c $tmp.config sample
 
+echo "== exercise various output options "
+pmrep -s 5 -t 2 $log -c $tmp.config -o stdout -p -w 20 sample
+echo "---"
+pmrep -s 5 -t 2 $log -c $tmp.config -C -x sample
+echo "---"
+pmrep -s 5 -t 2 $log -c $tmp.config -G -H -U sample
+
 echo "== exercise raw counter mode "
 pmrep -s 5 -t 2 $log -c $tmp.config -r sample
+
+echo "== exercise timezone setting "
+pmrep -s 5 -t 2 --archive $here/archives/sample-secs -p -Z UTC sample
 
 echo "== exercise CSV and alternate delimiters"
 pmrep -s 5 -t 2 $log -c $tmp.config -o csv -p -P 1 sample
@@ -63,7 +77,9 @@ echo "== exercise repeated headers option"
 pmrep -s 5 -t 2 $log -E 2 sample.seconds
 
 echo "== exercise units conversion options"
-pmrep -s 5 -t 2 $log -p -y microsec sample
+pmrep -s 5 -t 2 $log2 -p -b MB -P 0 mem.util.free mem.util.used
+pmrep -s 5 -t 2 $log2 -p -q "count x 10^-2" -P 0 -w 15 disk.dev.read
+pmrep -s 5 -t 2 $log  -p -y microsec sample
 
 echo "== derived metrics configuration file"
 cat > $tmp.derived <<EOF
@@ -93,6 +109,10 @@ echo '[options]' > $tmp.vconfig
 echo 'version = 99' >> $tmp.vconfig
 cat $tmp.config >> $tmp.vconfig
 pmrep -s 5 -t 2 $log -c $tmp.vconfig sample
+
+echo "== un/interpolated archive mode reporting"
+pmrep -s 5 -t 2 $log2 -p    mem.util.free
+pmrep -s 5 -t 2 $log2 -p -u mem.util.free
 
 # success, all done
 echo "== done"

qa/1069.out
@@ -26,6 +26,34 @@ Interval can't be less than 1 second.
       1.000           1.000
       1.000           1.000
       1.000           1.000
+== exercise various output options
+                     s.seconds        s.milliseconds
+                          util                  util
+20:36:45                   N/A                   N/A
+20:36:47                   N/A                   N/A
+20:36:49                 1.000                 1.000
+20:36:51                 1.000                 1.000
+20:36:53                 1.000                 1.000
+---
+
+  archive: /tmp/pcp/var/lib/pcp/testsuite/archives/sample-secs
+     host: rattle
+ timezone: :Australia/Melbourne (creation, current is EET-2.0)
+    start: Mon May  1 13:36:45 2000
+      end: Mon May  1 13:36:53 2000
+  metrics: 2
+  samples: 5
+ interval: 2.0 sec
+ duration: 8 sec
+
+  s.seconds  s.milliseconds
+       util            util
+---
+        N/A             N/A
+        N/A             N/A
+      1.000           1.000
+      1.000           1.000
+      1.000           1.000
 == exercise raw counter mode 
   s.seconds  s.milliseconds
         sec        millisec
@@ -34,6 +62,14 @@ Interval can't be less than 1 second.
      380436   380435679.381
      380438   380437679.382
      380440   380439679.413
+== exercise timezone setting
+          s.seconds  s.milliseconds
+               util            util
+10:36:45        N/A             N/A
+10:36:47        N/A             N/A
+10:36:49      1.000           1.000
+10:36:51      1.000           1.000
+10:36:53      1.000           1.000
 == exercise CSV and alternate delimiters
 Time,sample.seconds,sample.milliseconds
 2000-05-01 20:36:45,N/A,N/A
@@ -62,6 +98,21 @@ ok
        util
       1.000
 == exercise units conversion options
+          m.u.free  m.u.used
+                MB        MB
+00:47:01        37      5926
+00:47:03        37      5926
+00:47:05        37      5926
+00:47:07        37      5926
+00:47:09        37      5926
+                 d.d.read         d.d.read         d.d.read         d.d.read         d.d.read         d.d.read
+                      sda              sdb              sdc              hdc              sdd              sde
+          count x 10^-2/s  count x 10^-2/s  count x 10^-2/s  count x 10^-2/s  count x 10^-2/s  count x 10^-2/s
+00:47:01              N/A              N/A              N/A              N/A              N/A              N/A
+00:47:03                0             3000               50                0                0              100
+00:47:05                0             3000                0                0                0              100
+00:47:07                0             3000               50                0                0              100
+00:47:09                0             3000               50                0                0               50
           s.seconds  s.milliseconds
                util            util
 20:36:45        N/A             N/A
@@ -103,4 +154,19 @@ ok
       1.000           1.000
 == exercise bad config version
 Incompatible configuration file version (read v99, need v1).
+== un/interpolated archive mode reporting
+          m.u.free
+             Kbyte
+00:47:01     38220
+00:47:03     38220
+00:47:05     38220
+00:47:07     38220
+00:47:09     38220
+          m.u.free
+             Kbyte
+00:48:01     36832
+00:49:01     38236
+00:50:01     37932
+00:51:01     37836
+00:52:01     38744
 == done