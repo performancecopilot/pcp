1f80408fd3f204c3121685614db45d1f7c33f6f9
Nathan Scott <nathans@redhat.com>
2016-02-05 13:59:30 +1100

    qa: add an archive and producer for Markos planned testing

---
+++ qa/archives/mk.rep.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+# 
+# Recipe for creating the rep archive, with the following matrix
+# of metric type / semantics / units ...
+#
+#    string / discrete / none - hinv.machine
+#    string / instant  / none - kernel.uname.sysname
+#
+#    int / instant / none - kernel.all.lastpid
+#    int / instant / byte - mem.util.used
+#    int / instant / count - network.tcpconn.close
+#
+#    int / discrete / none - hinv.ncpu
+#    int / discrete / byte - mem.physmem
+#    int / discrete / count - hinv.nfilesys
+#    int / discrete / c/s - kernel.all.hz
+#
+#    int / counter / none - sample.wrap.long
+#    int / counter / sec - kernel.percpu.cpu.user
+#    int / counter / byte - disk.all.read_bytes
+#    int / counter / count - kernel.all.sysfork
+#
+#    float / instant / none - kernel.all.load
+#
+#    float / discrete / none - hinv.cpu.bogomips
+#    float / discrete / count - hinv.cpu.clock
+#    float / discrete / b/s - network.interface.speed
+#
+
+. $PCP_DIR/etc/pcp.env
+
+here=`pwd`
+tmp=/tmp/$$
+
+if [ "$PCP_PLATFORM" != "linux" ]
+then
+    echo "$0: Error: requires Linux kernel metrics"
+    exit 1
+fi
+
+rm -rf $tmp $here/rep.{0,meta,index}
+trap "cd $here; rm -fr $tmp; exit" 0 1 2 3 15
+
+cat <<End-of-File >> $tmp.rep.config
+log mandatory on 1 sec {
+	hinv.machine
+	kernel.uname.sysname
+
+	kernel.all.lastpid
+	mem.util.used
+	network.tcpconn.close
+
+	hinv.ncpu
+	mem.physmem
+	hinv.nfilesys
+	kernel.all.hz
+
+	sample.wrap.long
+	kernel.percpu.cpu.user
+	disk.all.read_bytes
+	kernel.all.sysfork
+
+	kernel.all.load
+
+	hinv.cpu.bogomips
+	hinv.cpu.clock
+	network.interface.speed
+}
+End-of-File
+
+${PCP_BINADM_DIR}/pmlogger -s 5 -c $tmp.rep.config $here/rep
+
+exit
