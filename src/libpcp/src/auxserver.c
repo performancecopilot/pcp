8fcaf1baf5450b03ded20dcab2479494685c36f2
jirib <jirib@devio.us>
2016-02-10 18:01:49 +0100

    first wave of diffs to compile on OpenBSD

--- src/libpcp/src/auxserver.c
+++ src/libpcp/src/auxserver.c
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2013-2015 Red Hat.
- * 
+ *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
  * License for more details.
  */
 
-#include <sys/stat.h> 
+#include <sys/stat.h>
 
 #include "pmapi.h"
 #include "impl.h"
@@ -20,6 +20,11 @@
 #include "internal.h"
 #if defined(HAVE_GETPEERUCRED)
 #include <ucred.h>
+#endif
+
+/* OpenBSD doesn't have EPROTO */
+#ifndef EPROTO
+#define EPROTO EINTR
 #endif
 
 #define STRINGIFY(s)	#s
--- src/libpcp/src/pdubuf.c
+++ src/libpcp/src/pdubuf.c
@@ -1,17 +1,16 @@
 /*
  * Copyright (c) 1995 Silicon Graphics, Inc.  All Rights Reserved.
  * Copyright (c) 2015 Red Hat, Inc.
- * 
+ *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
  * License for more details.
- *
  * Thread-safe notes
  *
  * To avoid buffer trampling, on success __pmFindPDUBuf() now returns
@@ -24,6 +23,9 @@
 #include "compiler.h"
 #include <assert.h>
 #include <search.h>
+#ifdef __OpenBSD__
+#include <sys/stdint.h>
+#endif
 
 typedef struct bufctl
 {
