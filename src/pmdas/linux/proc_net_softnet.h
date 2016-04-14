e76344a401122e9d17795fc74ed173a33cfcd3b4
Nathan Scott <nathans@redhat.com>
2016-04-14 13:02:27 +1000

    pmdalinux: tweak softnet copyright dates

--- src/pmdas/linux/proc_net_softnet.h
+++ src/pmdas/linux/proc_net_softnet.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Red Hat, Inc.  All Rights Reserved.
+ * Copyright (c) 2015-2016 Red Hat.
  * 
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
