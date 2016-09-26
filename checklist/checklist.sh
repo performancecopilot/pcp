99eebb230fa6076d6d2580b5f9a7eb833be62ee0
Frank Ch. Eigler <fche@redhat.com>
2016-09-26 15:24:37 -0400

    checklist toyishly alive
    It uses a private pmrep to archive metrics & private pmwebd instance
    to serve checklist etc. pieces.
    
    To run: "sh checklist/checklist.sh".
    ^C to stop.
    
    Navigate to individual "node" links with right/middle-click rather
    than left-click, due to some apparent grafana problem.

---
+++ checklist/checklist.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+# starts the checklist services
+
+PMWEBD_PORT=44444 # choose any random one
+BROWSER="/usr/bin/firefox -new-instance"
+# BROWSER="/usr/bin/google-chrome"
+PCP_ENV=/etc/pcp.env
+CHECKLISTDIR=`dirname $0`
+CHECKLIST=$CHECKLISTDIR/checklist.json
+WEBAPPSDIR=`cd $CHECKLISTDIR/..; pwd`
+
+. $PCP_ENV
+
+set -e
+
+tmpdir=`mktemp -d`
+echo "For use by checklist pid $$" > $tmpdir/README
+echo "Created temporary directory $tmpdir"
+
+# baby systemd -- or maybe instead systemd-run --scope=user ...
+pids=""
+trap 'kill $pids; rm -r "$tmpdir"; exit 0' 0 1 2 3 5 9 15 
+
+# start private logger if needed
+# XXX: create metrics list from search of json file
+metrics="kernel.percpu kernel.all.load"
+refresh="1" # XXX: parametrize
+$PCP_BIN_DIR/pmrep -F ${tmpdir}/archive-`date +%s` -o archive -t $refresh $metrics &
+pids="$pids $!"
+
+$PCP_BINADM_DIR/pmwebd -i $refresh -G -R ${WEBAPPSDIR} -p ${PMWEBD_PORT} -A ${tmpdir} -P &
+pids="$pids $!"
+
+echo "Started service pids $pids"
+
+cmd="$BROWSER http://localhost:${PMWEBD_PORT}/grafana/index.html#/dashboard/script/checklist.js"
+echo "Starting web browser:"
+echo "$cmd"
+eval $cmd
+
+wait
---
+++ grafana/app/dashboards/checklist.js
@@ -0,0 +1,136 @@
+/* global _ */
+
+/*
+ * Complex scripted dashboard
+ * This script generates a dashboard object that Grafana can load. It also takes a number of user
+ * supplied URL parameters (int ARGS variable)
+ *
+ * Global accessable variables
+ * window, document, $, jQuery, ARGS, moment
+ *
+ * Return a dashboard object, or a function
+ *
+ * For async scripts, return a function, this function must take a single callback function,
+ * call this function with the dasboard object
+ */
+
+// accessable variables in this scope
+var window, document, ARGS, $, jQuery, moment, kbn;
+
+var pmwebd = location.protocol + "//" + location.hostname + ":" + location.port;
+var checklist_url = "/grafana/index.html#/dashboard/script/checklist.js";
+
+return function(callback) {
+    $.ajax({
+        method: 'GET',
+        url: pmwebd + "/checklist/checklist.json"
+    }).done(function(result) {
+        var dashboard;
+
+        // url parameter to identify checklist node
+        var node = ARGS["node"]; 
+        if (node == undefined) { node = "Start"; }
+        
+        // Intialize a skeleton with nothing but a rows array and service object
+        dashboard = {
+            style: 'light',
+            rows: [],
+            services : {},
+            refresh: '2s', // XXX: parametrize
+            style: 'light',
+            nav: [ { type : "timepicker",
+                     collapse: false,
+                     enable : true,
+                     time_options : [ "5m", "15m", "1h", "6h",
+                                      "12h", "24h", "2d", "7d",
+                                      "30d", "90d", "365d" ],
+                     now: true
+                   } ],
+            time: { from: "now-1m", // overriden with ARGS[from]
+                    to: "now"       // overriden with ARGS[to]
+                  }
+        };
+        
+        // Set a title
+        dashboard.title = 'PCP CHECKLIST ' + node;
+        
+        // create navigation link/menu at the top
+        // XXX: grafana suppresses normal click linkfollowing action in these A HREF links
+        var markdown = "";
+        markdown = "[**RESTART**](" + pmwebd + checklist_url + ")";
+        var nodes = result.nodes;
+        for (var i=0; i<nodes.length; i++) {
+            var highlight = "";
+            if (nodes[i].name == node) {
+                highlight="**";
+            }
+            markdown +=
+                " | [" + highlight + nodes[i].name + highlight + "]" +
+                "(" + pmwebd + checklist_url + "?node=" + encodeURIComponent(nodes[i].name) + ")";
+        }
+        dashboard.rows.push({
+            title: '', height: '0',
+            panels: [ { title: 'Navigation', type: 'text', span: 12, fill: 1, content: markdown } ]
+        });
+
+        // find        
+        var panel;
+        for (var i=0; i<nodes.length; i++) {
+            if (nodes[i].name == node) {
+                panel = nodes[i];
+            }
+        }
+        if (panel == undefined) {
+            alert ("checklist node " + node + "not found");
+            callback(dashboard);
+            return;
+        }
+
+        // create description panel
+        var markdown = "";
+        if ("description" in panel) {
+            markdown = panel.description;
+        } else {
+            markdown = "(no description)";
+        }
+        dashboard.rows.push({
+            title: '', height: '0',
+            panels: [ { title: 'Description', type: 'text', span: 12, fill: 1, content: markdown } ]
+        });
+
+        // create pcp metrics panel
+        if ("pcp_metrics" in panel) {
+            var metrics = panel.pcp_metrics;
+            console.log(metrics);
+
+            if (! (metrics instanceof Array))
+                metrics = [ metrics ];
+            for (t in metrics) {
+                var targets = metrics[t];
+                var panel = {
+                    title: targets,
+                    type: 'graph',
+                    span: 12,
+                    fill: 1,
+                    linewidth: 2,
+                    targets: [],
+                    seriesOverrides: [ { yaxis: 2, fill: 0, linewidth: 5 } ]
+                };
+                var subtargets = targets.split(',');
+                for (j in subtargets) {
+                    panel.targets.push({target: "*."+subtargets[j]});
+                }
+
+                console.log(panel);
+                dashboard.rows.push({
+                    title: 'Chart',
+                    height: 250, // XXX: param
+                    panels: [ panel ]});
+            }
+        }
+        
+        // when dashboard is composed call the callback function and
+        // pass the dashboard
+        callback(dashboard);
+    });
+}
