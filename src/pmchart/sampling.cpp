e49a20b7bd34b4893dc194e8d373595164c300bd
Nathan Scott <nathans@redhat.com>
2016-10-07 11:40:09 +1100

    pmchart: stacked bar charts need same scaling as utilization

--- src/pmchart/sampling.cpp
+++ src/pmchart/sampling.cpp
@@ -357,7 +357,8 @@ SamplingItem::setPlotStack(int index, double sum)
     if (!hidden() && !qIsNaN(my.itemData[index])) {
 	sum += my.itemData[index];
 	my.itemData[index] = sum;
-    }
+    } else
+	my.itemData[index] = 0.0;
     return sum;
 }
 
