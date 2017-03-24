d5eb00bf975d5bf823062771407e29c565ad48c0
Nathan Scott <nathans@redhat.com>
2017-03-24 14:16:11 +1100

    python: bump module version for docs updates

--- src/python/setup.py
+++ src/python/setup.py
@@ -29,7 +29,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name = 'pcp',
-    version = '1.0',
+    version = '1.1',
     description = 'Performance Co-Pilot collector, monitor and instrumentation APIs',
     long_description = long_description,
     license = 'GPLv2+',
