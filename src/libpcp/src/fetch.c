c7d262a66c34ddc85facb6fe11f792573d0959e4
Ken McDonell <kenj@internode.on.net>
2017-06-26 07:10:17 +1000

    src/libpcp/fetch.c: remove extra c_lock unlock in fetch_ctx()
    Only on an error branch when using PM_CONTEXT_LOCAL and multiple
    threads (a banned combination), so not too much of an issue, except
    for QA.

--- src/libpcp/src/fetch.c
+++ src/libpcp/src/fetch.c
@@ -122,8 +122,6 @@ pmFetch_ctx(__pmContext *ctxp, int numpmid, pmID *pmidlist, pmResult **result)
 	if (ctxp->c_type == PM_CONTEXT_LOCAL && PM_MULTIPLE_THREADS(PM_SCOPE_DSO_PMDA)) {
 	    /* Local context requires single-threaded applications */
 	    sts = PM_ERR_THREAD;
-	    if (need_unlock)
-		PM_UNLOCK(ctxp->c_lock);
 	    goto done;
 	}
 
