c1e2ba042b0dd7d10cabb5a37cd62895cc67abea
Nathan Scott <nathans@redhat.com>
2017-08-09 10:38:28 +1000

    libpcp: annotate new internal hostent routine as internal

--- src/libpcp/src/internal.h
+++ src/libpcp/src/internal.h
@@ -197,7 +197,7 @@ struct __pmHostEnt {
     __pmAddrInfo	*addresses;
 };
 
-extern const __pmAddrInfo *__pmHostEntGetAddrInfo(const __pmHostEnt *, const void *);
+extern const __pmAddrInfo *__pmHostEntGetAddrInfo(const __pmHostEnt *, const void *) _PCP_HIDDEN;
 #endif
 
 extern unsigned __pmFirstInetSubnetAddr(unsigned, int) _PCP_HIDDEN;
