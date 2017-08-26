d5061361c46c0639ad7e7213557d304c23dd09dc
Ken McDonell <kenj@kenj.id.au>
2017-08-26 10:44:09 +1000

    src/multithread11.c: toughen up!
    Add -D and -i <iter> command line options.
    
    Then make the test much tougher, by creating 10 contexts of each
    flavour in each iteration and then destroying them in the reverse
    order ...  this stresses the create/destroy overlap much more with
    up to 20 contexts in play.
    
    Add PMAPI error detection and reporting ... has to be a little relaxed
    because qa/1096 _expects_ all the archive pmNewContext() calls to fail.
    
    Still have one thread juggling live contexts and another working on
    archive contexts.
    
    With these changes and without the libpcp fix, -i1 is enough to expose
    failure on a machine where previously -i5000 passed.

--- qa/src/multithread11.c
+++ qa/src/multithread11.c
@@ -10,15 +10,39 @@
 #include <pcp/pmapi.h>
 #include <pcp/impl.h>
 
+/*
+ * do iter host cycles and 10*iter archive cycles
+ */
+static int iter = 500;
+
+/*
+ * for each cycle, call pmNewContext() for NUM_CTX contexts and then
+ * call pmDestroyContext() to destroy them in the opposite order to
+ * the order of creation.
+ */
+#define NUM_CTX 10
+
 static void *
 liveworker(void *x)
 {
-    int         i, c;
+    int         i, c, ctx[NUM_CTX], sts;
     char        *host = x;
+    char	errmsg[PM_MAXERRMSGLEN];
 
-    for (i=0; i < 5000; i++) {
-	if ((c = pmNewContext(PM_CONTEXT_HOST, host)) >= 0)
-	    pmDestroyContext(c);
+    for (i=0; i < iter; i++) {
+	for (c = 0; c < NUM_CTX; c++) {
+	    if ((ctx[c] = pmNewContext(PM_CONTEXT_HOST, host)) < 0) {
+
+		fprintf(stderr, "liveworker: iter %d ctx[%d] create: %s\n", i, c, pmErrStr_r(ctx[c], errmsg, sizeof(errmsg)));
+	    }
+	}
+	for (c = NUM_CTX-1; c >= 0; c--) {
+	    if (ctx[c] >= 0) {
+		sts = pmDestroyContext(ctx[c]);
+		if (sts < 0)
+		    fprintf(stderr, "liveworker: iter %d ctx[%d] destroy: %s\n", i, c, pmErrStr_r(sts, errmsg, sizeof(errmsg)));
+	    }
+	}
     }
     return NULL;
 }
@@ -26,12 +50,29 @@ liveworker(void *x)
 static void *
 archworker(void *x)
 {
-    int         i, c;
+    int         i, c, ctx[NUM_CTX], sts;
     char        *archive = x;
+    char	errmsg[PM_MAXERRMSGLEN];
 
-    for (i=0; i < 50000; i++) {
-	if ((c = pmNewContext(PM_CONTEXT_ARCHIVE, archive)) >= 0)
-	    pmDestroyContext(c);
+    for (i=0; i < 10*iter; i++) {
+	for (c = 0; c < NUM_CTX; c++) {
+	    if ((ctx[c] = pmNewContext(PM_CONTEXT_ARCHIVE, archive)) < 0) {
+		/*
+		 * qa/1096 uses this code with a bad archive, so be silently
+		 * tolerant of some failures
+		 */
+		if (ctx[c] == PM_ERR_NODATA)
+		    continue;
+		fprintf(stderr, "archworker: iter %d ctx[%d] create: %s\n", i, c, pmErrStr_r(ctx[c], errmsg, sizeof(errmsg)));
+	    }
+	}
+	for (c = NUM_CTX-1; c >= 0; c--) {
+	    if (ctx[c] >= 0) {
+		sts = pmDestroyContext(ctx[c]);
+		if (sts < 0)
+		    fprintf(stderr, "archworker: iter %d ctx[%d] destroy: %s\n", i, c, pmErrStr_r(sts, errmsg, sizeof(errmsg)));
+	    }
+	}
     }
     return NULL;
 }
@@ -39,15 +80,44 @@ archworker(void *x)
 int
 main(int argc, char *argv[])
 {
+    int sts;
+    int errflag = 0;
+    int c;
     pthread_t p1, p2;
 
-    if (argc != 3) {
-    	fprintf(stderr, "Usage: %s hostname archivename\n", argv[0]);
+    __pmSetProgname(argv[0]);
+
+    while ((c = getopt(argc, argv, "D:i:?")) != EOF) {
+	switch (c) {
+	case 'D':
+	    sts = __pmParseDebug(optarg);
+	    if (sts < 0) {
+		fprintf(stderr, "%s: unrecognized debug flag specification (%s)\n",
+		    pmProgname, optarg);
+		errflag++;
+	    }
+	    else
+		pmDebug |= sts;
+	    break;
+
+	case 'i':
+	    iter = atoi(optarg);
+	    break;
+
+	case '?':
+	default:
+	    errflag++;
+	    break;
+	}
+    }
+
+    if (errflag || optind != argc-2) {
+    	fprintf(stderr, "Usage: %s [-D...] [-i iter] hostname archivename\n", pmProgname);
 	exit(1);
     }
 
-    pthread_create(&p1, NULL, &liveworker, argv[1]);
-    pthread_create(&p2, NULL, &archworker, argv[2]);
+    pthread_create(&p1, NULL, &liveworker, argv[optind]);
+    pthread_create(&p2, NULL, &archworker, argv[optind+1]);
     pthread_join(p2, NULL);
     pthread_join(p1, NULL);
 
