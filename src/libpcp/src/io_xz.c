050f26be4dc8e6f46f0344babcb093df3b8ec350
Dave Brolley <brolley@redhat.com>
2017-09-25 13:50:39 -0400

    libpcp: Initial implementation of seamless decompression of PCP archives.
    Handles decompression of .xz compressed archives on input only.

--- src/libpcp/src/io_xz.c
+++ src/libpcp/src/io_xz.c
@@ -34,6 +34,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -47,23 +48,28 @@
 #define XZ_FOOTER_MAGIC_LEN 2
 
 /* A block cache. Implemented as a very simple LRU list with a fixed depth. */
+#if 0 /* not used yet */
 typedef struct blkcache_stats {
     size_t hits;
     size_t misses;
 } blkcache_stats;
+#endif
 
-
-typedef struct blkcache {
-    size_t maxdepth;
-    struct block *blocks;
-    blkcache_stats stats;
-} blkcache;
-
-struct block {
+/* A buffer of uncompressed blocks */
+typedef struct block {
     uint64_t start;
     uint64_t size;
+    uint64_t current_offset;
     char *data;
-};
+} block;
+
+typedef struct blkcache {
+    int maxdepth;
+    block *blocks;
+#if 0 /* not used yet */
+    blkcache_stats stats;
+#endif
+} blkcache;
 
 /* The file handle */
 typedef struct xzfile {
@@ -73,16 +79,20 @@ typedef struct xzfile {
     size_t nr_streams;
     size_t nr_blocks;
     blkcache *cache;
+    off_t uncompressed_offset;
+  __uint64_t uncompressed_size;
   __uint64_t max_uncompressed_block_size;
 } xzfile;
 
 static void
 xz_debug(const char *fmt, ...)
 {
+#if 0
     va_list ap;
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
     va_end(ap);
+#endif
 }
 
 blkcache *
@@ -96,14 +106,16 @@ new_blkcache(size_t maxdepth)
     return NULL;
   }
 
-  c->blocks = calloc(maxdepth, sizeof(struct block));
+  c->blocks = calloc(maxdepth, sizeof(block));
   if (!c->blocks) {
     fprintf(stderr, "calloc: %m");
     free(c);
     return NULL;
   }
   c->maxdepth = maxdepth;
+#if 0 /* not used yet */
   c->stats.hits = c->stats.misses = 0;
+#endif
 
   return c;
 }
@@ -119,59 +131,34 @@ free_blkcache(blkcache *c)
   free(c);
 }
 
-char *
-get_block(blkcache *c, uint64_t offset, uint64_t *start, uint64_t *size)
-{
-  size_t i;
-  struct block tmp;
-
-  for (i = 0; i < c->maxdepth; ++i) {
-    if (c->blocks[i].data != NULL &&
-        c->blocks[i].start <= offset &&
-        offset < c->blocks[i].start + c->blocks[i].size) {
-      /* This block is now most recently used, so put it at the start. */
-      if (i > 0) {
-        tmp = c->blocks[0];
-        c->blocks[0] = c->blocks[i];
-        c->blocks[i] = tmp;
-      }
-
-      c->stats.hits++;
-      *start = c->blocks[0].start;
-      *size = c->blocks[0].size;
-      return c->blocks[0].data;
-    }
-  }
-
-  c->stats.misses++;
-  return NULL;
-}
-
-int
-put_block(blkcache *c, uint64_t start, uint64_t size, char *data)
-{
-  size_t i;
-
-  /* Eject the least recently used block. */
-  i = c->maxdepth-1;
-  if (c->blocks[i].data != NULL)
-    free(c->blocks[i].data);
-
-  for (; i >= 1; --i)
-    c->blocks[i] = c->blocks[i-1];
-
-  /* The new block is most recently used, so it goes at the start. */
-  c->blocks[0].start = start;
-  c->blocks[0].size = size;
-  c->blocks[0].data = data;
-
-  return 0;
-}
-
+#if 0 /* not used yet */
 void
 blkcache_get_stats(blkcache *c, blkcache_stats *ret)
 {
   memcpy(ret, &c->stats, sizeof(c->stats));
+}
+#endif
+
+static int
+xz_feof(__pmFILE *f)
+{
+    xzfile *xz = f->priv;
+    if (xz->uncompressed_offset >= xz->uncompressed_size)
+	return 1;
+    return 0;
+}
+
+static int
+xz_ferror(__pmFILE *f)
+{
+    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
+    return 0;
+}
+
+static void
+xz_clearerr(__pmFILE *f)
+{
+    /* Do nothing --- for now */
 }
 
 static int
@@ -393,7 +380,7 @@ iter_indexes(lzma_index *idx,
 }
 
 static int
-xz_init(xzfile *xz)
+init(xzfile *xz)
 {
   /* Check file magic. */
   if (check_header_magic(xz->f) != 0)
@@ -409,8 +396,10 @@ xz_init(xzfile *xz)
                     &xz->nr_blocks, &xz->max_uncompressed_block_size) == -1)
       return 1; /* error */
 
-  //  lzma_index_uncompressed_size(xz->idx);
-
+  xz->uncompressed_size = lzma_index_uncompressed_size(xz->idx);
+  xz->uncompressed_offset = 0;
+  xz->cache = new_blkcache(4); /* 4 blocks in the cache, for now */
+  
   return 0; /* ok */
 }
 
@@ -430,7 +419,7 @@ xz_open(__pmFILE *f, const char *path, const char *mode)
   if (xz->f == NULL)
       goto err;
 
-  if (xz_init(xz) == 0) {
+  if (init(xz) == 0) {
       xz->fd = fileno(xz->f);
       f->priv = xz;
       return xz;
@@ -458,7 +447,7 @@ xz_fdopen(__pmFILE *f, int fd, const char *mode)
   if (xz->f == NULL)
       goto err;
 
-  if (xz_init(xz) == 0) {
+  if (init(xz) == 0) {
       xz->fd = fd;
       f->priv = xz;
       return xz;
@@ -473,35 +462,350 @@ xz_fdopen(__pmFILE *f, int fd, const char *mode)
 static int
 xz_seek(__pmFILE *f, off_t offset, int whence)
 {
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return -1;
+    xzfile *xz = (xzfile *)f->priv;;
+    __int64_t new_offset;
+
+    switch (whence) {
+    case SEEK_SET:
+	new_offset = offset;
+	break;
+    case SEEK_CUR:
+	new_offset = xz->uncompressed_offset + offset;
+	break;
+    case SEEK_END:
+	new_offset = xz->uncompressed_size + offset;
+	break;
+    default:
+	errno = EINVAL;
+	return -1;
+    }
+
+    if (new_offset < 0) {
+	errno = EINVAL;
+	return -1;
+    }
+
+    /* Don't actually seek to the requested offset now. Just record it. */
+    xz->uncompressed_offset = new_offset;
+    return 0;
+}
+
+static off_t
+xz_lseek(__pmFILE *f, off_t offset, int whence)
+{
+    /* The same as xz_seek for our purposes */
+    return xz_seek(f, offset, whence);
 }
 
 static void
 xz_rewind(__pmFILE *f)
 {
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
+    xzfile *xz = (xzfile *)f->priv;;
+
+    /* Don't actually seek to the requested offset now. Just record it. */
+    xz->uncompressed_offset = 0;
+
+    /* The function also requires that any error flag be reset. */
+    xz_clearerr(f);
 }
 
 static off_t
 xz_tell(__pmFILE *f)
 {
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return -1;
+    xzfile *xz = (xzfile *)f->priv;;
+    return xz->uncompressed_offset;
+}
+
+static block *
+cache_block_used(blkcache *cache, int slot)
+{
+    /* Move this block into the first slot, if it is not already there */
+    if (slot != 0) {
+	block used = cache->blocks[slot];
+	int i;
+	for (i = slot; i > 0; --i)
+	    cache->blocks[i] = cache->blocks[i - 1];
+	cache->blocks[0] = used;
+    }
+    return &cache->blocks[0];
+}
+
+static char *
+read_block(xzfile *xz, uint64_t offset,
+                   uint64_t *start_rtn, uint64_t *size_rtn)
+{
+  lzma_index_iter iter;
+  uint8_t header[LZMA_BLOCK_HEADER_SIZE_MAX];
+  lzma_block block;
+  lzma_filter filters[LZMA_FILTERS_MAX + 1];
+  lzma_ret r;
+  lzma_stream strm = LZMA_STREAM_INIT;
+  char *data;
+  ssize_t n;
+  size_t i;
+
+  /* Locate the block containing the uncompressed offset. */
+  lzma_index_iter_init(&iter, xz->idx);
+  if (lzma_index_iter_locate(&iter, offset)) {
+    fprintf(stderr, "cannot find offset %lu in the xz file", offset);
+    return NULL;
+  }
+
+  *start_rtn = iter.block.uncompressed_file_offset;
+  *size_rtn = iter.block.uncompressed_size;
+
+  fprintf(stderr, "seek: block number %d at file offset %lu",
+                (int) iter.block.number_in_file,
+                (uint64_t) iter.block.compressed_file_offset);
+
+  if (lseek(xz->fd, iter.block.compressed_file_offset, SEEK_SET) == -1) {
+    fprintf(stderr, "lseek: %m");
+    return NULL;
+  }
+
+  /* Read the block header.  Start by reading a single byte which
+   * tell us how big the block header is.
+   */
+  n = read(xz->fd, header, 1);
+  if (n == 0) {
+    fprintf(stderr, "read: unexpected end of file reading block header byte");
+    return NULL;
+  }
+  if (n == -1) {
+    fprintf(stderr, "read: %m");
+    return NULL;
+  }
+
+  if (header[0] == '\0') {
+    fprintf(stderr, "read: unexpected invalid block in file, header[0] = 0");
+    return NULL;
+  }
+
+  block.version = 0;
+  block.check = iter.stream.flags->check;
+  block.filters = filters;
+  block.header_size = lzma_block_header_size_decode(header[0]);
+
+  /* Now read and decode the block header. */
+  n = read(xz->fd, &header[1], block.header_size-1);
+  if (n >= 0 && n != block.header_size-1) {
+    fprintf(stderr, "read: unexpected end of file reading block header");
+    return NULL;
+  }
+  if (n == -1) {
+    fprintf(stderr, "read: %m");
+    return NULL;
+  }
+
+  r = lzma_block_header_decode(&block, NULL, header);
+  if (r != LZMA_OK) {
+    fprintf(stderr, "invalid block header (error %d)", r);
+    return NULL;
+  }
+
+  /* What this actually does is it checks that the block header
+   * matches the index.
+   */
+  r = lzma_block_compressed_size(&block, iter.block.unpadded_size);
+  if (r != LZMA_OK) {
+    fprintf(stderr, "cannot calculate compressed size (error %d)", r);
+    goto err1;
+  }
+
+  /* Read the block data. */
+  r = lzma_block_decoder(&strm, &block);
+  if (r != LZMA_OK) {
+    fprintf(stderr, "invalid block (error %d)", r);
+    goto err1;
+  }
+
+  data = malloc(*size_rtn);
+  if (data == NULL) {
+    fprintf(stderr, "malloc(%lu bytes): %m\n"
+                  "NOTE: If this error occurs, you need to recompress your xz files with a smaller block size.  Use: 'xz --block-size=16777216 ...'.",
+                  *size_rtn);
+    goto err1;
+  }
+
+  strm.next_in = NULL;
+  strm.avail_in = 0;
+  strm.next_out = (uint8_t *) data;
+  strm.avail_out = block.uncompressed_size;
+
+  do {
+    uint8_t buf[BUFSIZ];
+    lzma_action action = LZMA_RUN;
+
+    if (strm.avail_in == 0) {
+      strm.next_in = buf;
+      n = read(xz->fd, buf, sizeof buf);
+      if (n == -1) {
+        fprintf(stderr, "read: %m");
+        goto err2;
+      }
+      strm.avail_in = n;
+      if (n == 0)
+        action = LZMA_FINISH;
+    }
+
+    strm.avail_in = n;
+    strm.next_in = buf;
+    r = lzma_code(&strm, action);
+  } while (r == LZMA_OK);
+
+  if (r != LZMA_OK && r != LZMA_STREAM_END) {
+    fprintf(stderr, "could not parse block data (error %d)", r);
+    goto err2;
+  }
+
+  lzma_end(&strm);
+
+  for (i = 0; filters[i].id != LZMA_VLI_UNKNOWN; ++i)
+    free(filters[i].options);
+
+  return data;
+
+ err2:
+  free(data);
+  lzma_end(&strm);
+ err1:
+  for (i = 0; filters[i].id != LZMA_VLI_UNKNOWN; ++i)
+    free(filters[i].options);
+
+  return NULL;
+}
+
+static block *
+read_new_block(xzfile *xz, int slot)
+{
+    blkcache *cache;
+    block *blk;
+    char *data;
+    uint64_t start = 0, size = 0; /* silence coverity */
+
+    /* Decompress a new block into the given slot. */
+    data = read_block(xz, xz->uncompressed_offset, &start, &size);
+    if (data == NULL)
+	return NULL;
+
+    /* Add the data to this block */
+    cache = xz->cache;
+    blk = &cache->blocks[slot];
+    if (blk->data != NULL)
+	free(blk->data);
+    blk->data = data;
+    blk->start = start;
+    blk->size = size;
+    blk->current_offset = xz->uncompressed_offset - blk->start;
+
+    /* Mark this block as most recently used */
+    blk = cache_block_used(cache, slot);
+	    
+    return blk;
+}
+
+/*
+ * Find the block containing the current uncompressed offset and update the
+ * current offset within that block..
+ */
+static block *
+reposition(xzfile *xz)
+{
+    blkcache *cache;
+    block *blk;
+    int slot;
+
+    /*
+     * Search the cache for the block we want.
+     * The cache is sorted by most recently used blocks. This works out well
+     * since the data we want is most often in the block which will be checked
+     * first.
+     */
+    cache = xz->cache;
+    for (slot = 0; slot < cache->maxdepth; ++slot) {
+	blk = &cache->blocks[slot];
+	if (blk->data == NULL)
+	    break; /* end of cache */
+
+	if (xz->uncompressed_offset >= blk->start &&
+	    xz->uncompressed_offset < blk->size) {
+	    /* found it */
+	    blk->current_offset = xz->uncompressed_offset - blk->start;
+	    blk = cache_block_used(cache, slot);
+	    return blk;
+	}
+    }
+
+    /*
+     * No cached block contains the uncompressed offset that we want.
+     * Decompress a new block into an empty slot, or into the least recently
+     * used slot, which is the last one.
+     */
+    if (slot >= cache->maxdepth)
+	slot = cache->maxdepth - 1;
+    blk = read_new_block(xz, slot);
+
+    return blk;
 }
 
 static int
 xz_getc(__pmFILE *f)
 {
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return EOF;
+    xzfile *xz = (xzfile *)f->priv;;
+    block *blk = reposition(xz);
+    int c;
+
+    if (blk == NULL)
+	return EOF;
+
+    /* It's a single byte. It is guaranteed that we can copy it. */
+    c = *(blk->data + blk->current_offset);
+    ++xz->uncompressed_offset;
+    ++blk->current_offset;
+    return c;
 }
 
 static size_t
 xz_read(void *ptr, size_t size, size_t nmemb, __pmFILE *f)
 {
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return 0;
+    xzfile *xz = (xzfile *)f->priv;;
+    block *blk;
+    const char *p;
+    size_t n;
+    size_t copied;
+
+    /* Obtain the requested size in bytes. */
+    size *= nmemb;
+
+    /*
+     * Copy data from the current block and possibly from subsequent blocks
+     * until we have copied the requested number of bytes. */
+    copied = 0;
+    while (size > 0) {
+	blk = reposition(xz);
+	if (blk == NULL)
+	    return 0; /* error */
+
+	/* See how many bytes we can copy from the current block. */
+	p =  blk->data + blk->current_offset;
+	n = size;
+	if (n > blk->size - blk->current_offset)
+	    n = blk->size - blk->current_offset;
+
+	/*
+	 * Copy the requested number of bytes from the current block to the
+	 * output buffer. We are guaranteed to be able to copy at least one
+	 * byte.
+	 */
+	memcpy(ptr, p, n);
+	copied += n;
+	xz->uncompressed_offset += n;
+	blk->current_offset += n;
+	ptr = ((char *)ptr) + n;
+	size -= n;
+    }
+
+    return copied;
 }
 
 static size_t
@@ -532,38 +836,18 @@ xz_fileno(__pmFILE *f)
     return xz->fd;
 }
 
-static off_t
-xz_lseek(__pmFILE *f, off_t offset, int whence)
-{
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return -1;
-}
-
 static int
 xz_fstat(__pmFILE *f, struct stat *buf)
 {
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return -1;
-}
+    xzfile *xz = f->priv;
+    FILE *fp = xz->f;
+    int rc = fstat(fileno(fp), buf);
 
-static int
-xz_feof(__pmFILE *f)
-{
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return 0;
-}
-
-static int
-xz_ferror(__pmFILE *f)
-{
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
-    return 0;
-}
-
-static void
-xz_clearerr(__pmFILE *f)
-{
-    fprintf(stderr, "libpcp internal error: %s not implemented\n", __func__);
+    /* What the caller really wants for st_size is the uncompressed size. */
+    if (rc != -1)
+	buf->st_size = xz->uncompressed_size;
+    
+    return rc;
 }
 
 static int
@@ -581,6 +865,7 @@ xz_close(__pmFILE *f)
     
     lzma_index_end (xz->idx, NULL);
     sts = fclose(xz->f);
+    free_blkcache(xz->cache);
     free(xz);
     return sts;
 }
