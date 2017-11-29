performancecopilot/pcp

commit 26d8f9306ea5f818f4305116cd7844596e90bb2c

pmrep: rewrite stdout output code
Rewrite pmrep stdout output code to avoid the costly crossing of
C/Python boundary. Also, makes the code more readable.

QA passes OK.

src/pmrep/pmrep.py
@@ -1380,7 +1380,7 @@ class PMReporter(object):
         res = {}
         for _, metric in enumerate(self.metrics):
             try:
-                for inst, name, val in self.metrics[metric][5]():
+                for inst, name, val in self.metrics[metric][5](): # pylint: disable=unused-variable
                     try:
                         value = val()
                         if isinstance(value, float):
@@ -1429,59 +1429,63 @@ class PMReporter(object):
             line.append(timestamp)
         line.append(self.delimiter)
 
+        # Avoid crossing the C/Python boundary more than once per metric
+        res = {}
+        for _, metric in enumerate(self.metrics):
+            try:
+                for inst, name, val in self.metrics[metric][5](): # pylint: disable=unused-variable
+                    try:
+                        value = val()
+                        if isinstance(value, float):
+                            value = round(value, self.precision)
+                        res[metric + str(inst)] = value
+                    except:
+                        pass
+            except:
+                pass
+
+        # Add corresponding values for each column in the static header
         k = 0
         for i, metric in enumerate(self.metrics):
             l = self.metrics[metric][4]
-
             for j in range(len(self.insts[i][0])):
                 k += 1
-
-                found = 0
-                try:
-                    for inst, name, val in self.metrics[metric][5](): # pylint: disable=unused-variable
-                        if inst == PM_IN_NULL or inst == self.insts[i][0][j]:
-                            found = 1
-                            break
-                except:
-                    pass
-                if not found:
-                    value = NO_VAL
-                else:
-                    try:
-                        value = val() # pylint: disable=undefined-loop-variable
-                        if isinstance(value, list):
-                            value = value[0]
-                    except:
-                        value = NO_VAL
-
-                # Make sure the value fits
-                if isinstance(value, (int, long)):
-                    if len(str(value)) > l:
-                        value = TRUNC
-                    else:
-                        #fmt[k] = "{:" + str(l) + "d}"
-                        fmt[k] = "{X:" + str(l) + "d}"
-
-                if isinstance(value, float) and not math.isinf(value):
-                    c = self.precision
-                    s = len(str(int(value)))
-                    if s > l:
-                        c = -1
-                        value = TRUNC
-                    #for _ in reversed(range(c+1)):
-                        #t = "{:" + str(l) + "." + str(c) + "f}"
-                    for f in reversed(range(c+1)):
-                        r = "{X:" + str(l) + "." + str(c) + "f}"
-                        t = "{0:" + str(l) + "." + str(c) + "f}"
-                        if len(t.format(value)) > l:
-                            c -= 1
+                if metric + str(self.insts[i][0][j]) in res:
+                    value = res[metric + str(self.insts[i][0][j])]
+                    # Make sure the value fits
+                    if isinstance(value, (int, long)):
+                        if len(str(value)) > l:
+                            value = TRUNC
                         else:
-                            #fmt[k] = t
-                            fmt[k] = r
-                            break
+                            #fmt[k] = "{:" + str(l) + "d}"
+                            fmt[k] = "{X:" + str(l) + "d}"
+                    elif isinstance(value, str):
+                        if len(value) > l:
+                            value = TRUNC
 
-                line.append(value)
-                line.append(self.delimiter)
+                    if isinstance(value, float) and not math.isinf(value):
+                        c = self.precision
+                        s = len(str(int(value)))
+                        if s > l:
+                            c = -1
+                            value = TRUNC
+                        #for _ in reversed(range(c+1)):
+                            #t = "{:" + str(l) + "." + str(c) + "f}"
+                        for f in reversed(range(c+1)):
+                            r = "{X:" + str(l) + "." + str(c) + "f}"
+                            t = "{0:" + str(l) + "." + str(c) + "f}"
+                            if len(t.format(value)) > l:
+                                c -= 1
+                            else:
+                                #fmt[k] = t
+                                fmt[k] = r
+                                break
+
+                    line.append(value)
+                    line.append(self.delimiter)
+                else:
+                    line.append(NO_VAL)
+                    line.append(self.delimiter)
 
         del line[-1]
         #self.writer.write('{}'.join(fmt).format(*tuple(line)) + "\n")

commit 27922ad1039c25d5469f7c99434107b49c395a2b

pmrep: fix string truncation
26d8f930 accidentally caused this, the previous behavior was documented
in pmrep(1) and makes more sense (in most cases, anyway).

Fix it up and add some QA.

src/pmrep/pmrep.py
@@ -840,9 +840,6 @@ class PMReporter(object):
                         else:
                             #fmt[k] = "{:" + str(l) + "d}"
                             fmt[k] = "{X:" + str(l) + "d}"
-                    elif isinstance(value, str):
-                        if len(value) > l:
-                            value = pmconfig.TRUNC
 
                     if isinstance(value, float) and \
                        not math.isinf(value) and \

qa/1069
@@ -299,6 +299,10 @@ pmrep -C $log2 -u -U -q MB -I mem.util
 pmrep -C $log2 -u    -i sda mem.util.free disk.dev mem.util
 pmrep -C $log2 -u -v -i sda mem.util.free disk.dev mem.util
 
+echo "== output value truncation"
+pmrep -s 1 $log2 -w 3 hinv.machine
+pmrep -s 1 $log2 -w 3 mem.util.free
+
 echo "== handle empty/invalid/multiple metricsets in config file"
 echo '[fail]' > $tmp.econfig
 echo 'failure = yes' >> $tmp.econfig

qa/1069.out
@@ -840,6 +840,13 @@ pmrep: Impossible value or scale conversion
   d.d.avactive  d.d.read  d.d.write  d.d.total  d.d.read_bytes  d.d.write_bytes
            sda       sda        sda        sda             sda              sda
           ms/s   count/s    count/s    count/s         Kbyte/s          Kbyte/s
+== output value truncation
+  h.m
+     
+  lin
+  m.u
+  Kby
+  xxx
 == handle empty/invalid/multiple metricsets in config file
 Invalid metric yes (PM_ERR_NAME Unknown metric name).
 Unknown option mem.util.free in [options].

qa/1134
@@ -34,15 +34,15 @@ _filter()
 
 # real QA test starts here
 echo "---"
-pmrep -z -p -u -a $A -w 32 kernel.all.uptime
+pmrep -z -p -u -a $A -w 40 kernel.all.uptime
 echo "---"
-pmrep -z -p -u -a $A -w 32 sample.float.one
-echo "---"
-# -s 10 can be removed when Python 2 support is dropped
-pmrep -z -p -u -a $A -w 32 -s 10 kernel.uname.release
+pmrep -z -p -u -a $A -w 40 sample.float.one
 echo "---"
 # -s 10 can be removed when Python 2 support is dropped
-pmrep -z -p -u -a $A -w 32 -s 10 -l \; kernel.uname.release
+pmrep -z -p -u -a $A -w 40 -s 10 kernel.uname.release
+echo "---"
+# -s 10 can be removed when Python 2 support is dropped
+pmrep -z -p -u -a $A -w 40 -s 10 -l \; kernel.uname.release
 echo "---"
 pmrep -z -p -u -a $A kernel.uname.release -o csv | sed -e 's,XXX.*XXX,XXX,g'
 echo "---"

qa/1134.out
@@ -1,58 +1,58 @@
 QA output created by 1134
 ---
-                                k.a.uptime
-                                       sec
-00:00:00                           1234567
-00:00:01                                 0
-00:00:02                        4294967295
-00:00:03                                 1
-00:00:04                                 0
-00:00:05                        4294967295
-00:00:06                                 1
-00:00:07                                 0
-00:00:29                           7654321
+                                        k.a.uptime
+                                               sec
+00:00:00                                   1234567
+00:00:01                                         0
+00:00:02                                4294967295
+00:00:03                                         1
+00:00:04                                         0
+00:00:05                                4294967295
+00:00:06                                         1
+00:00:07                                         0
+00:00:29                                   7654321
 ---
-                                   s.f.one
-                                          
-00:00:00                       1234567.000
-00:00:08                               NaN
-00:00:09                               inf
-00:00:10           9223372036854775808.000
-00:00:11           9223372036854775808.000
-00:00:12                             1.000
-00:00:13                             0.000
-00:00:14                            -1.000
-00:00:15          -9223372036854775808.000
-00:00:16          -9223372036854775808.000
-00:00:17                              -inf
-00:00:18                               NaN
-00:00:29                       7654321.000
+                                           s.f.one
+                                                  
+00:00:00                               1234567.000
+00:00:08                                       NaN
+00:00:09                                       inf
+00:00:10                   9223372036854775808.000
+00:00:11                   9223372036854775808.000
+00:00:12                                     1.000
+00:00:13                                     0.000
+00:00:14                                    -1.000
+00:00:15                  -9223372036854775808.000
+00:00:16                  -9223372036854775808.000
+00:00:17                                      -inf
+00:00:18                                       NaN
+00:00:29                               7654321.000
 ---
-                               k.u.release
-                                          
-00:00:00                             1.2.3
-00:00:19                             null:
-00:00:20                     line:\n:break
-00:00:21    this:":is:\':a:':quote:\":test
-00:00:22                               xxx
-00:00:23                                  
-00:00:24  dont:\n;touch /tmp/toast;\n:eval
-00:00:25                               xxx
-00:00:26                               xxx
-00:00:27                                 X
+                                       k.u.release
+                                                  
+00:00:00                                     1.2.3
+00:00:19                                     null:
+00:00:20                             line:\n:break
+00:00:21            this:":is:\':a:':quote:\":test
+00:00:22       char:<:&:`:$:;:!:#: :?:*:|:~:>:test
+00:00:23                                          
+00:00:24          dont:\n;touch /tmp/toast;\n:eval
+00:00:25  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+00:00:26  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+00:00:27                                         X
 ---
-        ;                     k.u.release
-        ;                                
-00:00:00;                           1.2.3
-00:00:19;                           null:
-00:00:20;                   line:\n:break
-00:00:21;  this:":is:\':a:':quote:\":test
-00:00:22;                             xxx
-00:00:23;                                
-00:00:24;dont:\n_touch /tmp/toast_\n:eval
-00:00:25;                             xxx
-00:00:26;                             xxx
-00:00:27;                               X
+        ;                             k.u.release
+        ;                                        
+00:00:00;                                   1.2.3
+00:00:19;                                   null:
+00:00:20;                           line:\n:break
+00:00:21;          this:":is:\':a:':quote:\":test
+00:00:22;     char:<:&:`:$:_:!:#: :?:*:|:~:>:test
+00:00:23;                                        
+00:00:24;        dont:\n_touch /tmp/toast_\n:eval
+00:00:25;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+00:00:26;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+00:00:27;                                       X
 ---
 Time,"kernel.uname.release"
 1970-01-01 00:00:00,"1.2.3"