b9f8c80b35421028417c81e539d9281ef78c4a1d
Marko Myllynen <myllynen@redhat.com>
2017-11-29 14:39:49 +1100

    pmrep: handle empty delimiter
    Corner case but nail it down regardless.

--- src/pmrep/pmrep.py
+++ src/pmrep/pmrep.py
@@ -604,7 +604,9 @@ class PMReporter(object):
                     # Mark metrics with instance domain but without instances
                     if self.pmconfig.descs[i].contents.indom != PM_IN_NULL and self.pmconfig.insts[i][1][0] is None:
                         name += "-"
-                    name = name.replace(self.delimiter, " ").replace("\n", " ").replace("\"", " ")
+                    if self.delimiter:
+                        name = name.replace(self.delimiter, " ")
+                    name = name.replace("\n", " ").replace("\"", " ")
                     self.writer.write(self.delimiter + "\"" + name + "\"")
             self.writer.write("\n")
 
@@ -756,7 +758,9 @@ class PMReporter(object):
                 if metric + str(self.pmconfig.insts[i][0][j]) in res:
                     value = res[metric + str(self.pmconfig.insts[i][0][j])]
                     if isinstance(value, str):
-                        value = value.replace(self.delimiter, "_").replace("\n", " ").replace('"', " ")
+                        if self.delimiter:
+                            value = value.replace(self.delimiter, "_")
+                        value = value.replace("\n", " ").replace('"', " ")
                         line += str('"' + value + '"')
                     else:
                         line += str(value)
@@ -807,7 +811,7 @@ class PMReporter(object):
                             value = round(value, self.precision)
                         elif isinstance(value, str):
                             value = value.replace("\n", "\\n")
-                            if not self.delimiter.isspace():
+                            if self.delimiter and not self.delimiter.isspace():
                                 value = value.replace(self.delimiter, "_")
                         res[metric + str(inst)] = value
                     except:
@@ -945,7 +949,7 @@ class PMReporter(object):
                         value = inst[2]
                         if isinstance(value, str):
                             value = value.replace("\n", "\\n")
-                            if not self.delimiter.isspace():
+                            if self.delimiter and not self.delimiter.isspace():
                                 value = value.replace(self.delimiter, "_")
                         found = 1
                         break
