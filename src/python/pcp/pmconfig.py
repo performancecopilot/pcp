d51f29a18bec0311a85fcc1f1072c1a21f6a0221
Marko Myllynen <myllynen@redhat.com>
2017-12-05 08:46:55 +1100

    pmconfig.py: add transparent support for per-item fetching
    In commit ede27b6961 we switched to always use extend_indom
    instead of extend_item to resolve RHBZ#1377464 and it helps
    with that particular issue (pmrep slow startup time).
    
    But when filtering the results it may be better to retrieve
    only the few requested instances, not possibly hundreds of them.
    
    IOW, we want to push instance filtering to C level (libpcp).
    
    We extend pmconfig.py to allow clients to choose either option
    transparently; we add a small helper class to provide consistent
    interface to retrieve the results regardless of the method
    initially chosen. If and only if the current instances only and
    also filtering was requested, then instead of extend_indom we use
    extend_item. We are careful to add only those instances that
    are really present at the time when adding them to pmfg but
    also prevent unexpected situation to have a metric w/o instances.
    
    For existing clients this is a transparent change but for clients
    to use this we need to add another argument to the validate_metrics
    method, we do so in a slightly incompatible manner to make the
    parameter names more understandable in the long run. Since the
    needed change is trivial and we're doing various API changes in 4.0
    anyway, this should be ok as a one-off change.
    
    Follow-up patches will start utilizing this functionality, notable
    performance benefits in certain cases included.

--- src/python/pcp/pmconfig.py
+++ src/python/pcp/pmconfig.py
@@ -36,6 +36,8 @@ from pcp import pmapi
 # Common defaults (for applicable utils)
 TRUNC = "xxx"
 VERSION = 1
+CURR_INSTS = False
+MAX_INSTS = 1024
 
 class pmConfig(object):
     """ Config reader and validator """
@@ -380,7 +382,17 @@ class pmConfig(object):
             label = label.replace(" / ", "/")
         return label
 
-    def validate_metrics(self, instances=1024):
+    class pmfg_items_to_indom(object): # pylint: disable=too-few-public-methods
+        """ Helper to provide consistent interface with pmfg items and indoms """
+        def __init__(self, items):
+            """ Initialize an instance with items """
+            self._items = items
+
+        def __call__(self):
+            """ Retrieve the items """
+            return self._items
+
+    def validate_metrics(self, curr_insts=CURR_INSTS, max_insts=MAX_INSTS):
         """ Validate the metrics set """
         # Check the metrics against PMNS, resolve non-leaf metrics
         if self.util.derived:
@@ -547,10 +559,31 @@ class pmConfig(object):
             if self.util.metrics[metric][4] < len(TRUNC):
                 self.util.metrics[metric][4] = len(TRUNC) # Forced minimum
 
-            # Add fetchgroup item
+            # Add fetchgroup items
             try:
+                items = []
+                max_insts = max(1, max_insts)
                 scale = self.util.metrics[metric][2][0]
-                self.util.metrics[metric][5] = self.util.pmfg.extend_indom(metric, mtype, scale, instances)
+                if curr_insts and self.util.metrics[metric][1]:
+                    mitems = 0
+                    vanished = []
+                    for j in range(0, len(self.insts[i][1])):
+                        if mitems < max_insts:
+                            try:
+                                items.append((self.insts[i][0][j], self.insts[i][1][j], self.util.pmfg.extend_item(metric, mtype, scale, self.insts[i][1][j])))
+                                mitems += 1
+                            except:
+                                vanished.append(j)
+                        else:
+                            del self.insts[i][0][-1]
+                            del self.insts[i][1][-1]
+                    if mitems > 0:
+                        for v in reversed(vanished):
+                            del self.insts[i][0][v]
+                            del self.insts[i][1][v]
+                    self.util.metrics[metric][5] = self.pmfg_items_to_indom(items)
+                else:
+                    self.util.metrics[metric][5] = self.util.pmfg.extend_indom(metric, mtype, scale, max_insts)
             except:
                 if hasattr(self.util, 'ignore_incompat') and self.util.ignore_incompat:
                     # Schedule the metric for removal
