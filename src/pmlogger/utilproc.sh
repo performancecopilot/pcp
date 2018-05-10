29765ce06e2f48a82eed198084c4bb0cfed163c0
Ken McDonell <kenj@kenj.id.au>
2018-05-10 10:32:54 +1000

    src/pmlogger/utilproc.sh: add _save_prev_file()
    Canonical and robust implementation when we want to save a prior
    log file (or similar) as the same file name with a ".prev" suffix.
    
    Trickiness here is mostly around existence of foo and/or foo.prev
    at the time of the call, and various permissions wrinkles (mostly
    from QA, rather than production use cases).

--- src/pmlogger/utilproc.sh
+++ src/pmlogger/utilproc.sh
@@ -80,3 +80,69 @@ _do_dir_and_args()
     fi
     $do_shell && dir="`eval echo $dir`"
 }
+
+# Usage: _save_prev_file pathname
+#
+# if pathname exists, try to preserve the contents in pathname.prev and
+# remove pathname, prior to a new pathname being created by the caller
+#
+# return status indicates success
+#
+_save_prev_file()
+{
+    if [ ! -e "$1" ]
+    then
+	# does not exist, nothing to be done
+	return 0
+    elif [ -L "$1" ]
+    then
+	echo "_save_prev_filename: \"$1\" exists and is a symlink" >&2
+	ls -ld "$1" >&2
+	return 1
+    elif [ -f "$1" ]
+    then
+	# complicated because pathname.prev may already exist and
+	# pathname may already exist and one or other or both
+	# may not be able to be removed
+	#
+	if [ -e "$1.prev" ]
+	then
+	    if rm -f "$1.prev"
+	    then
+		:
+	    else
+		echo "_save_prev_filename: unable to remove \"$1.prev\"" >&2
+		ls -ld "$1.prev" >&2
+		if rm -f "$1"
+		then
+		    :
+		else
+		    echo "_save_prev_filename: unable to remove \"$1\"" >&2
+		    ls -ld "$1" >&2
+		fi
+		return 1
+	    fi
+	fi
+	__sts=0
+	if cp -p "$1" "$1.prev"
+	then
+	    :
+	else
+	    echo "_save_prev_filename: copy \"$1\" to \"$1.prev\" failed" >&2
+	    __sts=1
+	fi
+	if rm -f "$1"
+	then
+	    :
+	else
+	    echo "_save_prev_filename: unable to remove \"$1\"" >&2
+	    ls -ld "$1" >&2
+	    __sts=1
+	fi
+	return $__sts
+    else
+	echo "_save_prev_filename: \"$1\" exists and is not a file" >&2
+	ls -ld "$1" >&2
+	return 1
+    fi
+}
