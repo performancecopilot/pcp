bd76480312b93f787f2b93d163e2b6294c8b952f
Nathan Scott <nathans@redhat.com>
2018-05-16 15:44:43 +1000

    pmdalinux: code simplification, cleanup in /proc/locks code

--- src/pmdas/linux/proc_locks.c
+++ src/pmdas/linux/proc_locks.c
@@ -19,10 +19,10 @@
 int
 refresh_proc_locks(proc_locks_t *proc_locks)
 {
-    char		locktype[16], locksec[16], lockacc[16], buf[256];
+    char		type[16], access[16], buf[256];
     lock_stats_t	*lp;
     FILE		*fp;
-    int			unused, sts;
+    int			sts;
 
     memset(proc_locks, 0, sizeof(*proc_locks));
 
@@ -30,28 +30,27 @@ refresh_proc_locks(proc_locks_t *proc_locks)
 	return -oserror();
 
     while (fgets(buf, sizeof(buf), fp) != NULL) {
-	if ((sts = sscanf(buf, "%d: %15s %15s %15s %d",
-			&unused, locktype, locksec, lockacc, &unused)) != 5)
+	if ((sts = sscanf(buf, "%*d: %15s %*s %15s %*d", type, access)) != 5)
 	    continue;
-	if (strncmp(locktype, "POSIX", sizeof("POSIX")-1) == 0 ||
-	    strncmp(locktype, "ACCESS", sizeof("ACCESS")-1) == 0 ||
-	    strncmp(locktype, "OFDLCK", sizeof("OFDLCK")-1) == 0)
+	if (strncmp(type, "POSIX", sizeof("POSIX")-1) == 0 ||
+	    strncmp(type, "ACCESS", sizeof("ACCESS")-1) == 0 ||
+	    strncmp(type, "OFDLCK", sizeof("OFDLCK")-1) == 0)
 	    lp = &proc_locks->posix;
-	else if (strncmp(locktype, "FLOCK", sizeof("FLOCK")-1) == 0)
+	else if (strncmp(type, "FLOCK", sizeof("FLOCK")-1) == 0)
 	    lp = &proc_locks->flock;
-	else if (strncmp(locktype, "DELEG", sizeof("DELEG")-1) == 0 ||
-	    strncmp(locktype, "LEASE", sizeof("LEASE")-1) == 0)
+	else if (strncmp(type, "DELEG", sizeof("DELEG")-1) == 0 ||
+	    strncmp(type, "LEASE", sizeof("LEASE")-1) == 0)
 	    lp = &proc_locks->lease;
 	else
 	    continue;
 
 	lp->count++;
 
-	if (strncmp(lockacc, "READ", sizeof("READ")-1) == 0)
+	if (strncmp(access, "READ", sizeof("READ")-1) == 0)
 	    lp->read++;
-	else if (strncmp(lockacc, "WRITE", sizeof("WRITE")-1) == 0)
+	else if (strncmp(access, "WRITE", sizeof("WRITE")-1) == 0)
 	    lp->write++;
-	else if (strncmp(lockacc, "RW", sizeof("RW")-1) == 0) {
+	else if (strncmp(access, "RW", sizeof("RW")-1) == 0) {
 	    lp->write++;
 	    lp->read++;
 	}
