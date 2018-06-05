526389b7c7e54bbef7e4f5bd8b8578bdc790911a
Ken McDonell <kenj@kenj.id.au>
2018-06-05 10:15:49 +1000

    qa/632 & qa/src/timeshift.c: add support for pmlogger naming format for date-and-time
    In addition to __pmParseCtime() format, the desired time can also be
    specified using the naming convention for pmlogger archives, namely
    YYYYMMDD or YYYYMMDD.HH.MM or YYYYMMDD.HH.MM-NN

--- qa/src/timeshift.c
+++ qa/src/timeshift.c
@@ -5,7 +5,12 @@
  * Compute timeshift (pmlogrewrite format) to move start of archive
  * to a new ctime.
  *
- * Usage: timeshift [-z] -a archive "ctime"
+ * Usage: timeshift [-zd] -a archive "datetime"
+ *
+ * datetime is either the variant of ctime(3) format that __pmParseCtime()
+ * understands (namely [Day] [Mmm] [dd] [hh[:mm[:ss[.nnn]]]] [yyyy]), or
+ * a standard pmlogger base filename like YYYYMMDD or YYYYMMDD.HH.MM or
+ * YYYYMMDD.HH.MM-NN
  */
 
 #include <pcp/pmapi.h>
@@ -16,11 +21,12 @@ static pmLongOptions longopts[] = {
     PMOPT_HOSTZONE,
     PMAPI_OPTIONS_HEADER("timeshift options"),
     { "verbose", 0, 'v', "N", "diagnostic output" },
+    { "dst", 0, 'd', "N", "use daylight savings time" },
     PMAPI_OPTIONS_END
 };
 
 static pmOptions opts = {
-    .short_options = "a:vz?",
+    .short_options = "a:dvz?",
     .long_options = longopts,
     .short_usage = "[options] \"ctime\"",
 };
@@ -30,8 +36,10 @@ main(int argc, char **argv)
 {
     int			c;
     int			sts;
+    int			dflag = 0;
     int			vflag = 0;
     int			ctx;
+    int			seq;
     __pmContext		*ctxp;
     time_t		t;
     struct tm		old;
@@ -45,6 +53,10 @@ main(int argc, char **argv)
 
     while ((c = pmGetOptions(argc, argv, &opts)) != EOF) {
 	switch (c) {
+
+	case 'd':	/* daylight saving time */
+	    dflag++;
+	    break;
 
 	case 'v':	/* verbose diagnostics */
 	    vflag++;
@@ -94,18 +106,69 @@ main(int argc, char **argv)
     pmLocaltime(&t, &old);
 
     if (vflag)
-	fprintf(stderr, "label: %ld %04d-%02d-%02d %02d:%02d:%02d\n", 
-	    (long)t, old.tm_year+1900, old.tm_mon+1, old.tm_mday, old.tm_hour, old.tm_min, old.tm_sec);
+	fprintf(stderr, "label: %ld %04d-%02d-%02d %02d:%02d:%02d dst=%d\n", 
+	    (long)t, old.tm_year+1900, old.tm_mon+1, old.tm_mday, old.tm_hour, old.tm_min, old.tm_sec, old.tm_isdst);
 
-    if (__pmParseCtime(argv[argc-1], &new, &errmsg) != 0) {
-	fprintf(stderr, "__pmParseCtime: failed:\n%s\n", errmsg);
-	exit(EXIT_FAILURE);
+    /*
+     * try the variants for datetime in turn ... longest to shortest
+     * of the filename variants, then the ctime-like format
+     */
+    memset(&new, 0, sizeof(new));
+    /* try YYYYMMDD.HH.MM-NN */
+    if ((sts = sscanf(argv[argc-1], "%4d%2d%2d.%2d.%2d-%2d",
+	    &new.tm_year, &new.tm_mon, &new.tm_mday, &new.tm_hour, &new.tm_min, &seq)) != 6) {
+	memset(&new, 0, sizeof(new));
+	/* try YYYYMMDD.HH.MM */
+	if ((sts = sscanf(argv[argc-1], "%4d%2d%2d.%2d.%2d",
+		&new.tm_year, &new.tm_mon, &new.tm_mday, &new.tm_hour, &new.tm_min)) != 5) {
+	    memset(&new, 0, sizeof(new));
+	    /* try YYYYMMDD */
+	    if ((sts = sscanf(argv[argc-1], "%4d%2d%2d",
+		    &new.tm_year, &new.tm_mon, &new.tm_mday)) != 3) {
+		memset(&new, 0, sizeof(new));
+		/* try ctime-like */
+		if (__pmParseCtime(argv[argc-1], &new, &errmsg) != 0) {
+		    fprintf(stderr, "__pmParseCtime: failed:\n%s\n", errmsg);
+		    exit(EXIT_FAILURE);
+		} 
+		sts = 1;
+	    }
+	}
     }
+
+    if (sts > 1) {
+	/* not ctime-like, so syntax looks OK, now for some semantic checks */
+	if (new.tm_year < 1970) {
+	    fprintf(stderr, "Error: Year (%04d) is bad\n", new.tm_year);
+	    sts = -1;
+	}
+	new.tm_year -= 1900;
+	if (new.tm_mon < 1 || new.tm_mon > 12) {
+	    fprintf(stderr, "Error: Month (%02d) is bad\n", new.tm_mon);
+	    sts = -1;
+	}
+	new.tm_mon--;		/* base 0 */
+	/*
+	 * crude check here, but I'm not doing the "days per month, oh and
+	 * is it a leap year" thing
+	 */
+	if (new.tm_mday < 1 || new.tm_mday > 31) {
+	    fprintf(stderr, "Error: Day (%02d) is bad\n", new.tm_mday);
+	    sts = -1;
+	}
+    }
+
+    if (sts < 0)
+	exit(EXIT_FAILURE);
+
+    if (dflag)
+	new.tm_isdst = 1;
+
     __pmConvertTime(&new, &epoch, &tv);
 
     if (vflag)
-	fprintf(stderr, "want: %ld %04d-%02d-%02d %02d:%02d:%02d\n", 
-	    (long)tv.tv_sec, new.tm_year+1900, new.tm_mon+1, new.tm_mday, new.tm_hour, new.tm_min, new.tm_sec);
+	fprintf(stderr, "want: %ld %04d-%02d-%02d %02d:%02d:%02d dst=%d\n", 
+	    (long)tv.tv_sec, new.tm_year+1900, new.tm_mon+1, new.tm_mday, new.tm_hour, new.tm_min, new.tm_sec, new.tm_isdst);
 
     delta = tv.tv_sec - t;
 
