6ce2c6d1becfedf244a927b73767ee4900106d70
Lukas Berk <lberk@redhat.com>
2019-01-24 09:46:24 -0500

    qa: update qa for vagrant with centos7/fedora29

--- provisioning/qa.sh
+++ provisioning/qa.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
 sudo su - pcpqa -c "./check ${1} >runqa.out 2>&1"
 sudo cp "/var/lib/pcp/testsuite/runqa.out" /qaresults/
-sudo cp "/var/lib/pcp/testsuite/"*.bad /qaresults/
+for file in /var/lib/pcp/testsuite/*.bad
+do
+    sudo cp "$file" /qaresults/
+done
