2c5d1e16581f3098479b99f0c51154fabce90a64
Nathan Scott <nathans@redhat.com>
2019-02-18 10:41:34 +1100

    libpcp_web: fix small memory leak spotted by coverity
    Ensure we free strings we allocate when parsing configs.

--- src/libpcp_web/src/config.c
+++ src/libpcp_web/src/config.c
@@ -18,12 +18,24 @@
 
 int pmIniFileParse(sds tool, ini_handler handler, void* user)
 {
+    int sts;
     sds cwd_config = sdscatprintf(sdsempty(), "./%s.conf", tool);
     sds homedir_config = sdscatprintf(sdsempty(), "%s/.%s.conf", getenv("HOME"), tool); /* threadsafe */
     sds homedir_pcp_config = sdscatprintf(sdsempty(), "%s/.pcp/%s.conf", getenv("HOME"), tool); /* threadsafe */
     sds sysconf_dir_config = sdscatprintf(sdsempty(), "%s/%s/%s.conf", pmGetConfig("PCP_SYSCONF_DIR"), tool, tool);
-    ini_parse(sysconf_dir_config, handler, user);
-    ini_parse(homedir_pcp_config, handler, user);
-    ini_parse(homedir_config, handler, user);
-    return ini_parse(cwd_config, handler, user);
+
+    sts = ini_parse(sysconf_dir_config, handler, user);
+    if (sts >= 0)
+	sts = ini_parse(homedir_pcp_config, handler, user);
+    if (sts >= 0)
+	sts = ini_parse(homedir_config, handler, user);
+    if (sts >= 0)
+	sts = ini_parse(cwd_config, handler, user);
+
+    sdsfree(cwd_config);
+    sdsfree(homedir_config);
+    sdsfree(homedir_pcp_config);
+    sdsfree(sysconf_dir_config);
+
+    return sts;
 }
