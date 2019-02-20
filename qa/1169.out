performancecopilot/pcp

commit 069b30b0a6c2f12a4317a5553b0da4f17962a5ae

pmrep: fix a dynamic header corner case with missing values
This is hard to reproduce, seem to need pmrep -1 -X "" and
derived metrics using e.g. rate() with no values available,
affects only the first line of report. Will try to figure
out a bit later a proper test archive to cover this case.

Before:

            PID         UID    %usr %syst           %guest      %CPU
00:00:00 017662         996 84942.0     1          pmproxy    201370

            PID         UID    %CPU   CPU          Command p.p.utime
00:00:00 017662         996  101097     1          pmproxy    239340

src/pmrep/pmrep.py
@@ -867,7 +867,8 @@ class PMReporter(object):
             labels.append(self.metrics[metric][0])
         for i, metric in enumerate(self.metrics):
             if self.colxrow is not None:
-                if self.metrics[metric][0] in labels:
+                if self.metrics[metric][0] in labels or \
+                   (self.dynamic_header and results and not results[metric]):
                     continue
                 add_header_items(metric, None)
                 continue

commit 7d1b1c5f4cef1a0b59de1062b0972618c5120eee

Add QA for pmrep commit 069b30b
Fails with commit 069b30b reverted, current output being correct.

qa/1169
@@ -2,7 +2,7 @@
 # PCP QA Test No. 1169
 # pmrep dynamic header tests.
 #
-# Copyright (c) 2018 Red Hat.
+# Copyright (c) 2018-2019 Red Hat.
 #
 
 seq=`basename $0`
@@ -65,6 +65,14 @@ pmrep $log -p -u -X test -1 -g         disk.dev.read disk.dev.write mem.util.use
 
 echo "== stdout/colxrow test with extended/repeating/separate header"
 pmrep $log -p -u -X test -1 -x -E 3 -g disk.dev.read disk.dev.write mem.util.used | _archive_filter
+
+echo "== stdout/colxrow test with derived metric missing values using rate"
+pmrep $log -p -u -X test -1 -S @00:00:02 -s 2 -J 3 -e 'a = 100 * rate(disk.dev.read)' \
+                                      disk.dev.read disk.dev.write a mem.util.used | _archive_filter
+
+echo "== stdout/colxrow test with derived metric missing values using raw values"
+pmrep $log -p -u -X test -1 -S @00:00:02 -s 2 -J 3 -e 'a = 100 * rate(disk.dev.read)' -r \
+                                      disk.dev.read disk.dev.write a mem.util.used | _archive_filter
 
 
 echo "== csv test"

qa/1169.out
@@ -910,6 +910,31 @@ QA output created by 1169
 
                        1         2
 00:00:19   sdb     1.000     1.000
+== stdout/colxrow test with derived metric missing values using rate
+          test  m.u.used
+                   Kbyte
+00:00:02     =         2
+
+          test  d.d.read  d.d.write    a  m.u.used
+                 count/s    count/s  cou     Kbyte
+00:00:03   sda     1.000      1.000  100         ~
+00:00:03   sdb     1.000      1.000  100         ~
+00:00:03   sdc     1.000      1.000  100         ~
+00:00:03     =         ~          ~    ~         3
+== stdout/colxrow test with derived metric missing values using raw values
+          test  d.d.read  d.d.write  m.u.used
+                   count      count     Kbyte
+00:00:02   sda         2          2         ~
+00:00:02   sdb         2          2         ~
+00:00:02   sdc         2          2         ~
+00:00:02     =         ~          ~         2
+
+          test  d.d.read  d.d.write    a  m.u.used
+                   count      count  cou     Kbyte
+00:00:03   sda         3          3  100         ~
+00:00:03   sdb         3          3  100         ~
+00:00:03   sdc         3          3  100         ~
+00:00:03     =         ~          ~    ~         3
 == csv test
 1970-01-01 00:00:00
 Time,"disk.dev.read-sda","disk.dev.read-sdb","disk.dev.write-sda","disk.dev.write-sdb"