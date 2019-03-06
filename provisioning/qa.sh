d04df26d4840ec74701e47744e422089533e5d55
Lukas Berk <lberk@redhat.com>
2019-03-06 10:26:24 -0500

    Vagrant: Update qa privisioning script output message
    Don't return an error if tests are run and everything passes.
    Return 1 (error) if no tests are run
    Return 1 (error) if there are TEST.out.bad files found

--- provisioning/qa.sh
+++ provisioning/qa.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 sudo su - pcpqa -c "./check ${1} >runqa.out 2>&1"
 sudo cp "/var/lib/pcp/testsuite/runqa.out" /qaresults/
-for file in /var/lib/pcp/testsuite/*.bad
-do
-    sudo cp "$file" /qaresults/
-done
+if [ `ls /var/lib/pcp/testsuite/*.bad >/dev/null 2>&1` ] ; then
+    for file in /var/lib/pcp/testsuite/*.bad
+    do
+        sudo cp "$file" /qaresults/
+    done
+    exit 1
+elif [ ! -f '/var/lib/pcp/testsuite/runqa.out' ] ; then
+    echo "No Tests run"
+    exit 1
+else
+    echo "All Tests Passed."
+    exit 0
+fi
