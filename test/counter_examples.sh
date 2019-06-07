cb9eed0a8557cd9822338e38201a39496434a8a1
mfoltyn <mfoltyn@localhost.localdomain>
2019-06-07 19:23:47 +0200

    small edits to basic parser

---
+++ test/counter_examples.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd ..
+
+make clean && make
+
+make run &
+
+cd test
+
+# wait until program is ready to listen
+# not sure how else to it now
+sleep 3;
+
+echo "<TEST DATA SEND START>"
+# valid
+echo "VALID CASES:"
+./data/counter/valid.sh
+# invalid
+echo "INVALID CASES:"
+./data/counter/invalid.sh
+echo "<TEST DATA SEND END>"
+
+> debug
+pid=$(pgrep pcp-statsd)
+kill -USR1 $pid
+# I have no idea how to watch output of parallel task and block until given text is found
+sleep 5
+kill -TERM $pid
