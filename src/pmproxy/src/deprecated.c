35f592bf82157f9a1d6be357082be1f08b65796f
Nathan Scott <nathans@redhat.com>
2019-09-23 14:07:15 +1000

    libpcp_web, pmproxy: license header correction to sources (should be LGPL)

--- src/pmproxy/src/deprecated.c
+++ src/pmproxy/src/deprecated.c
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2012-2019 Red Hat.
  * Copyright (c) 1995-2002 Silicon Graphics, Inc.  All Rights Reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- * 
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
  */
 
 #include "pmapi.h"
