a347ab04ebcda07ebdd667bc30a5c306351cf464
Shogo Matsumoto <shogo.matsumoto@jp.fujitsu.com>
2020-01-22 09:42:08 +1100

    pcp-pidstat: refinements to out-of-order archived metric checks
    Use a try/except block to ensure all possible out-of-order archive
    metrics are considered, preventing missing metrics exceptions.
    
    Related to https://github.com/performancecopilot/pcp/pull/822

--- src/pcp/pidstat/pcp-pidstat.py
+++ src/pcp/pidstat/pcp-pidstat.py
@@ -821,8 +821,7 @@ class PidstatReport(pmcc.MetricGroupPrinter):
         header_string += '(' + group['kernel.uname.nodename'].netValues[0][2] + ')  '
         header_string += time_string + '  '
         header_string += group['kernel.uname.machine'].netValues[0][2] + '  '
-        no_cpu =self.get_ncpu(group)
-        print("%s  (%s CPU)" % (header_string,no_cpu))
+        print("%s  (%s CPU)" % (header_string, self.get_ncpu(group)))
 
     def get_ncpu(self,group):
         return group['hinv.ncpu'].netValues[0][2]
@@ -836,16 +835,20 @@ class PidstatReport(pmcc.MetricGroupPrinter):
         if not group['hinv.ncpu'].netValues or not group['kernel.uname.sysname'].netValues:
             return
 
-        if not self.Machine_info_count:
-            self.print_machine_info(group, manager)
-            self.Machine_info_count = 1
+        try:
+            ncpu = self.get_ncpu(group)
+            if not self.Machine_info_count:
+                self.print_machine_info(group, manager)
+                self.Machine_info_count = 1
+        except IndexError:
+            # missing some metrics
+            return
 
         ts = group.contextCache.pmLocaltime(int(group.timestamp))
         timestamp = time.strftime(PidstatOptions.timefmt, ts.struct_time())
         interval_in_seconds = self.timeStampDelta(group)
         header_indentation = "        " if len(timestamp)<9 else (len(timestamp)-7)*" "
         value_indentation = ((len(header_indentation)+9)-len(timestamp))*" "
-        ncpu = self.get_ncpu(group)
 
         metric_repository = ReportingMetricRepository(group)
 
