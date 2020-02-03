6733971c94ec12102dae94af6156b3e802106c56
Andreas Gerstmayr <agerstmayr@redhat.com>
2020-02-03 16:31:57 +0100

    ci: change VM size, update computer name, incr timeout, start redis
    about 50% savings when using Standard_B2s instead of Standard_F2s_v2
    (and just a few minutes longer builds)

--- build/ci/scripts/start_vmss.sh
+++ build/ci/scripts/start_vmss.sh
@@ -13,7 +13,7 @@ az vmss create \
   --resource-group "${AZ_RESOURCE_GROUP}" \
   --name "${AZ_VMSS}" \
   --vm-sku "${AZ_VM_SIZE}" \
-  --computer-name-prefix "${AZ_VMSS}-inst" \
+  --computer-name-prefix "pcpbuild" \
   --instance-count "$2" \
   --image "${AZ_IMAGE}" \
   --lb "" \
