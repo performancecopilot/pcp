10b49c7179f51857a3f43a06936de2c85d5b692a
Andreas Gerstmayr <agerstmayr@redhat.com>
2020-02-05 16:57:32 +0100

    ci: fix hostname resolution
    sometimes hostname resolution doesn't work in VMSS instances

--- build/ci/common/redhat/install.sh
+++ build/ci/common/redhat/install.sh
@@ -2,4 +2,6 @@
 
 cd ./artifacts
 sudo rpm -iv *.rpm
-echo 'pcpqa ALL=(ALL)   NOPASSWD: ALL' | sudo tee /etc/sudoers.d/pcpqa
+
+echo "pcpqa ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/pcpqa
+echo "127.0.0.1 $(hostname)" | sudo tee -a /etc/hosts
