af6d09b9ad5535dd1bc78cb68ddb9ee40af747ca
Andreas Gerstmayr <agerstmayr@redhat.com>
2020-02-25 23:13:57 +0100

    ci: add hostname in test report

--- build/ci/scripts/start_tests.sh
+++ build/ci/scripts/start_tests.sh
@@ -26,7 +26,7 @@ echo Generate JUnit output
 cat << EOF > "${tests_junit_file}"
 <?xml version="1.0" encoding="UTF-8"?>
 <testsuites>
-  <testsuite name="tests">
+  <testsuite name="tests" hostname="${AZ_VMSS}">
     $(cat "${tests_results_dir}"/*/stderr)
   </testsuite>
 </testsuites>
