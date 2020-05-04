cac6c5b4f115f5eebf449415c8e5000581be7acd
Nathan Scott <nathans@redhat.com>
2020-05-04 10:55:50 +1000

    pmlogconf: rewrite the shell scripts as a single C program for speed
    The venerable pmlogconf utility has been rewritten as a program now,
    to resolve performance issues around its use in pmlogger setup.  Its
    now able to complete a typical localhost configuration in 20-30 msec
    where the previous version took 6-8 sec to run.  This should help us
    with both local service startup times, and also scaling up pmlogger
    farms to greater numbers of hosts.
    
    The functionality is equivalent, with the exception of retiring the
    IRIX v1 format; for the past 10+ years this format was automatically
    transitioned to the v2 format used in all open source releases, and
    it just doesn't seem worthwhile carrying that code forward anymore
    (it would need to be re-implemented).
    
    One small change is that the groups are now kept sorted, in-memory
    and on-disk - this sometimes results in a one-time reconfiguration,
    and several of the QA tests have slightly different output.
    
    The (undocumented, internal) pmlogconf-setup script has been retired
    completely.  Previously this functionality was implemented by these
    two scripts, now its an all-on-one command.  I've added some notes
    to the pcpcompat(1) man page describing this, and a -s option to the
    new pmlogconf in case anyone needs that old output format (I don't
    expect this to be the case, however it is handy for use by QA).
    
    A new -g/--group option is added, allowing an existing setup to be
    queried in terms of states of individual groups - this is expected
    to be used in configuration automation systems (like Ansible).
    
    Resolves https://github.com/performancecopilot/pcp/issues/895

---
+++ src/pmlogconf/util.h
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2020 Red Hat.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+#ifndef PMLOGCONF_UTIL_H
+#define PMLOGCONF_UTIL_H
+
+#include "pmapi.h"
+#include "libpcp.h"
+#include <ctype.h>
+#ifdef HAVE_REGEX_H
+#include <regex.h>
+#endif
+
+extern int istoken(const char *, const char *, size_t);
+extern char *trim(const char *);
+extern char *chop(char *);
+extern char *copy_string(const char *);
+extern char *append(char *, const char *, int);
+extern char *copy_token(const char *);
+
+typedef void (*fmt_t)(const char *, int, void *);
+extern void fmt(const char *, char *, size_t, int, int, fmt_t, void *);
+
+extern int values_hash(pmResult *);
+extern pmValueSet *metric_values(pmID);
+extern int metric_type(pmID);
+
+typedef int (*numeric_cmp_t)(double, double);
+extern int number_equal(double, double);
+extern int number_nequal(double, double);
+extern int number_greater(double, double);
+extern int number_gtequal(double, double);
+extern int number_lessthan(double, double);
+extern int number_ltequal(double, double);
+
+typedef int (*string_cmp_t)(const char *, const char *);
+extern int string_equal(const char *, const char *);
+extern int string_nequal(const char *, const char *);
+extern int string_greater(const char *, const char *);
+extern int string_gtequal(const char *, const char *);
+extern int string_lessthan(const char *, const char *);
+extern int string_ltequal(const char *, const char *);
+
+typedef int (*regex_cmp_t)(const regex_t *, const char *);
+extern int string_regexp(const regex_t *, const char *);
+extern int string_nregexp(const regex_t *, const char *);
+
+enum { STATE_AVAILABLE = 1, STATE_INCLUDE, STATE_EXCLUDE };
+
+#define AVAILABLE	"available"
+#define AVAILABLE_LEN	(sizeof(AVAILABLE)-1)
+#define INCLUDE		"include"
+#define INCLUDE_LEN	(sizeof(INCLUDE)-1)
+#define EXCLUDE		"exclude"
+#define EXCLUDE_LEN	(sizeof(EXCLUDE)-1)
+#define EXISTS		"exists"
+#define EXISTS_LEN	(sizeof(EXISTS)-1)
+#define VALUES		"values"
+#define VALUES_LEN	(sizeof(VALUES)-1)
+
+enum { PROBE_EXISTS = 1, PROBE_VALUES,
+       PROBE_EQ, PROBE_NEQ, PROBE_GT, PROBE_GE,
+       PROBE_LT, PROBE_LE, PROBE_RE, PROBE_NRE
+};
+
+#define EQUAL			"=="
+#define EQUAL_LEN		(sizeof(EQUAL)-1)
+#define NOTEQUAL		"!="
+#define NOTEQUAL_LEN		(sizeof(NOTEQUAL)-1)
+#define GREATER			">"
+#define GREATER_LEN		(sizeof(GREATER)-1)
+#define GTEQUAL			">="
+#define GTEQUAL_LEN		(sizeof(GTEQUAL)-1)
+#define LESSER			"<"
+#define LESSER_LEN		(sizeof(LESSER)-1)
+#define LTEQUAL			"<="
+#define LTEQUAL_LEN		(sizeof(LTEQUAL)-1)
+#define REGEXP			"~"
+#define REGEXP_LEN		(sizeof(REGEXP)-1)
+#define NOTREGEXP		"!~"
+#define NOTREGEXP_LEN		(sizeof(NOTREGEXP)-1)
+
+extern const char *operandstr(unsigned int);
+
+enum { LOG_ADVISORY, LOG_MANDATORY };
+
+extern const char *loggingstr(unsigned int);
+
+#endif
