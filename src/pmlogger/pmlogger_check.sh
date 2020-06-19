performancecopilot/pcp

commit f4887081796782673250424bc8ec39eee61e7383

src/pmlogger/pmlogger_check.sh: don't update the Latest folio
This is now done by pmlogger.

src/pmlogger/pmlogger_check.sh
@@ -786,8 +786,6 @@ END				{ print m }'`
 
 	if [ -z "$pid" -a $START_PMLOGGER = true ]
 	then
-	    rm -f Latest
-
 	    if [ "X$primary" = Xy ]
 	    then
 		envs=`grep ^PMLOGGER "$PMLOGGERENVS" 2>/dev/null`
@@ -875,20 +873,6 @@ END				{ print m }'`
 
 	    # wait for pmlogger to get started, and check on its health
 	    _check_logger $pid
-
-	    # the archive folio Latest is for the most recent archive in
-	    # this directory
-	    #
-	    mylogname=`ls *.0 2>/dev/null | tail -1 | sed -e 's/\.0$//'`
-	    if [ -z "$mylogname" ]
-	    then
-		echo "$prog: Error: archive file $LOGNAME missing"
-	    elif [ -f "$mylogname.0" ] 
-	    then
-		$VERBOSE && echo "Latest folio created for $mylogname"
-		mkaf $mylogname.0 >Latest 2>/dev/null
-		chown $PCP_USER:$PCP_GROUP Latest >/dev/null 2>&1
-	    fi
 
 	    # if SaveLogs exists in the same directory that the archive
 	    # is being created, save pmlogger log file there as well

commit 9d340416d4c36a07c0fd6ef7f540b51872286855

pmlogger_check.sh: fix botch if <hostname>/SaveLogs exists
Commit f4887081 accidentally removed the initialization of $mylogname
... in review the initialization was NQR, so fix it!

The scope of this in real terms is minimal, as only someone desperate
to debug a pmlogger farm would have created the ...<hostname>/SaveLogs
directory in the first place and without this directory existing the
bugs are benign.

src/pmlogger/pmlogger_check.sh
@@ -997,15 +997,26 @@ END				{ print m }'`
 	    # if SaveLogs exists in the same directory that the archive
 	    # is being created, save pmlogger log file there as well
 	    #
-	    dirname=`dirname $mylogname.0`
-	    if [ -d $dirname/SaveLogs ]
+	    if [ -d ./SaveLogs ]
 	    then
-		if [ ! -f $dirname/SaveLogs/$mylogname.log ]
+		# get archive basename, which is the expanded version
+		# of $LOGNAME, possibly with duplicate resolution ...
+		# the $PCP_TMP_DIR/pmlogger file has the answer
+		#
+		mylogname=`sed -n -e 3p $PCP_TMP_DIR/pmlogger/$pid 2>/dev/null \
+		           | sed -e 's;.*/;;'`
+		if [ -n "$mylogname" ]
 		then
-		    $LN $logfile $dirname/SaveLogs/$mylogname.log
+		    if [ ! -f ./SaveLogs/$mylogname.log ]
+		    then
+			$LN $logfile ./SaveLogs/$mylogname.log
+		    else
+			$VERBOSE && echo "Failed to link $logfile, SaveLogs/$mylogname.log already exists"
+		    fi
+		else
+		    $VERBOSE && echo "Failed to get archive basename from $PCP_TMP_DIR/pmlogger/$pid"
 		fi
 	    fi
-
 	elif [ ! -z "$pid" -a $STOP_PMLOGGER = true ]
 	then
 	    # Send pmlogger a SIGTERM, which is noted as a pending shutdown.