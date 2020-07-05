5f357cd3337d9e8cf62b9c02b0755b5433efcbb4
Shogo Matsumoto <shogo.matsumoto@fujitsu.com>
2020-07-06 08:30:13 +0900

    pmdaproc: acct: introduce struct acct_v3 which is defined in kernel (include/uapi/linux/acct.h)

--- src/pmdas/linux_proc/acct.h
+++ src/pmdas/linux_proc/acct.h
@@ -14,6 +14,35 @@
  * for more details.
  */
 
+#include <linux/types.h>
+
+typedef __u16	comp_t;
+typedef __u32	comp2_t;
+
+#define ACCT_COMM	16
+
+struct acct_v3 {
+	char		ac_flag;		/* Flags */
+	char		ac_version;		/* Always set to ACCT_VERSION */
+	__u16		ac_tty;			/* Control Terminal */
+	__u32		ac_exitcode;		/* Exitcode */
+	__u32		ac_uid;			/* Real User ID */
+	__u32		ac_gid;			/* Real Group ID */
+	__u32		ac_pid;			/* Process ID */
+	__u32		ac_ppid;		/* Parent Process ID */
+	__u32		ac_btime;		/* Process Creation Time */
+	float		ac_etime;		/* Elapsed Time */
+	comp_t		ac_utime;		/* User Time */
+	comp_t		ac_stime;		/* System Time */
+	comp_t		ac_mem;			/* Average Memory Usage */
+	comp_t		ac_io;			/* Chars Transferred */
+	comp_t		ac_rw;			/* Blocks Read or Written */
+	comp_t		ac_minflt;		/* Minor Pagefaults */
+	comp_t		ac_majflt;		/* Major Pagefaults */
+	comp_t		ac_swaps;		/* Number of Swaps */
+	char		ac_comm[ACCT_COMM];	/* Command Name */
+};
+
 enum {
 	ACCT_TTY      = 0,
 	ACCT_EXITCODE = 1,
