ca3444b2bc6a4693ae953af809edf1f11eae24c6
Shogo Matsumoto <shogo.matsumoto@fujitsu.com>
2020-07-06 08:31:20 +0900

    pmdaproc: acct: initialize acct information

--- src/pmdas/linux_proc/acct.c
+++ src/pmdas/linux_proc/acct.c
@@ -16,7 +16,45 @@
 
 #include "acct.h"
 
+#define RINGBUF_SIZE			5000
+unsigned long hertz;
+
+static struct {
+	const char* path;
+	int fd;
+	int prev_size;
+	int acct_enabled;
+	int version;
+	int record_size;
+	time_t last_fail_open;
+	time_t last_check_accounting;
+} acct_file;
+
+typedef struct {
+	time_t time;
+	struct pmdaInstid instid;
+} acct_ringbuf_entry_t;
+
+static struct {
+	acct_ringbuf_entry_t *buf;
+	int next_index;
+} acct_ringbuf;
+
+static void init_acct_file_info(void) {
+	memset(&acct_file, 0, sizeof(acct_file));
+	acct_file.fd = -1;
+}
+
 void acct_init(proc_acct_t *proc_acct) {
+	init_acct_file_info();
+
+	acct_ringbuf.next_index = 0;
+	acct_ringbuf.buf = calloc(RINGBUF_SIZE, sizeof(acct_ringbuf_entry_t));
+
+	proc_acct->indom->it_numinst = 0;
+	proc_acct->indom->it_set = calloc(RINGBUF_SIZE, sizeof(pmdaInstid));
+
+	hertz = sysconf(_SC_CLK_TCK);
 }
 
 void refresh_acct(proc_acct_t *proc_acct) {
