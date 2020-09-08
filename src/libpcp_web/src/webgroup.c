performancecopilot/pcp

commit c88e1c906780e4dcf13deaa08e6951dc05cc6049

pmproxy: fix REST API time series ids for metrics with indom
Some labels were not guaranteed to be present when the SHA1
hash for metric identifiers was calculated - make it so.

Resolves https://github.com/performancecopilot/pcp/issues/1017

src/libpcp_web/src/webgroup.c
@@ -1396,8 +1396,10 @@ webmetric_lookup(const char *name, void *arg)
 
     pmwebapi_add_domain_labels(cp, mp->cluster->domain);
     pmwebapi_add_cluster_labels(cp, mp->cluster);
-    if (mp->indom)
+    if (mp->indom) {
+	pmwebapi_add_instances_labels(cp, mp->indom);
 	pmwebapi_add_indom_labels(mp->indom);
+    }
     pmwebapi_add_item_labels(cp, mp);
     pmwebapi_metric_hash(mp);
     pmwebapi_metric_help(cp, mp);

qa/1543
@@ -101,10 +101,11 @@ _filter_json()
 	    -e 's,"sec": [0-9][0-9]*,"sec": SECONDS,g' \
 	#end
 	context < $tmp.filtered > $tmp.context
+	series < $tmp.filtered > $tmp.series
     else
 	echo "Invalid JSON: $status"
 	cat $tmp.unfiltered
-	rm -f $tmp.context
+	rm -f $tmp.context $tmp.series
     fi
 }
 
@@ -115,6 +116,23 @@ context()
 	-e 's/.*context\": //g' \
 	-e 's/,$//g' \
     #end
+}
+
+series()
+{
+    grep '"series"' | \
+    sed \
+	-e 's/.*series\": "//g' \
+	-e 's/",$//g' \
+    #end
+}
+
+infoseries()
+{
+    echo "== pminfo -s $@ ==" >> $here/$seq.full
+    pminfo -s "$1" | tee -a $here/$seq.full | \
+    grep Series: | \
+    awk '{ print $NF }'
 }
 
 section()
@@ -197,7 +215,23 @@ section "GET /pmapi/metric"
 # optional params: prefix, names, name
 
 curl -s "http://localhost:$port/pmapi/metric?name=sample.long.ten" \
-	| _filter_json "metric name"
+	| _filter_json "metric name (single)"
+webapiseries=`cat $tmp.series`
+pminfoseries=`infoseries sample.long.ten`
+if [ "$webapiseries" != "$pminfoseries" ]; then
+	echo "Series mismatch (single): \"$webapiseries\" not equal to \"$pminfoseries\""
+else
+	echo "Series checked (single)"
+fi
+curl -s "http://localhost:$port/pmapi/metric?name=sample.long.bin" \
+	| _filter_json "metric name (indom)"
+webapiseries=`cat $tmp.series`
+pminfoseries=`infoseries sample.long.bin`
+if [ "$webapiseries" != "$pminfoseries" ]; then
+	echo "Series mismatch (indom): \"$webapiseries\" not equal to \"$pminfoseries\""
+else
+	echo "Series checked (indom)"
+fi
 curl -s "http://localhost:$port/pmapi/metric?prefix=sample.long" \
 	| _filter_json "metric prefix"
 curl -s "http://localhost:$port/pmapi/metric?name=bad.metric" \

qa/1543.out
@@ -78,7 +78,7 @@ Context successfully expired
 
 *****   GET /pmapi/metric   *****
 
-== metric name ==
+== metric name (single) ==
 {
     "context": "CONTEXT"
     "metrics": [
@@ -101,6 +101,32 @@ Context successfully expired
         }
     ]
 }
+Series checked (single)
+== metric name (indom) ==
+{
+    "context": "CONTEXT"
+    "metrics": [
+        {
+            "name": "sample.long.bin",
+            "series": "SERIES"
+            "pmid": "29.0.103",
+            "indom": "29.2",
+            "type": "32",
+            "sem": "instant",
+            "units": "none",
+            "labels": {
+                "agent": "sample",
+                "cluster": "zero",
+                "domainname": "DOMAINNAME"
+                "hostname": "HOSTNAME"
+                "role": "testing"
+            },
+            "text-oneline": "like sample.bin but type 32",
+            "text-help": "like sample.bin but type 32"
+        }
+    ]
+}
+Series checked (indom)
 == metric prefix ==
 {
     "context": "CONTEXT"

commit 28c75a5be391508f942379a9ef580965ddb28fbb

libpcp_web, qa: fix recent regression in test 662
Fixed regression picked up by test 662 relating to pmStore(3)
interacting incorrectly with following pmFetch(3) on metrics
with instances.  Fallout from commit c88e1c906780.

src/libpcp_web/src/webgroup.c
@@ -1370,6 +1370,7 @@ webmetric_lookup(const char *name, void *arg)
     seriesname_t	*snp = NULL;
     context_t		*cp = lookup->context;
     metric_t		*mp;
+    indom_t		*ip;
 
     if (webgroup_use_context(cp, &lookup->status, &lookup->message, arg) == NULL)
 	return;
@@ -1396,9 +1397,10 @@ webmetric_lookup(const char *name, void *arg)
 
     pmwebapi_add_domain_labels(cp, mp->cluster->domain);
     pmwebapi_add_cluster_labels(cp, mp->cluster);
-    if (mp->indom) {
-	pmwebapi_add_instances_labels(cp, mp->indom);
-	pmwebapi_add_indom_labels(mp->indom);
+    if ((ip = mp->indom) != NULL) {
+	if (pmwebapi_add_indom_instances(cp, ip) > 0)
+	    pmwebapi_add_instances_labels(cp, ip);
+	pmwebapi_add_indom_labels(ip);
     }
     pmwebapi_add_item_labels(cp, mp);
     pmwebapi_metric_hash(mp);
@@ -1864,12 +1866,15 @@ store_add_instid(struct instore *store, int id)
 static void
 store_add_profile(struct instore *store)
 {
+    int		sts;
+
     if (store->insts) {
-	    int sts;
 	sts = pmDelProfile(store->indom, 0, NULL);
-	fprintf(stderr, "pmDelProfile: sts=%d\n", sts);
+	if (pmDebugOptions.libweb)
+	    fprintf(stderr, "pmDelProfile: sts=%d\n", sts);
 	sts = pmAddProfile(store->indom, store->count, store->insts);
-	fprintf(stderr, "pmAddProfile: sts=%d\n", sts);
+	if (pmDebugOptions.libweb)
+	    fprintf(stderr, "pmAddProfile: sts=%d\n", sts);
 	free(store->insts);
 	store->insts = NULL;
     }