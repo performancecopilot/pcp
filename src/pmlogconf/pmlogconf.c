952e58e56f6bfb436313920e2d601ff644327e7f
Nathan Scott <nathans@redhat.com>
2020-10-06 19:35:03 +1100

    pmlogconf: remove needless variable setting (covscan)

--- src/pmlogconf/pmlogconf.c
+++ src/pmlogconf/pmlogconf.c
@@ -989,7 +989,6 @@ create_tempfile(FILE *file, FILE **tempfile, struct stat *stat)
     FILE		*fp;
     char		bytes[BUFSIZ];
     mode_t		cur_umask;
-    int			sts;
 
     /* create a temporary file in the same directory as config (for rename) */
     pmsprintf(bytes, sizeof(bytes), "%s.new", config);
@@ -1012,11 +1011,11 @@ create_tempfile(FILE *file, FILE **tempfile, struct stat *stat)
     umask(cur_umask);
 
     if (stat) {
-	if ((sts = fchown(fd, stat->st_uid, stat->st_gid)) < 0) {
+	if (fchown(fd, stat->st_uid, stat->st_gid) < 0) {
 	    fprintf(stderr, "%s: Warning: fchown() failed: %s\n",
 			pmGetProgname(), osstrerror());
 	}
-	if ((sts = fchmod(fd, stat->st_mode)) < 0) {
+	if (fchmod(fd, stat->st_mode) < 0) {
 	    fprintf(stderr, "%s: Warning: fchmod() failed: %s\n",
 			pmGetProgname(), osstrerror());
 	}
