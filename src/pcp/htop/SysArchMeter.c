c7109e59d91c30e4da8aadb7501cc1163d9cf75b
Sohaib <sohaib.amhmd@gmail.com>
2021-02-16 08:01:39 +0200

    Ops, No need to SysArchMeter_display

--- src/pcp/htop/SysArchMeter.c
+++ src/pcp/htop/SysArchMeter.c
@@ -24,26 +24,10 @@ static void SysArchMeter_updateValues(Meter* this, char* buffer, size_t size) {
    snprintf(buffer, size, "%s %s [%s]", data.name, data.release, data.machine);
 }
 
-static void SysArchMeter_display(ATTR_UNUSED const Object* cast, RichString* out) {
-   SysArchInfo data;
-   Platform_setSysArch(&data);
-   char buffer[50];
-
-   xSnprintf(buffer, sizeof(buffer), "%s ", data.name);
-   RichString_writeAscii(out, CRT_colors[METER_VALUE_NOTICE] , buffer);
-
-   xSnprintf(buffer, sizeof(buffer), "%s ", data.release);
-   RichString_appendAscii(out, CRT_colors[METER_VALUE_NOTICE], buffer);
-
-   xSnprintf(buffer, sizeof(buffer), "[%s]", data.machine);
-   RichString_appendAscii(out, CRT_colors[METER_VALUE_NOTICE], buffer);
-}
-
 const MeterClass SysArchMeter_class = {
    .super = {
       .extends = Class(Meter),
       .delete = Meter_delete,
-      .display = SysArchMeter_display
    },
    .updateValues = SysArchMeter_updateValues,
    .defaultMode = TEXT_METERMODE,
