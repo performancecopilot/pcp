4dc8a93d5d277caffbbe27a76cf3631b8351b3d4
Andreas Gerstmayr <agerstmayr@redhat.com>
2021-02-18 15:08:25 +0100

    ci: fix conmon release link

--- .github/workflows/upgrade_conmon.sh
+++ .github/workflows/upgrade_conmon.sh
@@ -1,15 +1,14 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 podman --version
 
 echo Previous conmon
 /usr/libexec/podman/conmon --version
 
-wget -q -O conmon.zip https://github.com/containers/conmon/releases/download/v2.0.26/conmon-2.0.26.zip
-unzip conmon.zip
-chmod +x result/bin/conmon
-sudo cp -f result/bin/conmon /usr/bin/conmon
-sudo cp -f result/bin/conmon /usr/libexec/podman/conmon
+wget -q https://github.com/containers/conmon/releases/download/v2.0.26/conmon.amd64
+chmod +x conmon.amd64
+sudo cp -f conmon.amd64 /usr/bin/conmon
+sudo cp -f conmon.amd64 /usr/libexec/podman/conmon
 
 echo Upgraded conmon
 /usr/libexec/podman/conmon --version
