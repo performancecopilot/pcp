762bc3047ddebbdcc7f03566b943d6d5a29ad5d8
Sohaib <sohaib.amhmd@gmail.com>
2021-02-20 14:37:31 +0200

    pcp-htop: Add ZFS ARC statistics

---
+++ src/pcp/htop/zfs/ZfsArcMeter.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_ZfsArcMeter
+#define HEADER_ZfsArcMeter
+/*
+htop - ZfsArcMeter.h
+(C) 2004-2011 Hisham H. Muhammad
+Released under the GNU GPLv2, see the COPYING file
+in the source distribution for its full text.
+*/
+
+#include "ZfsArcStats.h"
+
+#include "Meter.h"
+
+void ZfsArcMeter_readStats(Meter* this, const ZfsArcStats* stats);
+
+extern const MeterClass ZfsArcMeter_class;
+
+#endif
---
+++ src/pcp/htop/zfs/ZfsArcStats.c
@@ -0,0 +1,10 @@
+/*
+htop - ZfsArcStats.c
+(C) 2014 Hisham H. Muhammad
+Released under the GNU GPLv2, see the COPYING file
+in the source distribution for its full text.
+*/
+
+#include "Macros.h"
+
+static int make_iso_compilers_happy ATTR_UNUSED;
