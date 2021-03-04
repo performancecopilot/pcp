ed1f5f7cee56ed2c4f5dc70d8c3a0ca0e100359f
Ken McDonell <kenj@kenj.id.au>
2021-03-04 14:43:50 +1100

    src/libpcp_pmda/src/open.c: strcpy -> pmstrncpy (covscan)
    Fixes Coverity CID 287870

--- src/libpcp_pmda/src/open.c
+++ src/libpcp_pmda/src/open.c
@@ -238,7 +238,7 @@ __pmdaOpenUnix(char *sockname, int *infd, int *outfd)
     }
     memset(&myaddr, 0, sizeof(myaddr));
     myaddr.sun_family = AF_UNIX;
-    strcpy(myaddr.sun_path, sockname);
+    pmstrncpy(myaddr.sun_path, sizeof(myaddr.sun_path), sockname);
     len = (int)offsetof(struct sockaddr_un, sun_path) + (int)strlen(myaddr.sun_path);
     sts = bind(sfd, (struct sockaddr*) &myaddr, len);
     if (sts < 0) {
