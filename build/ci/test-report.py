75f01d3f1901207f042291ea6cf411c0bfd0c61f
Andreas Gerstmayr <agerstmayr@redhat.com>
2021-04-27 18:03:02 +0200

    ci: rename "Daily QA" to "QA", always generate Allure report
    When testing larger commits, it's advised to run a full QA run on the
    new branch before merging. This change generates the Allure test report
    when the QA workflow is trigged manually, and publishes the report as a
    zip file.
    
    I've renamed "Daily QA" to "QA", as it can and should be invoked manually
    as well. When the workflow is run on schedule, it publishes the test
    report to GitHub pages and sends a Slack message as usual.

--- build/ci/test-report.py
+++ build/ci/test-report.py
@@ -50,22 +50,27 @@ def read_test_groups(group_path: str) -> Dict[str, List[str]]:
     return tests
 
 
-def read_testlog(qa_dir: str, testartifacts_dir: str, groups: Dict[str, List[str]],
-                 test_durations: Dict[str, List[int]], host: str) -> List[Test]:
+def read_testlog(
+    qa_dir: str,
+    testartifacts_dir: str,
+    groups: Dict[str, List[str]],
+    test_durations: Dict[str, List[int]],
+    host: str,
+) -> List[Test]:
     tests: List[Test] = []
-    testlog_path = os.path.join(testartifacts_dir, 'test.log')
+    testlog_path = os.path.join(testartifacts_dir, "test.log")
     with open(testlog_path) as testlog_file:
         for line in testlog_file:
             # [xx%] will be displayed if there are more than 9 tests
             #  Xs ... will be displayed if test was already run
-            success_m = re.match(r'^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)?\n$', line)
-            notrun_m = re.match(r'^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)? \[not run\] (.+)\n$', line)
-            failed_m = re.match(r'^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)? ((\- |\[).+)\n$', line)
-            cancelled_m = re.match(r'^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)?\Z', line)
+            success_m = re.match(r"^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)?\n$", line)
+            notrun_m = re.match(r"^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)? \[not run\] (.+)\n$", line)
+            failed_m = re.match(r"^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)? ((\- |\[).+)\n$", line)
+            cancelled_m = re.match(r"^(?:\[\d+%\] )?(\d+)(?: \d+s \.\.\.)?\Z", line)
 
             if success_m or notrun_m or failed_m or cancelled_m:
                 test_no = (success_m or notrun_m or failed_m or cancelled_m).group(1)
-                platform, runner = host.split('-')
+                platform, runner = host.split("-")
 
                 test = Test(test_no)
                 test.groups = groups[test_no]
@@ -78,9 +83,9 @@ def read_testlog(qa_dir: str, testartifacts_dir: str, groups: Dict[str, List[str
                 with open(os.path.join(qa_dir, test_no)) as f:
                     next(f)  # skip shebang
                     for test_line in f:
-                        if test_line == '\n':
+                        if test_line == "\n":
                             break
-                        test.description += test_line.strip(' #') + '\n'
+                        test.description += test_line.strip(" #") + "\n"
                 tests.append(test)
 
             if success_m:
@@ -93,39 +98,69 @@ def read_testlog(qa_dir: str, testartifacts_dir: str, groups: Dict[str, List[str
                 if failed_msg.startswith("- "):
                     failed_msg = failed_msg[2:]
 
-                test.status = Test.Status.Broken if 'failed' in failed_msg else Test.Status.Failed
+                test.status = Test.Status.Broken if "failed" in failed_msg else Test.Status.Failed
                 test.message = failed_msg + "\n"
             elif cancelled_m:
                 test.status = Test.Status.Broken
                 test.message = "test cancelled"
             elif tests and tests[-1].status in [Test.Status.Failed, Test.Status.Broken]:
                 # if this line doesn't match any regex, it's probably output from the previous test
-                if len(tests[-1].message) < 10000: # ignore output for huge diffs
+                if len(tests[-1].message) < 10000:  # ignore output for huge diffs
                     tests[-1].message += line
 
     return tests
 
 
 def read_hosts(qa_dir: str, artifacts_path: str) -> List[Test]:
-    groups = read_test_groups(os.path.join(qa_dir, 'group'))
+    groups = read_test_groups(os.path.join(qa_dir, "group"))
     hosts = set()
     tests = []
     for artifact_dir in os.listdir(artifacts_path):
-        if artifact_dir.startswith('build-'):
-            host = artifact_dir[len('build-'):]
+        if artifact_dir.startswith("build-"):
+            host = artifact_dir[len("build-") :]
             hosts.add(host)
-        elif artifact_dir.startswith('test-'):
-            host = artifact_dir[len('test-'):]
+        elif artifact_dir.startswith("test-"):
+            host = artifact_dir[len("test-") :]
             hosts.add(host)
 
             testartifacts_dir = os.path.join(artifacts_path, artifact_dir)
-            test_timings_file = os.path.join(testartifacts_dir, 'check.timings')
+            test_timings_file = os.path.join(testartifacts_dir, "check.timings")
 
-            # ignore test runs without any QA run (e.g. build failure)
-            if os.path.exists(test_timings_file):
-                test_durations = read_test_durations(test_timings_file)
-                tests.extend(read_testlog(qa_dir, testartifacts_dir, groups, test_durations, host))
+            test_durations = read_test_durations(test_timings_file)
+            tests.extend(read_testlog(qa_dir, testartifacts_dir, groups, test_durations, host))
     return list(hosts), tests
+
+
+def print_test_summary(tests: List[Test]):
+    print("TEST SUMMARY")
+
+    failed_tests = [t for t in tests if t.status in [Test.Status.Failed, Test.Status.Broken]]
+    if not failed_tests:
+        print("No test failures.")
+        return
+
+    print("\nAll failed tests:")
+    failed_tests_by_platform = defaultdict(list)
+    for test in failed_tests:
+        failed_tests_by_platform[test.platform].append(test)
+    for platform in sorted(failed_tests_by_platform):
+        print(f"{platform}:", ",".join(map(lambda t: t.name, failed_tests_by_platform[platform])))
+
+    print("\nMost failed tests:")
+    failed_tests_by_name = defaultdict(list)
+    for test in failed_tests:
+        failed_tests_by_name[test.name].append(test)
+    failed_tests_by_name_count = {name: len(tests) for name, tests in failed_tests_by_name.items()}
+    failed_tests_by_name_sorted = sorted(
+        failed_tests_by_name_count.items(),
+        key=lambda t: t[1],
+        reverse=True,
+    )
+    print(f"{'Test':>5}  {'Count':>5}")
+    for name, count in failed_tests_by_name_sorted[:10]:
+        print(f"{name:>5}  {count:>5}")
+
+    print(f"\n::error::{len(failed_tests_by_name)} test failures ({len(failed_tests)} unique)")
 
 
 def write_allure_result(test: Test, commit: str, allure_results_path: str):
@@ -135,87 +170,72 @@ def write_allure_result(test: Test, commit: str, allure_results_path: str):
         "historyId": f"{test.name}@{test.host}",
         "description": test.description,
         "status": test.status,
-        "statusDetails": {
-            "message": test.message,
-            "flaky": "flakey" in test.groups
-        },
+        "statusDetails": {"message": test.message, "flaky": "flakey" in test.groups},
         "attachments": [],
         "start": test.start * 1000,
         "stop": test.stop * 1000,
-        "labels": [{
-            "name": "suite",
-            "value": test.host
-        }, {
-            "name": "host",
-            "value": test.host
-        }],
-        "links": [{
-            "type": "test_case",
-            "url": f"https://github.com/performancecopilot/pcp/blob/{commit}/qa/{test.name}",
-            "name": "Source"
-        }, {
-            "type": "test_case",
-            "url": f"https://github.com/performancecopilot/pcp/blob/{commit}/qa/{test.name}.out",
-            "name": "Expected output"
-        }]
+        "labels": [
+            {"name": "suite", "value": test.host},
+            {"name": "host", "value": test.host},
+        ],
+        "links": [
+            {
+                "type": "test_case",
+                "url": f"https://github.com/performancecopilot/pcp/blob/{commit}/qa/{test.name}",
+                "name": "Source",
+            },
+            {
+                "type": "test_case",
+                "url": f"https://github.com/performancecopilot/pcp/blob/{commit}/qa/{test.name}.out",
+                "name": "Expected output",
+            },
+        ],
     }
 
     for group in test.groups:
         allure_result["labels"].append({"name": "epic", "value": group})
 
     if test.status in [Test.Status.Failed, Test.Status.Broken]:
-        allure_result[
-            "description"] += (f"**To reproduce this test in a container, please run:**\n\n"
-                               f"    ./build/ci/run.py --runner {test.runner} --platform {test.platform} reproduce")
+        allure_result["description"] += (
+            f"**To reproduce this test in a container, please run:**\n\n"
+            f"    ./build/ci/run.py --runner {test.runner} --platform {test.platform} reproduce"
+        )
 
         out_bad = os.path.join(test.path, f"{test.name}.out.bad")
         if os.path.exists(out_bad):
             out_bad_attachment_fn = f"{test.host}-{test.name}.out.bad-attachment.txt"
             shutil.copyfile(out_bad, os.path.join(allure_results_path, out_bad_attachment_fn))
-            allure_result["attachments"].append({
-                "type": "text/plain",
-                "name": f"{test.name}.out.bad",
-                "source": out_bad_attachment_fn
-            })
+            allure_result["attachments"].append(
+                {
+                    "type": "text/plain",
+                    "name": f"{test.name}.out.bad",
+                    "source": out_bad_attachment_fn,
+                }
+            )
 
         out_full = os.path.join(test.path, f"{test.name}.full")
         if os.path.exists(out_full):
             out_full_attachment_fn = f"{test.host}-{test.name}.full-attachment.txt"
             shutil.copyfile(out_full, os.path.join(allure_results_path, out_full_attachment_fn))
-            allure_result["attachments"].append({
-                "type": "text/plain",
-                "name": f"{test.name}.full",
-                "source": out_full_attachment_fn
-            })
+            allure_result["attachments"].append(
+                {
+                    "type": "text/plain",
+                    "name": f"{test.name}.full",
+                    "source": out_full_attachment_fn,
+                }
+            )
 
-    with open(os.path.join(allure_results_path, f"{test.host}-{test.name}-result.json"), 'w') as f:
+    with open(os.path.join(allure_results_path, f"{test.host}-{test.name}-result.json"), "w") as f:
         json.dump(allure_result, f)
 
 
-def print_test_summary(tests: List[Test]):
-    print("TEST SUMMARY")
-    print()
-
-    grouped_by_name = defaultdict(list)
-    for test in tests:
-        grouped_by_name[test.name].append(test)
-
-    print("Most failed tests:")
-    failed_tests = {name: len([t for t in tests if t.status in [Test.Status.Failed, Test.Status.Broken]])
-                    for name, tests in grouped_by_name.items()}
-    failed_tests_sorted = sorted([(name, count) for name, count in failed_tests.items() if count > 0],
-                                 key=lambda t: t[1], reverse=True)
-    print(f"{'Test':>5}  {'Count':>5}")
-    for name, count in failed_tests_sorted[:10]:
-        print(f"{name:>5}  {count:>5}")
-
-    num_failed_tests = sum(failed_tests.values())
-    if num_failed_tests:
-        print(f"\n::error::{num_failed_tests} unique QA test failures.")
-
-
-def send_slack_notification(slack_channel: str, github_run_url: str, qa_report_url: str, hosts: List[str],
-                            tests: List[Test]):
+def send_slack_notification(
+    slack_channel: str,
+    github_run_url: str,
+    qa_report_url: str,
+    hosts: List[str],
+    tests: List[Test],
+):
     host_stats = defaultdict(lambda: {"passed": 0, "failed": 0, "skipped": 0, "cancelled": 0})
     for test in tests:
         if test.status == Test.Status.Passed:
@@ -232,12 +252,12 @@ def send_slack_notification(slack_channel: str, github_run_url: str, qa_report_u
         if host in host_stats:
             stats = host_stats[host]
             host_stats_text = f"Passed: {stats['passed']}, Failed: {stats['failed']}, Skipped: {stats['skipped']}"
-            if stats['cancelled'] > 0:
+            if stats["cancelled"] > 0:
                 host_stats_text = f"CANCELLED ({host_stats_text})"
 
-            if stats['cancelled'] > 0 or stats['passed'] < 1000:
+            if stats["cancelled"] > 0 or stats["passed"] < 1000:
                 symbol = ":x:"
-            elif stats['failed'] > 0:
+            elif stats["failed"] > 0:
                 symbol = ":warning:"
             else:
                 symbol = ":heavy_check_mark:"
@@ -249,54 +269,63 @@ def send_slack_notification(slack_channel: str, github_run_url: str, qa_report_u
     host_blocks = []
     # slack only allows max 10 fields inside one block
     for i in range(0, len(host_texts), 10):
-        host_blocks.append({
-            "type": "section",
-            "fields": [{
-                "type": "mrkdwn",
-                "text": text
-            } for text in host_texts[i:i + 10]]
-        })
+        host_blocks.append(
+            {
+                "type": "section",
+                "fields": [{"type": "mrkdwn", "text": text} for text in host_texts[i : i + 10]],
+            }
+        )
 
     payload = {
         "channel": slack_channel,
-        "attachments": [{
-            "blocks": [{
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Daily QA:*"
-                }
-            }] + host_blocks + [{
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"*<{github_run_url}|View build logs>*\n*<{qa_report_url}|View QA report>*"
-                }
-            }]
-        }]
+        "attachments": [
+            {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {"type": "mrkdwn", "text": "*Daily QA:*"},
+                    }
+                ]
+                + host_blocks
+                + [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": f"*<{github_run_url}|View build logs>*\n*<{qa_report_url}|View QA report>*",
+                        },
+                    }
+                ]
+            }
+        ],
     }
 
     print("Sending slack notification...")
-    r = requests.post('https://slack.com/api/chat.postMessage', headers={
-        "Authorization": f"Bearer {os.environ['SLACK_BOT_TOKEN']}"
-    }, json=payload)
+    r = requests.post(
+        "https://slack.com/api/chat.postMessage",
+        headers={"Authorization": f"Bearer {os.environ['SLACK_BOT_TOKEN']}"},
+        json=payload,
+    )
     print(r.text)
     r.raise_for_status()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--qa', default='./qa')
-    parser.add_argument('--artifacts', default='./artifacts')
-    parser.add_argument('--commit', required=True)
-    parser.add_argument('--allure-results', dest='allure_results')
-    parser.add_argument('--slack-channel', dest='slack_channel')
-    parser.add_argument('--github-run-url', dest='github_run_url')
-    parser.add_argument('--qa-report-url', dest='qa_report_url')
+    parser.add_argument("--qa", default="./qa")
+    parser.add_argument("--artifacts", default="./artifacts")
+    parser.add_argument("--commit", required=True)
+    parser.add_argument("--summary", action="store_true")
+    parser.add_argument("--allure-results", dest="allure_results")
+    parser.add_argument("--slack-channel", dest="slack_channel")  # required only for slack message
+    parser.add_argument("--github-run-url", dest="github_run_url")  # required only for slack message
+    parser.add_argument("--qa-report-url", dest="qa_report_url")  # required only for slack message
     args = parser.parse_args()
 
     hosts, tests = read_hosts(args.qa, args.artifacts)
-    print_test_summary(tests)
+
+    if args.summary:
+        print_test_summary(tests)
 
     if args.allure_results:
         os.makedirs(args.allure_results, exist_ok=True)
@@ -308,5 +337,5 @@ def main():
         send_slack_notification(args.slack_channel, args.github_run_url, args.qa_report_url, hosts, tests)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
