779d6eae584640f54d129df9f336ca77a2cb66cc
Ken McDonell <kenj@kenj.id.au>
2021-07-02 13:11:32 +1000

    qa/600 & qa/src/tztest.c: finally tired of this failing twice a year
    This test historically breaks in the first week of July and January
    EVERY year.  It included this comment:
        I don't understand the sed replacement below - Ken, Jul 2003
    and 8 years on nothing has changed.
    
    So I've refactored the test program and removed the silly 1st
    half of the year vs 2nd half of the year logic, set the test
    datestamps correctly and nuked the undecipherable sed.
    
    Let's see what happens now ...

--- qa/src/tztest.c
+++ qa/src/tztest.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pcp/pmapi.h>
 #include "libpcp.h"
 
 int main()
 {
-    time_t sep = (30*365.25+244)*24*3600; /* 1 September 2000 */
-    time_t mar = (30*365.25+60)*24*3600; /* 1 March 2000 */
+    time_t	sep;
+    time_t	mar;
+    struct tm	init;
 
-    /* Two sets of timezone strings - one works for the first half
-     * of the year, another for the second. Make sure that in the case
-     * where we cannot reduce the timezone string and have to re-write it
-     * same timezone is choosen */
-    char *zones[][8] = {
-    {
+    /* Test timezone strings.
+     * Make sure that in the case where we cannot reduce the timezone
+     * string and have to re-write it the same timezone offset
+     * is choosen
+     */
+    char *zones[] = {
 	"ABC-10",
 	"ABC-10:01:02XYZ-11:03:04,M12.5.0/3:04:05,M7.1.0/2:06:07",
 	"ABC-10:00:00XYZ-11:00:00,M12.5.0/3:00:00,M7.5.0/2:00:00",
@@ -23,40 +25,50 @@ int main()
 	"ABC-10",
 	":Someplace/Somewhere",
 	NULL
-    },
-    {
-	"ABC-10",
-	"XYZ-11:03:04ABC-10:01:02,M12.5.0/3:04:05,M7.1.0/2:06:07",
-	"ABC-10:00:00XYZ-11:00:00,M12.5.0/3:00:00,M7.5.0/2:00:00",
-	"ABC-10XYZ-11,M12.5.0/3:00:00,M7.1.0/2:00:00",
-	"XYZ-11ABC-10,M12.5.0/3:01:02,M7.1.0/2:03:04",
-	"ABC-10",
-	":Someplace/Somewhere",
-	NULL
-    }};
+    };
     char * tz = getenv("TZ");
+
+    putenv("TZ=UTC");
+    /* mar == 12:00:00 1 Mar 2000 UTC */
+    memset(&init, 0, sizeof(init));
+    init.tm_hour = 12;
+    init.tm_mday = 1;
+    init.tm_mon = 2;
+    init.tm_year = 2000 - 1900;
+    mar = mktime(&init);
+
+    /* sep == 12:00:00 1 Sep 2000 UTC */
+    memset(&init, 0, sizeof(init));
+    init.tm_hour = 12;
+    init.tm_mday = 1;
+    init.tm_mon = 8;
+    init.tm_year = 2000 - 1900;
+    sep = mktime(&init);
 
     if (tz == NULL) {
 	printf("Debug: Mar time_t %ld -> %s", (long)mar, ctime(&mar));
 	printf("Debug: Sep time_t %ld -> %s", (long)sep, ctime(&sep));
 	puts("Timezone is not set, abort the test");
     } else {
-	int i;
-	time_t now = time(NULL);
-	struct tm * today = localtime(&now);
-	int which = today->tm_mon / 6;
-	char tb[256];
-	char * newtz = __pmTimezone();
+	int		i;
+	char		tb[256];
+	char		*newtz;
+
+	/* restore $TZ from environment at the start of main() */
+	pmsprintf(tb, sizeof(tb), "TZ=%s", tz);
+	putenv(tb);
+
+	newtz = __pmTimezone();
 
 	printf("%s -> %s\n", tz, newtz);
 
-	for (i=0; zones[which][i] != NULL; i++) {
+	for (i=0; zones[i] != NULL; i++) {
 	    char * tz;
 	    struct tm  *tmp;
 	    char tstr[64];
 	    int dst;
 
-	    pmsprintf(tb, sizeof(tb), "TZ=%s", zones[which][i]);
+	    pmsprintf(tb, sizeof(tb), "TZ=%s", zones[i]);
 	    putenv(tb);
 	    tzset();
 	    tz = getenv("TZ");
