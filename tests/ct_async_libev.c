eb9aa3b58ddcaa5cfe8d33dba7c735fb2837efeb
Nathan Scott <nathans@redhat.com>
2021-09-21 16:55:03 +1000

    Squashed 'vendor/github.com/Nordix/hiredis-cluster/' changes from 070097fc45..4c546b180a
    4c546b180a ci: resolve final lgtm.com recommendations
    4d8f805596 Improve memory efficiency (#64)
    e794b69eb8 Improve redisClusterReset documentation. (#62)
    439b829dbc Support AUTH with username
    043091923e Don't allocate memory for optional password when not needed
    85b6ae4679 Visualize disabled tests for missing event libraries
    c7d962a6be Add adapter and test for using the event library: GLib
    5d63ca2280 Add adapter and test for using the event library: libev
    1c068b826d Add test of the async API when using the event library: libuv
    9a4c67796a Build the build examples in CI
    3cca6bb753 Update CMakeLists.txt (#52)
    c73715d89b ci: add coverity scan
    
    git-subtree-dir: vendor/github.com/Nordix/hiredis-cluster
    git-subtree-split: 4c546b180af4fd58602e543176e76e80ca515e63

---
+++ tests/ct_async_libev.c
@@ -0,0 +1,61 @@
+#include "adapters/libev.h"
+#include "hircluster.h"
+#include "test_utils.h"
+#include <assert.h>
+
+#define CLUSTER_NODE "127.0.0.1:7000"
+
+void setCallback(redisClusterAsyncContext *acc, void *r, void *privdata) {
+    UNUSED(privdata);
+    redisReply *reply = (redisReply *)r;
+    ASSERT_MSG(reply != NULL, acc->errstr);
+}
+
+void getCallback(redisClusterAsyncContext *acc, void *r, void *privdata) {
+    UNUSED(privdata);
+    redisReply *reply = (redisReply *)r;
+    ASSERT_MSG(reply != NULL, acc->errstr);
+
+    /* Disconnect after receiving the first reply to GET */
+    redisClusterAsyncDisconnect(acc);
+}
+
+void connectCallback(const redisAsyncContext *ac, int status) {
+    ASSERT_MSG(status == REDIS_OK, ac->errstr);
+    printf("Connected to %s:%d\n", ac->c.tcp.host, ac->c.tcp.port);
+}
+
+void disconnectCallback(const redisAsyncContext *ac, int status) {
+    ASSERT_MSG(status == REDIS_OK, ac->errstr);
+    printf("Disconnected from %s:%d\n", ac->c.tcp.host, ac->c.tcp.port);
+}
+
+int main(int argc, char **argv) {
+    UNUSED(argc);
+    UNUSED(argv);
+
+    redisClusterAsyncContext *acc =
+        redisClusterAsyncConnect(CLUSTER_NODE, HIRCLUSTER_FLAG_NULL);
+    assert(acc);
+    ASSERT_MSG(acc->err == 0, acc->errstr);
+
+    int status;
+    status = redisClusterLibevAttach(acc, EV_DEFAULT);
+    assert(status == REDIS_OK);
+
+    redisClusterAsyncSetConnectCallback(acc, connectCallback);
+    redisClusterAsyncSetDisconnectCallback(acc, disconnectCallback);
+
+    status = redisClusterAsyncCommand(acc, setCallback, (char *)"ID",
+                                      "SET key value");
+    ASSERT_MSG(status == REDIS_OK, acc->errstr);
+
+    status =
+        redisClusterAsyncCommand(acc, getCallback, (char *)"ID", "GET key");
+    ASSERT_MSG(status == REDIS_OK, acc->errstr);
+
+    ev_loop(EV_DEFAULT_ 0);
+
+    redisClusterAsyncFree(acc);
+    return 0;
+}
