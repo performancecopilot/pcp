a43e439fdfb6324945ddb8763ead4d51279e56f0
Marko Myllynen <myllynen@redhat.com>
2021-12-29 11:20:52 +0200

    pcp2template: update copyright year to match latest changes

--- src/pcp2template/pcp2template.py
+++ src/pcp2template/pcp2template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env pmpython
 #
-# Copyright (C) 2015-2019 Marko Myllynen <myllynen@redhat.com>
+# Copyright (C) 2015-2021 Marko Myllynen <myllynen@redhat.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
