dfa82ef51c2366ca3508bf7b870d2b91f84c049d
Nathan Scott <nathans@redhat.com>
2022-03-16 13:32:18 +1100

    libpcp_web: simplify freeing of metric name

--- src/libpcp_web/src/query.c
+++ src/libpcp_web/src/query.c
@@ -190,8 +190,7 @@ freeSeriesGetSID(seriesGetSID *sid)
 
     seriesBatonCheckMagic(sid, MAGIC_SID, "freeSeriesGetSID");
     sdsfree(sid->name);
-    if (sid->metric)
-	sdsfree(sid->metric);
+    sdsfree(sid->metric);
     needfree = sid->freed;
     memset(sid, 0, sizeof(seriesGetSID));
     if (needfree)
