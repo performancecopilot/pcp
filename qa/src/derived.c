5e317c26855b9b4f344122c04fc1e96a80603a30
Ken McDonell <kenj@kenj.id.au>
2022-08-25 09:54:14 +1000

    qa/src/derived.c: add -m and -f flags
    To fetch metadata and fetch values.
    
    No QA test uses this yet, just scratching a curiosity and this made
    it simpler.

--- qa/src/derived.c
+++ qa/src/derived.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 Red Hat.
+ * Copyright (c) 2022 Ken McDonell.
  * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -10,16 +11,22 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
  * License for more details.
+ *
+ * Small derived metric exerciser.
  */
 #include <pcp/pmapi.h>
+#include "libpcp.h"
 
 static pmLongOptions longopts[] = {
     PMOPT_DEBUG,
     PMOPT_HELP,
+    PMAPI_OPTIONS_HEADER("derived options"),
+    { "", 0, 'f', NULL, "fetch values => -m as well" },
+    { "", 0, 'm', NULL, "fetch metadata" },
     PMAPI_OPTIONS_END
 };
 static pmOptions opts = {
-    .short_options = "D:?",
+    .short_options = "D:mf?",
     .long_options = longopts,
     .short_usage = "[options] name=expr ...",
 };
@@ -27,13 +34,39 @@ static pmOptions opts = {
 int
 main(int argc, char **argv)
 {
-    char *name, *expr;
-    char *errmsg;
+    int		c;
+    int		sts;
+    pmID	pmid;
+    pmDesc	desc;
+    char	*name, *expr;
+    char	*errmsg;
+    int		mflag = 0;
+    int		fflag = 0;
 
-    pmGetOptions(argc, argv, &opts);
+    while ((c = pmGetOptions(argc, argv, &opts)) != EOF) {
+	switch (c) {
+
+	case 'm':	/* try metadata */
+	    mflag++;
+	    break;	
+
+	case 'f':	/* try fetch */
+	    fflag++;
+	    mflag++;
+	    break;	
+	}
+    }
+
     if (opts.errors) {
 	pmUsageMessage(&opts);
 	return 1;
+    }
+
+    if (mflag || fflag) {
+	if ((sts = pmNewContext(PM_CONTEXT_HOST, "localhost")) < 0) {
+	    fprintf(stderr, "pmNewContext failed: %s\n", pmErrStr(sts));
+	    exit(1);
+	}
     }
 
     while (opts.optind < argc) {
@@ -46,9 +79,41 @@ main(int argc, char **argv)
 
 	if (pmRegisterDerivedMetric(name, expr, &errmsg) < 0) {
 	    fprintf(stderr, "%s: %s", pmGetProgname(), errmsg);
-	} else {
+	}
+	else {
 	    printf("%s: registered \"%s\" as: \"%s\"\n", pmGetProgname(), name, expr);
 	}
+
+	if (mflag || fflag) {
+	    if ((sts = pmLookupName(1, (const char **)&name, &pmid)) < 0) {
+		fprintf(stderr, "pmLookupName failed: %s\n", pmErrStr(sts));
+	    }
+	    else {
+		printf("    pmID: %s\n", pmIDStr(pmid));
+		if ((sts = pmLookupDesc(pmid, &desc)) < 0) {
+		    fprintf(stderr, "pmLookupDesc failed: %s\n", pmErrStr(sts));
+		    /*
+		     * wish there was more we could do here, but libpcp offers
+		     * no help ATM
+		     */
+		    ;
+		}
+		else {
+		    pmPrintDesc(stdout, &desc);
+		    if (fflag) {
+			pmResult	*rp;
+			if ((sts = pmFetch(1, &pmid, &rp)) < 0) {
+			    fprintf(stderr, "pmFetch failed: %s\n", pmErrStr(sts));
+			}
+			else {
+			    __pmDumpResult(stdout, rp);
+			    pmFreeResult(rp);
+			}
+		    }
+		}
+	    }
+	}
+
 	opts.optind++;
     }
 
