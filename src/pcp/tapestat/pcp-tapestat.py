95ca45c5486f3f25203f21258f2ed56c29fbd698
Nathan Scott <nathans@redhat.com>
2022-08-30 14:42:26 +1000

    docs: fix license text cut+paste failure (pcp-free? lol)

--- src/pcp/tapestat/pcp-tapestat.py
+++ src/pcp/tapestat/pcp-tapestat.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env pmpython
+#
 # Copyright (C) 2014-2016,2020 Red Hat.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
-# tapestat Software Foundation; either version 2 of the License, or (at your
+# Free Software Foundation; either version 2 of the License, or (at your
 # option) any later version.
 #
 # This program is distributed in the hope that it will be useful, but
