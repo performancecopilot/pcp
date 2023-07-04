79c4115e222cb44e72382f51d0314602d5fd7ba4
Nathan Scott <nathans@redhat.com>
2023-07-05 09:42:56 +1000

    pcp-atop: fix CodeQL static analysis of new drawbar code.

--- src/pcp/atop/drawbar.c
+++ src/pcp/atop/drawbar.c
@@ -563,10 +563,10 @@ do_cpubars(struct sstat *sstat, int nsecs, char initlabels, char mono)
 		labellen = pmsprintf(buf, sizeof buf, "%d", numcpus);
 
 		vertvals = calloc(numlabs, sizeof(struct vertval));
-		ptrverify(vertvals, "Malloc for %ld vertval structs failed\n", numlabs);
+		ptrverify(vertvals, "Malloc failed for %d vertval structs\n", numlabs);
 
 		labarea = calloc(numlabs, (labellen+1));
-		ptrverify(labarea, "Malloc for %ld CPU labels failed\n", numlabs);
+		ptrverify(labarea, "Malloc failed for %d CPU labels\n", numlabs);
 
 		// create new X axis labels
 		//
@@ -746,10 +746,10 @@ do_dskbars(struct sstat *sstat, int nsecs, char initlabels, char mono)
 		numdisks = sstat->dsk.ndsk;
 
 		vertvals = calloc(numdisks, sizeof(struct vertval));
-		ptrverify(vertvals, "Malloc for %ld vertval structs failed\n", numdisks);
+		ptrverify(vertvals, "Malloc failed for %d vertval structs\n", numdisks);
 
 		labarea = calloc(numdisks, (MAXLABLEN+1));
-		ptrverify(labarea, "Malloc for %ld disk labels failed\n", numdisks);
+		ptrverify(labarea, "Malloc failed for %d disk labels\n", numdisks);
 
 		// create new X axis labels
 		//
@@ -943,10 +943,10 @@ addifscale(char *interface, int scale)
 		hash += *p;
 
 	isp = malloc(sizeof(struct ifscale));
-	ptrverify(isp, "Malloc failed for ifscale struct failed\n");
+	ptrverify(isp, "Malloc failed for ifscale struct\n");
 
 	isp->interface 	= malloc(len+1);
-	ptrverify(isp->interface, "Malloc failed for ifscale name failed\n");
+	ptrverify(isp->interface, "Malloc failed for ifscale name\n");
 	strcpy(isp->interface, interface);
 
 	isp->next 	= ishash[hash&(ISNUM-1)];
