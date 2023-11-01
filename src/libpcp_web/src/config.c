performancecopilot/pcp

commit 732bd5361a2cf86cebdef19c7bcfa357212945b3

libpcp_web: implement config override by canonical env vars
Extending utility of PCP_* environment variables to be generally
usable to override settings detected via libpcp_web config.c

Introduces canonical form of config options as:
    "PCP_<SECTION|GROUP>_<OPTION|KEY>"
where dots '.' are represented as underscores '_'.

Example usage:
    $ export PCP_PMSERIERS_STREAM_MAXLEN=424242
    $ pmseries --load <archive> -Dlibweb
    ...
    pmIniFileParse set pmseries.stream.maxlen = 424242

Primary motivation was to create an easy and convenient way to
to adjust configuration inside the archive-analysis container.

Signed-off-by: Vratislav Bendel <vbendel@redhat.com>

src/libpcp_web/src/config.c
@@ -15,6 +15,7 @@
 #include "libpcp.h"
 #include "util.h"
 #include <inih/ini.h>
+#include <ctype.h>
 
 int
 pmIniFileParse(const char *progname, ini_handler handler, void *data)
@@ -53,12 +54,62 @@ pmIniFileParse(const char *progname, ini_handler handler, void *data)
     return 0;
 }
 
+char *
+construct_canonic_envvar_name(const char *group, const char *key)
+{
+    int i;
+    size_t section_length = strlen(group);
+    size_t option_length = strlen(key);
+    char section[section_length];
+    char option[option_length];
+    char *tmp;
+
+    /* Canonical form: "PCP_<SECTION>_<OPTION>\0" */
+    int envvar_name_len = 4 + section_length + 1 + option_length + 1;
+    char *envvar_name = malloc((size_t)envvar_name_len*sizeof(char));
+    if (!envvar_name)
+	return NULL;
+
+    for (i = 0; i < section_length; i++)
+	section[i] = toupper(group[i]);
+
+    for (i = 0; i < option_length; i++)
+	if (key[i] == '.')
+		option[i] = '_';
+	else
+		option[i] = toupper(key[i]);
+
+    tmp = memcpy(envvar_name, "PCP_", 4) + 4;
+    tmp = memcpy(tmp, section, section_length) + section_length;
+    tmp = memcpy(tmp, "_", 1) + 1;
+    tmp = memcpy(tmp, option, option_length);
+    envvar_name[envvar_name_len - 1] = '\0';
+
+    return envvar_name;
+}
+
+const char *
+check_envvar_override(const char *group, const char *key, const char *value)
+{
+    char *tmp_value;
+    char *envvar_name = construct_canonic_envvar_name(group, key);
+    if (!envvar_name)
+	return value;
+
+    if ((tmp_value = getenv(envvar_name)) != NULL)
+	value = tmp_value;
+
+    free(envvar_name);
+    return value;
+}
+
 static int
 dict_handler(void *arg, const char *group, const char *key, const char *value)
 {
     dict	*config = (dict *)arg;
     sds		name = sdsempty();
 
+    value = check_envvar_override(group, key, value);
     name = sdscatfmt(name, "%s.%s", group ? group : pmGetProgname(), key);
     if (pmDebugOptions.libweb)
 	fprintf(stderr, "pmIniFileParse set %s = %s\n", name, value);

commit 0f6038ca05e8b8fee6cba465b10bc2cd6176b687

libpcp_web: fix envvar override bugs
Fix warnings detected by Coverity scan in commit 732bd5361
"libpcp_web: implement config override by canonical env vars".

Details in:
https://github.com/performancecopilot/pcp/pull/1822#issuecomment-1786379344

Fixes: 732bd5361

Signed-off-by: Vratislav Bendel <vbendel@redhat.com>

src/libpcp_web/src/config.c
@@ -60,8 +60,8 @@ construct_canonic_envvar_name(const char *group, const char *key)
     int i;
     size_t section_length = strlen(group);
     size_t option_length = strlen(key);
-    char section[section_length];
-    char option[option_length];
+    char section[section_length+1];
+    char option[option_length+1];
     char *tmp;
 
     /* Canonical form: "PCP_<SECTION>_<OPTION>\0" */
@@ -72,12 +72,14 @@ construct_canonic_envvar_name(const char *group, const char *key)
 
     for (i = 0; i < section_length; i++)
 	section[i] = toupper(group[i]);
+    section[section_length] = '\0';
 
     for (i = 0; i < option_length; i++)
 	if (key[i] == '.')
 		option[i] = '_';
 	else
 		option[i] = toupper(key[i]);
+    option[option_length] = '\0';
 
     tmp = memcpy(envvar_name, "PCP_", 4) + 4;
     tmp = memcpy(tmp, section, section_length) + section_length;
@@ -109,8 +111,11 @@ dict_handler(void *arg, const char *group, const char *key, const char *value)
     dict	*config = (dict *)arg;
     sds		name = sdsempty();
 
+    if (!group)
+	group = pmGetProgname();
+
     value = check_envvar_override(group, key, value);
-    name = sdscatfmt(name, "%s.%s", group ? group : pmGetProgname(), key);
+    name = sdscatfmt(name, "%s.%s", group, key);
     if (pmDebugOptions.libweb)
 	fprintf(stderr, "pmIniFileParse set %s = %s\n", name, value);
     return dictReplace(config, name, sdsnew(value)) != DICT_OK;