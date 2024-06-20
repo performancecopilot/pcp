5f33d4f214ef6b277a09477187b2ee450c9a2598
Ken McDonell <kenj@kenj.id.au>
2024-06-21 06:58:03 +1000

    libpcp: add -Dqed for libpcp_qed diagnostics

--- src/include/pcp/pmapi.h
+++ src/include/pcp/pmapi.h
@@ -1266,6 +1266,7 @@ typedef struct {
     int	appl9;		/* Application-specific flag 9 */
     int	tls;		/* Transport Layer Security operations */
     int	misc;		/* Miscellaneous odds and sods */
+    int	qed;		/* Methods in libpcp_qed */
 } pmdebugoptions_t;
 
 PCP_DATA extern pmdebugoptions_t	pmDebugOptions;
