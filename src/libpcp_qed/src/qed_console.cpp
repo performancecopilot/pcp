92d4a865bc767ece8904514b51e0b9c522ae3fbe
Ken McDonell <kenj@kenj.id.au>
2024-06-24 16:32:03 +1000

    libpcp_qed: changes to help pmview
    - QedApp::globalFontSize() make 9pt for all platorms (was 7pt for non-Darwin)
      platforms which is too small
    
    - in the QedConsole class, rework debug flag mappings
      - -Dqed (newish) => QedApp::DebugApp and QedApp::DebugUi and
        QedApp::DebugView and QedApp::DebugTimeless, but no change for
        QedConsole::post where QedApp::DebugApp (and hence -Dqed)  is still
        the flag that enables diagnostics here
      - -Dtimecontrol => QedApp::DebugProtocol
      - -Ddesperate => QedApp::DebugForce
    
    - in the QedGroupControl class add calls to updateTimeButton() in
      all the places where we call newButtonState() ... this sets/updates
      the state icon in pmviews's statusBar
    
    - add more verbose debugging with -Ddesperate of the QmcTime packet
      received in QedGroupControl::step()
    
    - in QedTimeButton::setButtonState add -Dqed diagnostic

--- src/libpcp_qed/src/qed_console.cpp
+++ src/libpcp_qed/src/qed_console.cpp
@@ -21,15 +21,17 @@ QedConsole *console;
 QedConsole::QedConsole(struct timeval origin) : QDialog()
 {
     my.level = 0;
-    if (pmDebugOptions.appl0) {
+    if (pmDebugOptions.qed) {
 	my.level |= QedApp::DebugApp;		// general and UI tracing
 	my.level |= QedApp::DebugUi;
-    }
-    if (pmDebugOptions.appl1)
-	my.level |= QedApp::DebugProtocol;	// trace time protocol
-    if (pmDebugOptions.appl2) {
 	my.level |= QedApp::DebugView;		// config files, for QA
 	my.level |= QedApp::DebugTimeless;
+    }
+    if (pmDebugOptions.timecontrol) {
+	my.level |= QedApp::DebugProtocol;	// trace time protocol
+    }
+    if (pmDebugOptions.desperate) {
+	my.level |= QedApp::DebugForce;		// and the kitchen sink
     }
     setupUi(this);
 
