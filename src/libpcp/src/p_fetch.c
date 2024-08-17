17689e942499c4f34c86f37a9ad69a634214959d
Ken McDonell <kenj@kenj.id.au>
2024-08-17 15:40:19 +1000

    src/libpcp/src/p_fetch.c: __pmDecodeFetch: reworked integrity checks and warning messages
    Also remade qa/1538.out to match.

--- src/libpcp/src/p_fetch.c
+++ src/libpcp/src/p_fetch.c
@@ -82,7 +82,7 @@ __pmDecodeFetchPDU(__pmPDU *pdubuf, int *ctxidp, int *numpmidp, pmID **pmidlist)
 
     if (pduend - (char*)pp < sizeof(fetch_t)) {
 	if (pmDebugOptions.pdu) {
-	    fprintf(stderr, "__pmDecodeFetchReq: PM_ERR_IPC: PDU remainder %d < sizeof(fetch_t) %d\n",
+	    fprintf(stderr, "__pmDecodeFetchPDU: PM_ERR_IPC: short PDU %d < min size %d\n",
 		(int)(pduend - (char*)pp), (int)sizeof(fetch_t));
 	}
 	return PM_ERR_IPC;
@@ -90,7 +90,7 @@ __pmDecodeFetchPDU(__pmPDU *pdubuf, int *ctxidp, int *numpmidp, pmID **pmidlist)
     numpmid = ntohl(pp->numpmid);
     if (numpmid <= 0) {
 	if (pmDebugOptions.pdu) {
-	    fprintf(stderr, "__pmDecodeFetchReq: PM_ERR_IPC: numpmid %d <= 0\n",
+	    fprintf(stderr, "__pmDecodeFetchPDU: PM_ERR_IPC: numpmid %d <= 0\n",
 		numpmid);
 	}
 	return PM_ERR_IPC;
@@ -98,10 +98,10 @@ __pmDecodeFetchPDU(__pmPDU *pdubuf, int *ctxidp, int *numpmidp, pmID **pmidlist)
     if ((pduend - (char *)pp) != sizeof(fetch_t) + ((sizeof(pmID)) * (numpmid-1))) {
 	if (pmDebugOptions.pdu) {
 	    if ((pduend - (char *)pp) > sizeof(fetch_t) + ((sizeof(pmID)) * (numpmid-1))) 
-		fprintf(stderr, "__pmDecodeFetchReq: PM_ERR_IPC: PDU too long %d > sizeof(fetch_t) %d + sizeof(pmID) %d * (numpmid-1) %d\n",
+		fprintf(stderr, "__pmDecodeFetchPDU: PM_ERR_IPC: long PDU %d > sizeof(fetch_t) %d + sizeof(pmID) %d * (numpmid-1) %d\n",
 		    (int)(pduend - (char*)pp), (int)sizeof(fetch_t), (int)sizeof(pmID), numpmid-1);
 	    else
-		fprintf(stderr, "__pmDecodeFetchReq: PM_ERR_IPC: PDU too short %d < sizeof(fetch_t) %d + sizeof(pmID) %d * (numpmid-1) %d\n",
+		fprintf(stderr, "__pmDecodeFetchPDU: PM_ERR_IPC: short PDU %d < sizeof(fetch_t) %d + sizeof(pmID) %d * (numpmid-1) %d\n",
 		    (int)(pduend - (char*)pp), (int)sizeof(fetch_t), (int)sizeof(pmID), numpmid-1);
 	}
 	return PM_ERR_IPC;
