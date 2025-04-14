2f38d10fd688d8491f3b4251f6fa4318fc8384f1
Nathan Scott <nathans@redhat.com>
2025-04-14 17:53:08 +1000

    build: add pylint checking to archive-import script

--- build/containers/archive-analysis/root/usr/bin/archive-import.py
+++ build/containers/archive-analysis/root/usr/bin/archive-import.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pmpython
+#
+# Copyright (C) 2025 Marko Myllynen <myllynen@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+# pylint: disable=global-statement
+
 import logging
 import time
 import argparse
