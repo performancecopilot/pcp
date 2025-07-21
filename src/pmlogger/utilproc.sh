352bbb0096e3ab32d00a8c61372465f6d67ede6c
Ken McDonell <kenj@kenj.id.au>
2025-07-21 14:59:22 +1000

    src/pmlogger/utilproc.sh: when "duplicate dirs" found, provide more info
    We're doing the check for duplicate directories across all control
    files in a more consistent manner now and this is tripping up some QA
    tests (qa/340 in particular), so add some additional diagnostics to
    identify the control file name and line number where the "duplicate"
    directories are found.

--- src/pmlogger/utilproc.sh
+++ src/pmlogger/utilproc.sh
@@ -492,13 +492,16 @@ s/^\([A-Za-z][A-Za-z0-9_]*\)=/export \1; \1=/p
 	then
 	    # check for directory duplicate entries
 	    #
-	    if [ "`grep "$dir" $tmp/_dir`" = "$dir" ]
+	    grep "^$dir " <$tmp/_dir >$tmp/_tmp
+	    if [ -s $tmp/_tmp ]
 	    then
 		_error "Duplicate pmlogger instances for archive directory \"$dir\""
+		echo "Prior instance(s) for this direcrtory:"
+		cat $tmp/_tmp
 		continue
 	    fi
 	fi
-	echo "$dir" >>$tmp/_dir
+	echo "$dir $filename:$line" >>$tmp/_dir
 
 	# make sure output directory hierarchy exists and $PCP_USER
 	# user can write there
