8f7c3b8d5aa2c9cc9fc5b68f53db6ce42757bf1d
Nathan Scott <nathans@redhat.com>
2025-08-28 11:06:35 +1000

    qa: rework fetchgroup test program to work with -DNDEBUG

--- qa/src/fetchgroup.c
+++ qa/src/fetchgroup.c
@@ -14,7 +14,6 @@
  */
 
 #include <pcp/pmapi.h>
-#include <assert.h>
 
 void
 __pcp_assert(int sts, const char *FILE, int LINE)
@@ -22,10 +21,21 @@ __pcp_assert(int sts, const char *FILE, int LINE)
     if (sts < 0) {
 	fprintf(stderr, "ERROR %s:%d: sts %d (%s)\n",
 		FILE, LINE, sts, pmErrStr(sts));
-	exit (1);
+	exit(1);
     }
 }
 #define pcp_assert(s) __pcp_assert((s),__FILE__,__LINE__)
+
+void
+__fg_assert(int condition, const char *cause, const char *FILE, int LINE)
+{
+    if (!condition) {
+	fprintf(stderr, "ERROR %s:%d: assert failed: %s\n",
+		FILE, LINE, cause);
+	exit(1);
+    }
+}
+#define fg_assert(c) __fg_assert((c), #c, __FILE__,__LINE__)
 
 void
 test_indoms(void)
@@ -42,7 +52,7 @@ test_indoms(void)
 
     sts = pmCreateFetchGroup(&fg, PM_CONTEXT_HOST, "local:");
     pcp_assert(sts);
-    assert(fg != NULL);
+    fg_assert(fg != NULL);
 
     sts = pmExtendFetchGroup_indom(fg, "sample.bogus_bin", "rate",
 				   values_inst_codes, values_inst_names,
@@ -57,19 +67,19 @@ test_indoms(void)
 	sts = pmFetchGroup(fg);
 	pcp_assert(sts);
 
-	assert(values_sts == PM_ERR_TOOBIG); /* 5 < 9 */
+	fg_assert(values_sts == PM_ERR_TOOBIG); /* 5 < 9 */
 
 	for (j = 0; j < almost_bins; j++) {
-	    assert((i==0) || (values_stss[j] == 0));
+	    fg_assert((i==0) || (values_stss[j] == 0));
 	    if (i > 0)
-		assert(values[j].l == 0);
-	    assert(values_inst_codes[j] >= 0);
+		fg_assert(values[j].l == 0);
+	    fg_assert(values_inst_codes[j] >= 0);
 	    /* validate bogus indom names */
 	    if (values_inst_codes[j] % 100)
-		assert(values_inst_names[j] == NULL);
+		fg_assert(values_inst_names[j] == NULL);
 	    else
 		/* bin-XXX */
-		assert(atoi(values_inst_names[j]+4) == values_inst_codes[j]);
+		fg_assert(atoi(values_inst_names[j]+4) == values_inst_codes[j]);
 	}
     }
 
@@ -94,7 +104,7 @@ test_counter(void)
 
     sts = pmCreateFetchGroup(&fg, PM_CONTEXT_HOST, "local:");
     pcp_assert(sts);
-    assert(fg != NULL);
+    fg_assert(fg != NULL);
 
     sts = pmExtendFetchGroup_item(fg, "sample.rapid", NULL, NULL,
 				  &rapid_counter, PM_TYPE_FLOAT,
@@ -122,26 +132,26 @@ test_counter(void)
 	sts = pmFetchGroup(fg);
 	pcp_assert(sts);
 
-	assert(rapid_counter2_sts == 0);
-	assert(constant_rate_counter_count == 1); /* only one instance */
+	fg_assert(rapid_counter2_sts == 0);
+	fg_assert(constant_rate_counter_count == 1); /* only one instance */
 	if (i <= 0 && rapid_counter_sts >= 0)
 	    fprintf(stderr, "botch 1: i=%d rapid_counter_sts=%d\n", i, rapid_counter_sts);
-	assert((i > 0) || (rapid_counter_sts < 0));
+	fg_assert((i > 0) || (rapid_counter_sts < 0));
 	if (i <= 0 && constant_rate_counter_stss[0] >= 0)
 	    fprintf(stderr, "botch 2: i=%d constant_rate_counter_stss[0]=%d\n", i, constant_rate_counter_stss[0]);
-	assert((i > 0) || (constant_rate_counter_stss[0] < 0));
+	fg_assert((i > 0) || (constant_rate_counter_stss[0] < 0));
 	if (constant_rate_counter_stss[1] >= 0)
 	    fprintf(stderr, "botch 3: constant_rate_counter_stss[1]=%d\n", constant_rate_counter_stss[1]);
-	assert((constant_rate_counter_stss[1] < 0));
+	fg_assert((constant_rate_counter_stss[1] < 0));
 
 	/* ticking at 10Hz, expect 864000-ish "count/24 hours" */
 	if (i > 0) {
-	    assert(constant_rate_counter_values[0].ll >  800000 &&
+	    fg_assert(constant_rate_counter_values[0].ll >  800000 &&
 			   constant_rate_counter_values[0].ll < 1000000);
 	}
 	/* doubles can exactly represent full 32-bit uint range */
-	assert(rapid_counter2.ul == rapid_counter3.d);
-	/* assert(rapid_counter.f > 0.0); <- wraparound can make rate < 0.0 */
+	fg_assert(rapid_counter2.ul == rapid_counter3.d);
+	/* fg_assert(rapid_counter.f > 0.0); <- wraparound can make rate < 0.0 */
 	sleep(3);
     }
 
@@ -176,7 +186,7 @@ test_events(const char *event_metric_name)
 
     sts = pmCreateFetchGroup(&fg, PM_CONTEXT_HOST, "local:");
     pcp_assert(sts);
-    assert(fg != NULL);
+    fg_assert(fg != NULL);
 
     sts = pmExtendFetchGroup_event(fg, event_metric_name,
 				   "fungus", "sample.event.type", NULL,
@@ -199,7 +209,7 @@ test_events(const char *event_metric_name)
 	sts = pmFetchGroup(fg);
 	pcp_assert(sts);
 
-	assert(values_sts != PM_ERR_TOOBIG);
+	fg_assert(values_sts != PM_ERR_TOOBIG);
 	if (values2_sts == PM_ERR_TOOBIG) /* sometimes too many param_strings */
 	    too_bigs++;
 
@@ -207,31 +217,31 @@ test_events(const char *event_metric_name)
 	    int k;
 
 	    pcp_assert(values_stss[j]);
-	    assert(values[j].cp != NULL);
+	    fg_assert(values[j].cp != NULL);
 	    k = atoi(values[j].cp);
-	    assert(0 <= k && k <= 16); /* sample.event.type value range */
-	    assert(times[j].tv_sec > 0);
+	    fg_assert(0 <= k && k <= 16); /* sample.event.type value range */
+	    fg_assert(times[j].tv_sec > 0);
 	    if (j > 0) /* assert time ordering */
-		assert(times[j-1].tv_sec < times[j].tv_sec ||
+		fg_assert(times[j-1].tv_sec < times[j].tv_sec ||
 			(times[j-1].tv_sec == times[j].tv_sec &&
 			 times[j-1].tv_nsec <= times[j].tv_nsec));
 	}
 
 	for (j = 0; j < values2_num; j++) {
 	    pcp_assert(values2_stss[j]);
-	    assert(values2[j].cp != NULL);
+	    fg_assert(values2[j].cp != NULL);
 	    /* the sole literals; not from [bogus] */
-	    assert(strcmp(values2[j].cp, "6") == 0 ||
+	    fg_assert(strcmp(values2[j].cp, "6") == 0 ||
 		    strcmp(values2[j].cp, "twelve") == 0 ||
 		    strcmp(values2[j].cp, "thirteen") == 0);
-	    assert(times2[j].tv_sec > 0);
-	    if (j > 0) /* assert time ordering */
-		assert(times2[j-1].tv_sec < times2[j].tv_sec ||
+	    fg_assert(times2[j].tv_sec > 0);
+	    if (j > 0) /* check time ordering */
+		fg_assert(times2[j-1].tv_sec < times2[j].tv_sec ||
 			(times2[j-1].tv_sec == times2[j].tv_sec &&
 			 times2[j-1].tv_nsec <= times2[j].tv_nsec));
 	}
     }
-    assert(too_bigs > 0);
+    fg_assert(too_bigs > 0);
 
     sts = pmDestroyFetchGroup(fg);
     pcp_assert(sts);
