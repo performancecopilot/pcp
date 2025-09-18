b45892cd72d6c4d673d2e64cc26fb080dfba0ad6
Nathan Scott <nathans@redhat.com>
2025-09-18 15:25:59 +1000

    docs: add help text for metrics generated by guidellm2pcp

--- src/guidellm2pcp/guidellm2pcp.py
+++ src/guidellm2pcp/guidellm2pcp.py
@@ -21,9 +21,10 @@ import sys
 
 from datetime import datetime, timezone
 from pcp import pmapi, pmi
-from cpmapi import (PM_COUNT_ONE, PM_LABEL_CONTEXT, PM_TIME_SEC,
-                    PM_TIME_MSEC, PM_TYPE_DOUBLE, PM_TYPE_U64,
-                    PM_SEM_DISCRETE, PM_TEXT_PMID, PM_TEXT_ONELINE)
+from cpmapi import (PM_COUNT_ONE, PM_LABEL_CONTEXT,
+                    PM_TIME_SEC, PM_TIME_MSEC,
+                    PM_TYPE_DOUBLE, PM_TYPE_U64, PM_SEM_DISCRETE,
+                    PM_TEXT_PMID, PM_TEXT_ONELINE, PM_TEXT_HELP)
 
 def register_metrics(pcp) -> pmapi.pmInDom:
     domain = 510 # PCP domain number (guidellm import)
@@ -37,22 +38,30 @@ def register_metrics(pcp) -> pmapi.pmInDom:
     msecunits = pcp.pmiUnits(0, 1, 0, 0, PM_TIME_MSEC, 0)
     psecunits = pcp.pmiUnits(0, 1, 1, 0, PM_TIME_SEC, PM_COUNT_ONE)
 
-    def add_metric(name, datatype, units, text=None):
+    def add_metric(name, datatype, units, oneline=None, text=None) -> None:
         nonlocal item
         pmid = pcp.pmiID(domain, cluster, item)
         pcp.pmiAddMetric(name, pmid, datatype,
                      indom, PM_SEM_DISCRETE, units)
+        if oneline:
+            pcp.pmiPutText(PM_TEXT_PMID, PM_TEXT_ONELINE, pmid, oneline)
         if text:
-            pcp.pmiPutText(PM_TEXT_PMID, PM_TEXT_ONELINE, pmid, text)
+            pcp.pmiPutText(PM_TEXT_PMID, PM_TEXT_HELP, pmid, text)
         item += 1
 
     pmns = 'guidellm.'
-    add_metric(pmns + 'duration', PM_TYPE_DOUBLE, secunits)
+    add_metric(pmns + 'duration', PM_TYPE_DOUBLE, secunits,
+               'Elapsed time in seconds for each GuideLLM run completed')
     pmns = 'guidellm.run_stats.requests_made.'
-    add_metric(pmns + 'successful', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'errored', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'incomplete', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'total', PM_TYPE_U64, unitone)
+    add_metric(pmns + 'successful', PM_TYPE_U64, unitone,
+               'Count of requests processed (requests-per-second as a rate)')
+    add_metric(pmns + 'errored', PM_TYPE_U64, unitone,
+               'Count of failed requests, e.g. invalid input or server issues')
+    add_metric(pmns + 'incomplete', PM_TYPE_U64, unitone,
+               'Count of incomplete requests, e.g. timeouts or interruptions')
+    add_metric(pmns + 'total', PM_TYPE_U64, unitone,
+               'Count of all requests started irrespective of how they finish')
+
     pmns = 'guidellm.run_stats.'
     add_metric(pmns + 'queued_time_avg', PM_TYPE_DOUBLE, secunits)
     add_metric(pmns + 'scheduled_time_delay_avg', PM_TYPE_DOUBLE, secunits)
@@ -64,69 +73,162 @@ def register_metrics(pcp) -> pmapi.pmInDom:
     add_metric(pmns + 'request_start_time_targeted_delay_avg', PM_TYPE_DOUBLE, secunits)
     add_metric(pmns + 'request_time_delay_avg', PM_TYPE_DOUBLE, secunits)
     add_metric(pmns + 'request_time_avg', PM_TYPE_DOUBLE, secunits)
+
     pmns = 'guidellm.requests_per_second.total.'
-    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'median', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'count', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'min', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'max', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit)
-    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit)
+    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, psecunits,
+        'Average number of requests processed per second',
+        'Indicates throughput and the systems ability to handle concurrent workloads.')
+    add_metric(pmns + 'median', PM_TYPE_DOUBLE, psecunits,
+        'Median number of requests processed per second',
+        'Indicates throughput and the systems ability to handle concurrent workloads.')
+    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, psecunits,
+        'Most frequently occurring number of requests processed per second',
+        'Indicates throughput and the systems ability to handle concurrent workloads.')
+    add_metric(pmns + 'count', PM_TYPE_U64, unitone,
+        'Total number of requests per second data points')
+    add_metric(pmns + 'min', PM_TYPE_DOUBLE, psecunits,
+        'Minimum requests per second')
+    add_metric(pmns + 'max', PM_TYPE_DOUBLE, psecunits,
+        'Maximum requests per second')
+    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit,
+        'Standard deviation of requests per second')
+    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit,
+        'Variance within requests per second')
+
     pmns = 'guidellm.request_latency.total.'
-    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'count', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit)
-    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit)
+    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits,
+        'Average time taken to process requests',
+        'A critical metric for evaluating the responsiveness of the system.')
+    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits,
+        'Median time taken to process requests',
+        'A critical metric for evaluating the responsiveness of the system.')
+    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits,
+        'Most frequently occurring time taken to process requests',
+        'A critical metric for evaluating the responsiveness of the system.')
+    add_metric(pmns + 'count', PM_TYPE_U64, unitone,
+        'Total number of time taken to process request data points')
+    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits,
+        'Minimum time taken to process a single request')
+    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits,
+        'Maximum time taken to process a single request')
+    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit,
+        'Standard deviation of time taken to process requests')
+    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit,
+        'Variance within time taken to process requests')
+
     pmns = 'guidellm.time_to_first_token_ms.total.'
-    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'count', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit)
-    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit)
+    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits,
+        'Average time taken to generate the first token of the output',
+        'Indicates initial response time of the model, crucial for user-\n' +
+        'facing applications.')
+    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits,
+        'Median time taken to generate the first token of the output',
+        'Indicates initial response time of the model, crucial for user-\n' +
+        'facing applications.')
+    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits,
+        'Most frequently occurring time taken to generate first output token',
+        'Indicates initial response time of the model, crucial for user-\n' +
+        'facing applications.')
+    add_metric(pmns + 'count', PM_TYPE_U64, unitone,
+        'Total number of time taken to generate first output token measures')
+    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits,
+        'Minimum time taken to generate first output token')
+    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits,
+        'Maximum time taken to generate first output token')
+    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit,
+        'Standard deviation of time taken to generate first output token')
+    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit,
+        'Variance within time taken to generate first output token')
+
     pmns = 'guidellm.time_per_output_token_ms.total.'
-    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'count', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit)
-    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit)
+    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits,
+        'Average time taken to generate each output token',
+        'Provides a detailed view of the models token generation efficiency.')
+    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits,
+        'Median time taken to generate each output token',
+        'Provides a detailed view of the models token generation efficiency.')
+    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits,
+        'Most frequently occurring time taken to generate each output token',
+        'Provides a detailed view of the models token generation efficiency.')
+    add_metric(pmns + 'count', PM_TYPE_U64, unitone,
+        'Count of time taken to generate each output token measures')
+    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits,
+        'Minimum time taken to generate each output token')
+    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits,
+        'Maximum time taken to generate each output token')
+    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit,
+        'Standard deviation of time taken to generate each output token')
+    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit,
+        'Variance within time taken to generate each output token')
+
     pmns = 'guidellm.inter_token_latency_ms.total.'
-    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'count', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits)
-    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit)
-    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit)
+    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, msecunits,
+        'Average time between generating consecutive tokens in the output',
+        'Helps assess the smoothness and speed of token generation.')
+    add_metric(pmns + 'median', PM_TYPE_DOUBLE, msecunits,
+        'Median time between generating consecutive tokens in the output',
+        'Helps assess the smoothness and speed of token generation.')
+    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, msecunits,
+        'Most frequently occurring time between consecutive output tokens',
+        'Helps assess the smoothness and speed of token generation.')
+    add_metric(pmns + 'count', PM_TYPE_U64, unitone,
+        'Count of time taken between consecutive output tokens measures')
+    add_metric(pmns + 'min', PM_TYPE_DOUBLE, msecunits,
+        'Minimum time between generating consecutive tokens in the output')
+    add_metric(pmns + 'max', PM_TYPE_DOUBLE, msecunits,
+        'Maximum time between generating consecutive tokens in the output')
+    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit,
+        'Standard deviation of time between consecutive output tokens')
+    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit,
+        'Variance within time between consecutive output tokens')
+
     pmns = 'guidellm.output_tokens_per_second.total.'
-    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'median', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'count', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'min', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'max', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit)
-    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit)
+    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, psecunits,
+        'Average output tokens per second across all requests',
+        'Provides insights into the servers performance and efficiency\n' +
+        'in generating output tokens.')
+    add_metric(pmns + 'median', PM_TYPE_DOUBLE, psecunits,
+        'Median prompt and output tokens per second across all requests',
+        'Provides insights into the servers performance and efficiency\n' +
+        'in generating output tokens.')
+    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, psecunits,
+        'Mode of prompt and output tokens per second across all requests',
+        'Provides insights into the servers performance and efficiency\n' +
+        'in generating output tokens.')
+    add_metric(pmns + 'count', PM_TYPE_U64, unitone,
+        'Count of output tokens per second measures')
+    add_metric(pmns + 'min', PM_TYPE_DOUBLE, psecunits,
+        'Minimum output tokens per second')
+    add_metric(pmns + 'max', PM_TYPE_DOUBLE, psecunits,
+        'Maximum output tokens per second')
+    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit,
+        'Standard deviation of output tokens per second')
+    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit,
+        'Variance within output tokens per second')
+
     pmns = 'guidellm.tokens_per_second.total.'
-    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'median', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'count', PM_TYPE_U64, unitone)
-    add_metric(pmns + 'min', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'max', PM_TYPE_DOUBLE, psecunits)
-    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit)
-    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit)
+    add_metric(pmns + 'mean', PM_TYPE_DOUBLE, psecunits,
+        'Average prompt and output tokens per second across all requests',
+        'Provides insights into the overall performance and efficiency\n' +
+        'in processing both prompt and output tokens.')
+    add_metric(pmns + 'median', PM_TYPE_DOUBLE, psecunits,
+        'Median prompt and output tokens per second across all requests',
+        'Provides insights into the overall performance and efficiency\n' +
+        'in processing both prompt and output tokens.')
+    add_metric(pmns + 'mode', PM_TYPE_DOUBLE, psecunits,
+        'Mode of prompt and output tokens per second across all requests',
+        'Provides insights into the overall performance and efficiency\n' +
+        'in processing both prompt and output tokens.')
+    add_metric(pmns + 'count', PM_TYPE_U64, unitone,
+        'Count of prompt and output tokens per second measures')
+    add_metric(pmns + 'min', PM_TYPE_DOUBLE, psecunits,
+        'Minimum prompt and output tokens per second')
+    add_metric(pmns + 'max', PM_TYPE_DOUBLE, psecunits,
+        'Maximum prompt and output tokens per second')
+    add_metric(pmns + 'std_dev', PM_TYPE_DOUBLE, nounit,
+        'Standard deviation of prompt and output tokens per second')
+    add_metric(pmns + 'variance', PM_TYPE_DOUBLE, nounit,
+        'Variance within prompt and output tokens per second')
 
     return indom
 
