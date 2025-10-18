0155621e3c4709e891cddf04fe41514511a394e7
Jan Kurik <jkurik@redhat.com>
2025-10-18 10:37:55 +0200

    Added checks for NULL pointer when context is released
    Resolves: bz2404769

--- src/libpcp/src/logutil.c
+++ src/libpcp/src/logutil.c
@@ -574,10 +574,13 @@ logFreeHashLabels(__pmHashCtl *type_ctl)
 		    for (label = (__pmLogLabelSet *)ident_node->data; label != NULL; ) {
 			for (k = 0; k < label->nsets; k++) {
 			    labelset = &label->labelsets[k];
-			    free(labelset->json);
-			    free(labelset->labels);
+			    if (labelset->json != NULL)
+				free(labelset->json);
+			    if (labelset->labels != NULL)
+				free(labelset->labels);
 			}
-			free(label->labelsets);
+			if (label->labelsets != NULL)
+			    free(label->labelsets);
 			curr_label = label;
 			label = label->next;
 			free(curr_label);
@@ -590,12 +593,14 @@ logFreeHashLabels(__pmHashCtl *type_ctl)
 
 	    curr_type_node = type_node;
 	    type_node = type_node->next;
-	    free(ident_ctl->hash);
+	    if (ident_ctl->hash != NULL)
+		free(ident_ctl->hash);
 	    free(ident_ctl);
 	    free(curr_type_node);
 	}
     }
-    free(type_ctl->hash);
+    if (type_ctl->hash != NULL)
+	free(type_ctl->hash);
 }
 
 static void
@@ -623,18 +628,21 @@ logFreeHashText(__pmHashCtl *type_ctl)
 		    curr_ident_node = ident_node;
 		    ident_node = ident_node->next;
 		    free(curr_ident_node);
-		    free(text);
+		    if (text != NULL)
+			free(text);
 		}
 	    }
 
 	    curr_type_node = type_node;
 	    type_node = type_node->next;
-	    free(ident_ctl->hash);
+	    if (ident_ctl->hash != NULL)
+		free(ident_ctl->hash);
 	    free(ident_ctl);
 	    free(curr_type_node);
 	}
     }
-    free(type_ctl->hash);
+    if (type_ctl->hash != NULL)
+	free(type_ctl->hash);
 }
 
 static void
