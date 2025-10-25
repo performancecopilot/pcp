5a41fc9f1fafe08ab5788a7c8ca5bac33803ee6e
Ken McDonell <kenj@kenj.id.au>
2025-10-26 07:46:48 +1100

    src/python/pcp/pmconfig.py: replace strftime('%s') uses
    Revisit this with an improved fix.
    
    Put back the remade qa/667.out that was reverted earlier.

--- src/python/pcp/pmconfig.py
+++ src/python/pcp/pmconfig.py
@@ -1147,7 +1147,7 @@ class pmConfig(object):
 
         # Watch for end time in uninterpolated mode
         if not self.util.interpol:
-            sample = self.util.pmfg_ts().strftime('%s')
+            sample = self.util.pmfg_ts().timestamp()
             finish = self.util.opts.pmGetOptionFinish()
             if float(sample) > float(finish):
                 return -2
@@ -1168,7 +1168,7 @@ class pmConfig(object):
         self._round += 1
 
         if not self._init_ts:
-            self._init_ts = float(self.util.pmfg_ts().strftime("%s.%f"))
+            self._init_ts = self.util.pmfg_ts().timestamp()
 
         wakeup = self._init_ts + float(self.util.interval) * self._round
 
