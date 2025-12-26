performancecopilot/pcp

commit 590343053c5efe9a1e2210869a727539abed1daa

Fix libpcp_web build with no libuv.

src/libpcp_web/src/nowebgroup.c
@@ -103,3 +103,10 @@ pmWebGroupClose(pmWebGroupModule *module)
 {
     (void)module;
 }
+
+void
+pmWebGroupDestroy(pmWebGroupSettings *settings, sds id, void *arg)
+{
+    (void)settings; (void)id; (void)arg;
+    return -EOPNOTSUPP;
+}

src/libpcp_web/src/nodiscover.c
@@ -83,3 +83,11 @@ pmDiscoverClose(pmDiscoverModule *module)
 {
     (void)module;
 }
+
+int
+pmDiscoverSetHostSpec(pmDiscoverModule *module, sds hostspec)
+{
+    (void)module;
+    (void)hostspec;
+    return -ENOTSUP;	/* deprecated, use pmDiscoverSetConfiguration */
+}

src/libpcp_web/src/nologgroup.c
@@ -15,6 +15,8 @@
 /* Stub routines for platforms lacking necessary features (UV, SSL) */
 #include "pmwebapi.h"
 
+struct mmv_registry;
+
 int
 pmLogGroupLabel(pmLogGroupSettings *sp, const char *content, size_t length, struct dict *dp, void *arg)
 {
@@ -75,4 +77,17 @@ void
 pmLogGroupClose(pmLogGroupModule *module)
 {
     (void)module;
+}
+
+void
+pmLogPathsReset(pmLogGroupModule *module)
+{
+    (void)module;
+}
+
+int
+pmLogPathsSetMetricRegistry(pmLogGroupModule *module, struct mmv_registry *logpaths)
+{
+    (void)module; (void)logpaths;
+    return -EOPNOTSUPP;
 }

commit 0d9ee7d372f5165843a20740e6716f82443a5dae

Fix nowebgroup.c: remove return from void function
Fix compile error in nowebgroup.c:111 where pmWebGroupDestroy
(declared as void) incorrectly returns -EOPNOTSUPP.

This bug was introduced in commit 590343053c (Dec 17, 2025) which
added the stub but included a return statement in a void function.

Fixes compilation on macOS without libuv installed, where modern
Clang enforces -Wreturn-mismatch.

src/libpcp_web/src/nowebgroup.c
@@ -108,5 +108,4 @@ void
 pmWebGroupDestroy(pmWebGroupSettings *settings, sds id, void *arg)
 {
     (void)settings; (void)id; (void)arg;
-    return -EOPNOTSUPP;
 }