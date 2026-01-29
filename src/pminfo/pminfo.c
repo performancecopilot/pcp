performancecopilot/pcp

commit 86ab945bee84f8006e0e441d254256e4e1aa0639

src/pminfo/pminfo.c: error path rework
Started off with a memory corruption that was triggered by
qa/admin/upgrade-test when installing new PCP packages on
a system where PCP once lived, leading to a mismatch between
pmcd.conf and the PMNS, and a dynamic metric in the PMNS that
had no associated PMDA known to pmcd.

Fixing that proved a bit more difficult, as we have "batched"
and "unbatched" versions of both name lookup and pmDesc retrieval
that have different error protocols.  Needed to play games with
namelist[] and pmidlist[] to avoid "use-after-free" and "leak"
memory conditions in the presence of various error combinations
and command line arguments.

src/pminfo/pminfo.c
@@ -806,6 +806,12 @@ mydesc(pmDesc *desc)
     const char          *units;
     char                strbuf[60];
 
+    if (desc->pmid == PM_ID_NULL) {
+	/* no metadata, reason reported earlier */
+	printf("    Metadata unavailable\n");
+	return;
+    }
+
     printf("    Data Type: %s", (type = mytypestr(desc->type)));
     if (strcmp(type, "???") == 0)
 	printf(" (%d)", desc->type);
@@ -836,31 +842,59 @@ report(void)
     int		all_count;
     int		*all_inst;
     char	**all_names;
+    int		batch = batchidx;
 
     if (batchidx == 0)
 	return;
 
     /* Lookup names. 
      * Cull out names that were unsuccessfully looked up. 
-     * However, it is unlikely to fail because names come from a traverse PMNS. 
+     * However, it is unlikely to fail because names come from a
+     * traverse PMNS, however dynamic metrics can mean the non-leaf
+     * name is in the PMNS, but there are no descendent names.
      */
     if (need_pmid) {
+	int	j;
+	int	lsts;
         if ((sts = pmLookupName(batchidx, (const char **)namelist, pmidlist)) < 0) {
-	    int j = 0;
-	    for (i = 0; i < batchidx; i++) {
-		if (pmidlist[i] == PM_ID_NULL) {
-		    printf("%s: pmLookupName: %s\n", namelist[i], pmErrStr(sts));
-		    free(namelist[i]);
-		}
-		else {
-		    /* assert(j <= i); */
-		    pmidlist[j] = pmidlist[i];
-		    namelist[j] = namelist[i];
-		    j++;
+	    if (batchidx > 1)
+		printf("%s...%s: pmLookupName: %s\n", namelist[0], namelist[batchidx-1], pmErrStr(sts));
+	    else
+		printf("%s: pmLookupName: %s\n", namelist[0], pmErrStr(sts));
+	}
+
+	/*
+	 * cull any names with no PMID
+	 */
+	j = 0;
+	for (i = 0; i < batchidx; i++) {
+	    if (pmidlist[i] == PM_ID_NULL) {
+		if (sts >= 0)  {
+		    /*
+		     * not reported above, get the real reason for this one
+		     * not having a valid PMID
+		     */
+		    lsts = pmLookupName(1, (const char **)&namelist[i], &pmidlist[i]);
+		    printf("%s: pmLookupName: %s\n", namelist[i], pmErrStr(lsts));
 		}
 	    }
-	    batchidx = j;
+	    else {
+		/* assert(j <= i); */
+		if (j != i) {
+		    /*
+		     * swap names, so that free() works at the end
+		     * and shuffle PMIDs
+		     */
+		    char	*tmp;
+		    tmp = namelist[j];
+		    namelist[j] = namelist[i];
+		    namelist[i] = tmp;
+		    pmidlist[j] = pmidlist[i];
+		}
+		j++;
+	    }
 	}
+	batch = j;
     }
 
     if (p_value || p_label || verify) {
@@ -870,21 +904,55 @@ report(void)
 		exit(1);
 	    }
 	}
-	if ((sts = pmFetch(batchidx, pmidlist, &result)) < 0) {
-	    for (i = 0; i < batchidx; i++)
+	if ((sts = pmFetch(batch, pmidlist, &result)) < 0) {
+	    for (i = 0; i < batch; i++)
 		printf("%s: pmFetch: %s\n", namelist[i], pmErrStr(sts));
 	    goto done;
 	}
     }
 
     if (p_desc || p_value || p_label || p_series || verify) {
-	if (batchidx > 1) {
-	    if ((sts = pmLookupDescs(batchidx, pmidlist, desclist)) < 0) {
-		printf("%s...%s: pmLookupDescs: %s\n", namelist[0], namelist[batchidx-1], pmErrStr(sts));
+	if (batch > 1) {
+	    int		j;
+	    int		lsts;
+	    if ((sts = pmLookupDescs(batch, pmidlist, desclist)) < 0) {
+		printf("%s...%s: pmLookupDescs: %s\n", namelist[0], namelist[batch-1], pmErrStr(sts));
 		goto done;
+	    }
+	    /*
+	     * optionally report any metrics with no metadata and remove
+	     * them from the list ... for -f and -v reporting will be
+	     * done after pmFetch() with error codes per metric
+	     */
+	    if (!p_value && !verify) {
+		j = 0;
+		for (i = 0; i < batch; i++) {
+		    if (pmidlist[i] != PM_ID_NULL && desclist[i].pmid == PM_ID_NULL) {
+			/* no metadata, find out why ...  */
+			lsts = pmLookupDesc(pmidlist[i], &desclist[i]);
+			printf("%s: pmLookupDesc: %s\n", namelist[i], pmErrStr(lsts));
+			/* assert(j <= i); */
+			if (j != i) {
+			    /*
+			     * swap names, so that free() works at the end
+			     * and shuffle PMIDs
+			     */
+			    char	*tmp;
+			    tmp = namelist[j];
+			    namelist[j] = namelist[i];
+			    namelist[i] = tmp;
+			    pmidlist[j] = pmidlist[i];
+			}
+		    }
+		    else
+			j++;
+		}
+		batch = j;
 	    }
 	}
 	else {
+	    if (pmidlist[0] == PM_ID_NULL)
+		goto done;
 	    if ((sts = pmLookupDesc(pmidlist[0], &desclist[0])) < 0) {
 		printf("%s: pmLookupDesc: %s\n", namelist[0], pmErrStr(sts));
 		goto done;
@@ -892,12 +960,11 @@ report(void)
 	}
     }
 
-    for (i = 0; i < batchidx; i++) {
+    for (i = 0; i < batch; i++) {
 
 	if (p_desc || p_help || p_value || p_label)
 	    /* Not doing verify, output separator  */
 	    putchar('\n');
-
 
 	if (p_value || verify) {
 	    vsp = result->vset[i];

commit b5c19a6133c1f67634a4deb54a7dcff559ec9b52

src/pminfo/pminfo.c: fix bug introduced on 25 Sep 2025
Commit 86ab945be introduced some "shuffling" code to deal with batching
of requests to pmcd (pmLookupDescs() in particular) and metrics that
did not have a valid pmDesc, like sample.bad.unknown.

The "shuffling" code was just plain wrong!

And only by accident did qa/1105 start to fail on bozo after some
libpcp changes, but the trigger had nothing to do with libpcp,
rather it depends on the presence of a "bad" metric in the PMNS
and the (accidental) placement of that metric and the qa.ok metric
that qa/1105 was not seeing) in the batches after pmTraversePMNS()
has returned names in an order that depends on which PMDAs have been
installed and in which order they've been installed.

To make matters worse, pminfo passes but pminfo -d fails (in qa/1105).

src/pminfo/pminfo.c
@@ -882,14 +882,16 @@ report(void)
 		/* assert(j <= i); */
 		if (j != i) {
 		    /*
-		     * swap names, so that free() works at the end
-		     * and shuffle PMIDs
+		     * swap names and PMIDs, so that free() works at the end
 		     */
-		    char	*tmp;
-		    tmp = namelist[j];
+		    char	*tmp_name;
+		    pmID	tmp_pmid;
+		    tmp_name = namelist[j];
 		    namelist[j] = namelist[i];
-		    namelist[i] = tmp;
+		    namelist[i] = tmp_name;
+		    tmp_pmid = pmidlist[j];
 		    pmidlist[j] = pmidlist[i];
+		    pmidlist[i] = tmp_pmid;
 		}
 		j++;
 	    }
@@ -931,21 +933,24 @@ report(void)
 			/* no metadata, find out why ...  */
 			lsts = pmLookupDesc(pmidlist[i], &desclist[i]);
 			printf("%s: pmLookupDesc: %s\n", namelist[i], pmErrStr(lsts));
+		    }
+		    else {
 			/* assert(j <= i); */
 			if (j != i) {
 			    /*
-			     * swap names, so that free() works at the end
-			     * and shuffle PMIDs
+			     * swap names and PMIDs, so that free() works at the end
 			     */
-			    char	*tmp;
-			    tmp = namelist[j];
+			    char	*tmp_name;
+			    pmID	tmp_pmid;
+			    tmp_name = namelist[j];
 			    namelist[j] = namelist[i];
-			    namelist[i] = tmp;
+			    namelist[i] = tmp_name;
+			    tmp_pmid = pmidlist[j];
 			    pmidlist[j] = pmidlist[i];
+			    pmidlist[i] = tmp_pmid;
 			}
-		    }
-		    else
 			j++;
+		    }
 		}
 		batch = j;
 	    }