b99638960c2d21edd66a88ebc6b42747fb96774a
Ken McDonell <kenj@kenj.id.au>
2026-06-13 14:15:23 +1000

    src/pmdas/darwin: move some diagnostics inside the scope of -Dappl0 guards
    Several QA tests use PM_CONTEXT_LOCAL and having stderr unconditionally
    cluttered with "info" messages was not helpful.

--- src/pmdas/darwin/pmda.c
+++ src/pmdas/darwin/pmda.c
@@ -13,6 +13,9 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
+ *
+ * Debugging options
+ * appl0	enable info diagnostics
  */
 #include <stdio.h>
 #include <stdlib.h>
